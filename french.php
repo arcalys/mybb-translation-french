bfaee98f42e1e5f5a854475eff45e3cef08de7f2
Axel Fontana <axel.fontana@qualifio.com>
2025-09-17 19:05:33 +0200

    chore: initialize repository

---
+++ french.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * MyBB 1.8 French Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+// The friendly name of the language
+$langinfo['name'] = "Français (French)";
+
+// The author of the language
+$langinfo['author'] = "arcalys";
+
+// The language authors website
+$langinfo['website'] = "https://github.com/arcalys";
+
+// The documentation site link
+//$langinfo['docs_link'] = "";
+
+// The documentation FAQs site link
+$langinfo['common_issues_link'] = "https://github.com/arcalys/mybb-french-translation/issues";
+
+// The support website link
+$langinfo['support_link'] = "https://github.com/arcalys/mybb-french-translation";
+
+// The support website name
+$langinfo['support_name'] = "MyBB french translation (GitHub)";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1839";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 1;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "fr";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
