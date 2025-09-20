623569e42a66ff480042844243fa816423710edd
Axel Fontana <axel.fontana@qualifio.com>
2025-09-20 23:32:44 +0200

    feat: add translation for portal

---
+++ french/portal.lang.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_portal'] = "Portail";
+$l['forum'] = "Publié dans";
+$l['replies'] = "réponses";
+$l['reply'] = "réponse";
+$l['no_replies'] = "Aucune réponse";
+$l['latest_threads'] = "Derniers sujets";
+$l['latest_threads_replies'] = "réponses";
+$l['latest_threads_reply'] = "réponse";
+$l['latest_threads_views'] = "vues";
+$l['latest_threads_view'] = "vue";
+$l['latest_threads_lastpost'] = "Dernier message par";
+$l['private_messages'] = "Messages privés";
+$l['pms_unread'] = "Messages non lus";
+$l['pms_total'] = "Nombre de messages";
+$l['search_forums'] = "Rechercher dans les forums";
+$l['advanced_search'] = "Recherche avancée";
+$l['forum_stats'] = "Statistiques du forum";
+$l['num_members'] = "Membres";
+$l['latest_member'] = "Dernier membre inscrit";
+$l['num_threads'] = "Sujets";
+$l['num_posts'] = "Messages";
+$l['welcome'] = "Bienvenue, {1}";
+$l['guest_welcome_registration'] = "Vous devez <a href=\"{1}\">vous inscrire</a> avant de pouvoir publier sur notre site.";
+$l['username'] = "Nom d’utilisateur";
+$l['password'] = "Mot de passe";
+$l['login'] = "Connexion";
+$l['member_welcome_lastvisit'] = "Dernière visite :";
+$l['since_then'] = "Depuis, il y a eu :";
+$l['new_announcements'] = "{1} nouvelles annonces";
+$l['new_announcement'] = "1 nouvelle annonce";
+$l['new_threads'] = "{1} nouveaux sujets";
+$l['new_thread'] = "1 nouveau sujet";
+$l['new_posts'] = "{1} nouveaux messages";
+$l['new_post'] = "1 nouveau message";
+$l['online'] = "Utilisateurs en ligne";
+$l['online_user'] = "Il y a actuellement 1 utilisateur en ligne";
+$l['online_users'] = "Il y a actuellement {1} utilisateurs en ligne.";
+$l['online_counts'] = "{1} utilisateur(s) enregistré(s) et {2} invité(s)";
+$l['latest_announcements'] = "Dernières annonces";
+$l['portal_disabled'] = "Vous ne pouvez pas utiliser le portail car il a été désactivé par un administrateur.";
