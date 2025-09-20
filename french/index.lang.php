1ed566f011c02d1aa074d2ef00692936ef6774fd
Axel Fontana <axel.fontana@qualifio.com>
2025-09-20 23:36:43 +0200

    feat: add translation for index

---
+++ french/index.lang.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['boardstats'] = "Statistiques";
+$l['new_posts'] = "Ce forum contient de nouveaux messages";
+$l['no_new_posts'] = "Ce forum ne contient pas de nouveaux messages";
+$l['forum_closed'] = "Le forum est fermé";
+$l['forum_unapproved_posts_count'] = "Il y a actuellement {1} message(s) non approuvé(s) dans ce forum.";
+$l['forum_unapproved_post_count'] = "Il y a actuellement 1 message non approuvé dans ce forum.";
+$l['forum_unapproved_threads_count'] = "Il y a actuellement {1} sujet(s) non approuvé(s) dans ce forum.";
+$l['forum_unapproved_thread_count'] = "Il y a actuellement 1 sujet non approuvé dans ce forum.";
+$l['forumstats'] = "Statistiques du forum";
+$l['todays_birthdays'] = "Anniversaires";
+$l['birthdayhidden'] = "Invisible";
+$l['quick_login'] = "Connexion:";
+$l['index_logout'] = "Déconnexion";
+$l['private_messages'] = "Messages privés";
+$l['pms_new'] = "Vous avez {1} nouveau(x) message(s) depuis votre dernière visite.";
+$l['pms_unread_total'] = "(Vous avez {1} message(s) non lu(s) et {2} message(s) au total dans tous vos dossiers.)";
+$l['stats_posts_threads'] = "Nos membres ont publié un total de {1} messages dans {2} sujets.";
+$l['stats_numusers'] = "Nous avons actuellement {1} membre(s) enregistré(s).";
+$l['stats_newestuser'] = "Notre membre le plus récent est <b>{1}</b>";
+$l['stats_mostonline'] = "Le nombre maximal d’utilisateurs en ligne simultanément a été de {1}, {2} à {3}.";
+$l['whos_online'] = "Qui est en ligne ?";
+$l['complete_list'] = "Liste complète";
+$l['online_online_plural'] = "utilisateurs";
+$l['online_online_singular'] = "utilisateur";
+$l['online_member_plural'] = "utilisateurs enregistrés";
+$l['online_member_singular'] = "utilisateur enregistré";
+$l['online_anon_plural'] = "sont";
+$l['online_anon_singular'] = "est";
+$l['online_guest_plural'] = "invités";
+$l['online_guest_singular'] = "invité";
+$l['online_note'] = "{1} {2} actif dans les dernières {3} minutes ({4} {5}, dont {6} {7} invisible(s), et {8} {9}).";
+$l['subforums'] = "Sous-forums :";
+$l['board_stats_link_separator'] = ' | ';
