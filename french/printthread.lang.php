644f16ff2d0ee9aead11a1a29a463433ad2c37f2
Axel Fontana <axel.fontana@qualifio.com>
2025-09-21 23:03:06 +0200

    feat: add translation for threads printing

---
+++ french/printthread.lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['forum'] = "Forum:";
+$l['printable_version'] = "Version imprimable";
+$l['pages'] = "Pages:";
+$l['thread'] = "Sujet:";
