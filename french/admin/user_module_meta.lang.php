faa2e8a3f9bef2e5cd0b273f657480fdb2d2c685
Axel Fontana <axel.fontana@qualifio.com>
2025-09-21 13:36:42 +0200

    feat: add translation for admin/user-module

---
+++ french/admin/user_module_meta.lang.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['users_and_groups'] = "Utilisateurs et groupes";
+
+$l['users'] = "Utilisateurs";
+$l['awaiting_activation'] = "En attente d’activation";
+$l['groups'] = "Groupes";
+$l['user_titles'] = "Titres personnalisés";
+$l['banning'] = "Bannissements";
+$l['admin_permissions'] = "Permissions d’administration";
+$l['mass_mail'] = "Courriels groupés";
+$l['group_promotions'] = "Promotions de groupes";
+
+$l['can_manage_users'] = "Peut gérer les utilisateurs ?";
+$l['can_manage_awaiting_activation'] = "Peut gérer les comptes en attente d’activation ?";
+$l['can_manage_user_groups'] = "Peut gérer les groupes ?";
+$l['can_manage_user_titles'] = "Peut gérer les titres personnalisés ?";
+$l['can_manage_user_bans'] = "Peut gérer les bannissements ?";
+$l['can_manage_admin_permissions'] = "Peut gérer les permissions d’administration ?";
+$l['can_send_mass_mail'] = "Peut envoyer des courriels groupés ?";
+$l['can_manage_group_promotions'] = "Peut gérer les promotions de groupes ?";
