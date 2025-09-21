f56b43b292d112fcf2ab75759ee6cf6559780a86
Axel Fontana <axel.fontana@qualifio.com>
2025-09-21 14:56:20 +0200

    feat: add translation for admin/tools-php-info

---
+++ french/admin/tools_php_info.lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['php_info'] = "Informations PHP";
+$l['browser_no_iframe_support'] = "Votre navigateur ne supporte pas les iFrames.";
