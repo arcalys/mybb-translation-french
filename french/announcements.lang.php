cf464cc60eaa2ef7634580f0bc8ead6bc611e6f5
Axel Fontana <axel.fontana@qualifio.com>
2025-09-21 22:52:49 +0200

    feat: add translation for announcements

---
+++ french/announcements.lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_announcements'] = "Annonce";
+$l['announcements'] = "Annonce";
+$l['forum_announcement'] = "Annonce : {1}";
+$l['error_invalidannouncement'] = "L’annonce spécifiée est invalide.";
+
+$l['announcement_edit'] = "Modifier cette annonce";
+$l['announcement_qdelete'] = "Supprimer cette annonce";
+$l['announcement_quickdelete_confirm'] = "Êtes-vous sûr de vouloir supprimer cette annonce ?";
