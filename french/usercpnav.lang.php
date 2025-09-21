4d2f3f407a65209e1771800bd7f95c3e049bc443
Axel Fontana <axel.fontana@qualifio.com>
2025-09-21 23:09:55 +0200

    feat: add translation for user CP nav

---
+++ french/usercpnav.lang.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['ucp_nav_width'] = "180";
+$l['ucp_nav_menu'] = "Menu";
+$l['ucp_nav_messenger'] = "Messages privés";
+$l['ucp_nav_compose'] = "Nouveau message";
+$l['ucp_nav_tracking'] = "Suivi des messages";
+$l['ucp_nav_edit_folders'] = "Modifier les dossiers";
+$l['ucp_nav_profile'] = "Profil";
+$l['ucp_nav_edit_profile'] = "Modifier le profil";
+$l['ucp_nav_edit_options'] = "Modifier les préférences";
+$l['ucp_nav_change_email'] = "Modifier l’adresse courriel";
+$l['ucp_nav_change_pass'] = "Modifier le mot de passe";
+$l['ucp_nav_change_username'] = "Modifier le nom d’utilisateur";
+$l['ucp_nav_edit_sig'] = "Modifier la signature";
+$l['ucp_nav_change_avatar'] = "Modifier l’avatar";
+$l['ucp_nav_misc'] = "Divers";
+$l['ucp_nav_editlists'] = "Amis et ignorés";
+$l['ucp_nav_favorite_threads'] = "Sujets favoris";
+$l['ucp_nav_subscribed_threads'] = "Sujets suivis";
+$l['ucp_nav_forum_subscriptions'] = "Abonnements";
+$l['ucp_nav_drafts'] = "Brouillons enregistrés";
+$l['ucp_nav_drafts_active'] = "<strong>Brouillons enregistrés ({1})</strong>";
+$l['ucp_nav_notepad'] = "Vloc-notes personnel";
+$l['ucp_nav_view_profile'] = "Voir le profil";
+$l['ucp_nav_home'] = "Accueil du panneau d’utilisateur";
+$l['ucp_nav_usergroups'] = "Groupes";
+$l['ucp_nav_attachments'] = "Pièces jointes";
+$l['ucp_nav_securitylog'] = "Journal de sécurité";
