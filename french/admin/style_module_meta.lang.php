8f609e0bef89ecc676320e0124dff14297769f31
Axel Fontana <axel.fontana@qualifio.com>
2025-09-21 14:53:57 +0200

    feat: add translation for admin/style-module

---
+++ french/admin/style_module_meta.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['templates_and_style'] = "Thèmes et templates";
+
+$l['themes'] = "Thèmes";
+$l['templates'] = "Templates";
+
+$l['can_manage_themes'] = "Peut gérer les thèmes ?";
+$l['can_manage_templates'] = "Peut gérer les templates ?";
