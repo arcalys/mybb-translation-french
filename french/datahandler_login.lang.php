c62917097b225f05ca65b83f79085d9c58fd664f
Axel Fontana <axel.fontana@qualifio.com>
2025-09-22 10:31:18 +0200

    feat: add translation for login datahandler

---
+++ french/datahandler_login.lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['logindata_invalidpwordusername'] = "Vous avez saisi une combinaison nom d’utilisateur/mot de passe invalide.<br /><br />Si vous avez oublié votre mot de passe, veuillez <a href=\"member.php?action=lostpw\">le réinitialiser</a>.";
+$l['logindata_invalidpwordusernameemail'] = "Vous avez saisi une combinaison adresse courriel/mot de passe invalide.<br /><br />Si vous avez oublié votre mot de passe, veuillez <a href=\"member.php?action=lostpw\">le réinitialiser</a>.";
+$l['logindata_invalidpwordusernamecombo'] = "Vous avez saisi une combinaison nom d’utilisateur/mot de passe ou adresse courriel/mot de passe invalide.<br /><br />Si vous avez oublié votre mot de passe, veuillez <a href=\"member.php?action=lostpw\">le réinitialiser</a>.";
+
+$l['logindata_regimageinvalid'] = "Le code de vérification d’image que vous avez saisi est incorrect. Veuillez entrer le code tel qu’il apparaît dans l’image.";
+$l['logindata_regimagerequired'] = "Veuillez saisir le code de vérification d’image pour continuer le processus de connexion. Entrez le code tel qu’il apparaît dans l’image.";
