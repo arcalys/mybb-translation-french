afbb35dc5c30a79eef022665ae0a1a8404977d6f
Axel Fontana <axel.fontana@qualifio.com>
2025-09-23 12:17:08 +0200

    feat: add translation for admin/config/questions

---
+++ french/admin/config_questions.lang.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['security_questions'] = "Questions de sécurité";
+$l['security_questions_desc'] = "Cette section vous permet de gérer des questions de sécurité qui seront affichées aléatoirement lors de nouvelles inscriptions.";
+$l['add_new_question'] = "Ajouter une question";
+$l['add_new_question_desc'] = "Ici, vous pouvez ajouter une nouvelle question de sécurité.";
+$l['edit_question'] = "Modifier la question";
+$l['edit_question_desc'] = "Ici, vous pouvez modifier une question de sécurité.";
+$l['preview_question'] = "Prévisualiser la question";
+
+$l['options'] = "Options";
+$l['shown'] = "Affichages";
+$l['correct'] = "Réponses correctes";
+$l['incorrect'] = "Réponses incorrectes";
+$l['disable_question'] = "Désactiver la question";
+$l['enable_question'] = "Activer la question";
+$l['delete_question'] = "Supprimer la question";
+$l['no_security_questions'] = "Il n’existe actuellement aucune question de sécurité sur votre forum.";
+
+$l['question'] = "Question";
+$l['question_desc'] = "Saisissez la question à laquelle l’utilisateur devra répondre lors de l’inscription.";
+$l['answers'] = "Réponse(s)";
+$l['answers_desc'] = "Saisissez les réponses à la question ci-dessus (une réponse par ligne).";
+$l['active'] = "Activée";
+$l['save_question'] = "Enregistrer la question";
+
+$l['error_invalid_question'] = "La question spécifiée n’existe pas.";
+$l['error_missing_question'] = "Veuillez saisir une question.";
+$l['error_missing_answer'] = "Veuillez saisir au moins une réponse à la question.";
+
+$l['success_question_created'] = "La question a bien été créée.";
+$l['success_question_updated'] = "La question a bien été mise à jour.";
+$l['success_question_disabled'] = "La question a bien été désactivée.";
+$l['success_question_enabled'] = "La question a bien été activée.";
+$l['success_question_deleted'] = "La question a bien été supprimée.";
+
+$l['confirm_question_deletion'] = "Êtes-vous sûr de vouloir supprimer cette question ?";
