a355a810c1f5610956be233a885690ef893dbf7f
Axel Fontana <axel.fontana@qualifio.com>
2025-09-23 18:00:11 +0200

    feat: add translation for showteam

---
+++ french/showteam.lang.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_showteam'] = "Équipe du forum";
+$l['forum_team'] = "Équipe du forum";
+$l['moderators'] = "Modérateurs";
+$l['username'] = "Nom d’utilisateur";
+$l['lastvisit'] = "Dernière visite";
+$l['email'] = "Courriel";
+$l['pm'] = "Message privé";
+$l['mod_forums'] = "Forum(s)";
+$l['mod_groups'] = "Groupe(s)";
+$l['online'] = "En ligne";
+$l['offline'] = "Hors-ligne";
+$l['away'] = "Absent";
+
+$l['group_leaders'] = "Responsables du groupe";
+$l['group_members'] = "Membre(s)";
+
+$l['no_members'] = "Il n’y a aucun utilisateur dans ce groupe.";
+$l['showteam_disabled'] = "La fonction d’affichage de l’équipe du forum a été désactivée par un administrateur.";
+$l['error_noteamstoshow'] = "Il n’y a aucun membre de l’équipe du forum à afficher.";
