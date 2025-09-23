cb0f1da03ebb017007be85391d07440c58740c41
Axel Fontana <axel.fontana@qualifio.com>
2025-09-23 23:03:58 +0200

    feat: add translation for sendthread

---
+++ french/sendthread.lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_sendthread'] = "Partager un sujet avec un ami";
+
+$l['send_thread'] = "Partager un sujet avec un ami";
+$l['recipient'] = "Destinataire:";
+$l['recipient_note'] = "Saisissez l’adresse électronique de votre ami.";
+$l['subject'] = "Sujet:";
+$l['message'] = "Message:";
+$l['error_nosubject'] = "Vous devez saisir un sujet pour pouvoir envoyer ce message.";
+$l['error_nomessage'] = "Vous devez saisir un message avant de pouvoir envoyer ce sujet à votre ami.";
