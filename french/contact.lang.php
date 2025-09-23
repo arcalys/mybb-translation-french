5f572ddf497712b2e24aac4067de7dd03ef8b976
Axel Fontana <axel.fontana@qualifio.com>
2025-09-23 18:07:38 +0200

    feat: add translation for contact

--- french/contact.lang.php
+++ french/contact.lang.php
@@ -6,21 +6,21 @@
  *
  */
 
-$l['contact'] = 'Nous contacter';
-$l['contact_no_message'] = 'Veuillez saisir un message.';
-$l['contact_no_subject'] = 'Veuillez saisir un sujet.';
-$l['contact_no_email'] = 'Veuillez saisir une adresse courriel valide.';
-$l['contact_success_message'] = 'Votre message a bien été envoyé.';
-$l['contact_subject'] = 'Sujet';
-$l['contact_subject_desc'] = 'Indiquez brièvement l’objet de votre message.';
-$l['contact_message'] = 'Message';
-$l['contact_message_desc'] = 'Décrivez votre demande en détail.';
-$l['contact_email'] = 'Adresse courriel';
-$l['contact_email_desc'] = 'Entrez votre adresse courriel afin que nous puissions vous contacter.';
-$l['contact_send'] = 'Envoyer';
-$l['subject_too_long'] = 'Le sujet est trop long. Veuillez saisir un sujet de moins de {1} caractères (actuellement {2}).';
-$l['message_too_short'] = 'Le message est trop court. Veuillez saisir un message de plus de {1} caractères (actuellement {2}).';
-$l['message_too_long'] = 'Le message est trop long. Veuillez saisir un message de moins de {1} caractères (actuellement {2}).';
+$l['contact'] = "Nous contacter";
+$l['contact_no_message'] = "Veuillez saisir un message.";
+$l['contact_no_subject'] = "Veuillez saisir un sujet.";
+$l['contact_no_email'] = "Veuillez saisir une adresse électronique valide.";
+$l['contact_success_message'] = "Votre message a bien été envoyé.";
+$l['contact_subject'] = "Sujet";
+$l['contact_subject_desc'] = "Indiquez brièvement l’objet de votre message.";
+$l['contact_message'] = "Message";
+$l['contact_message_desc'] = "Décrivez votre demande en détail.";
+$l['contact_email'] = "Adresse électronique";
+$l['contact_email_desc'] = "Entrez votre adresse électronique afin que nous puissions vous contacter.";
+$l['contact_send'] = "Envoyer";
+$l['subject_too_long'] = "Le sujet est trop long. Veuillez saisir un sujet de moins de {1} caractères (actuellement {2}).";
+$l['message_too_short'] = "Le message est trop court. Veuillez saisir un message de plus de {1} caractères (actuellement {2}).";
+$l['message_too_long'] = "Le message est trop long. Veuillez saisir un message de moins de {1} caractères (actuellement {2}).";
 
-$l['error_stop_forum_spam_spammer'] = 'Désolé, {1} correspond à un spammeur connu et votre message a donc été bloqué.';
-$l['error_stop_forum_spam_fetching'] = 'Désolé, une erreur est survenue lors de la vérification de votre message dans la base de données des spammeurs. La base de données n’a probablement pas pu être consultée. Veuillez réessayer plus tard.';
+$l['error_stop_forum_spam_spammer'] = "{1} correspond à un spammeur connu et votre message a donc été bloqué.";
+$l['error_stop_forum_spam_fetching'] = "Une erreur est survenue lors de la vérification de votre message dans la base de données des spammeurs. Veuillez réessayer plus tard.";
