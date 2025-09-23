9f67cf02c3aebea19e9dd95c6468c68fd11ceb5f
Axel Fontana <axel.fontana@qualifio.com>
2025-09-23 18:04:31 +0200

    feat: add translation for stats

---
+++ french/stats.lang.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_stats'] = "Statistiques du forum";
+$l['stats_disabled'] = "Vous ne pouvez pas consulter la page des statistiques car elle a été désactivée par un administrateur.";
+
+$l['board_stats'] = "Statistiques du forum";
+$l['none'] = "Aucun";
+$l['totals'] = "Totaux";
+$l['averages'] = "Moyennes";
+$l['posts'] = "Messages";
+$l['threads'] = "Sujets";
+$l['members'] = "Utilisateurs";
+$l['ppd'] = "Messages par jour";
+$l['tpd'] = "Sujets par jour";
+$l['mpd'] = "Inscriptions par jour";
+$l['ppm'] = "Messages par utilisateur";
+$l['tpm'] = "Sujets par utilisateur";
+$l['rpt'] = "Réponses par sujet";
+$l['no_posts'] = "Aucun";
+$l['general'] = "Général";
+$l['newest_member'] = "Membre le plus récent";
+$l['members_posted'] = "Membres ayant publié des messages";
+$l['todays_top_poster'] = "Membre le plus actif aujourd’hui";
+$l['todays_top_poster_value'] = "{1} ({2} messages)";
+$l['popular_forum'] = "Forum le plus populaire";
+$l['popular_forum_value'] = "{1} ({2} messages, {3} sujets)";
+$l['most_popular'] = "Le plus populaire…";
+$l['most_replied_threads'] = "Sujets les plus commentés";
+$l['most_viewed_threads'] = "Sujets les plus consultés";
+$l['not_enough_info_stats'] = "Désolé, il n’y a pas assez d’informations pour générer les statistiques de ce forum. Pour les générer, le forum doit compter au moins 1 utilisateur et 1 sujet.";
+
+$l['replies'] = "réponses";
+$l['views'] = "vues";
+$l['top_referrer'] = "Meilleur parrain";
+$l['top_referrer_value'] = "{1} ({2} parrainages)";
