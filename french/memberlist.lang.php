726e082c8d6eab1fef1cd3b3f7d7332a1ed1285a
Axel Fontana <axel.fontana@qualifio.com>
2025-09-23 23:10:15 +0200

    feat: add translation for memberlist

---
+++ french/memberlist.lang.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_memberlist'] = "Liste des membres";
+
+$l['memberlist_disabled'] = "La fonction d’affichage de la liste des membres a été désactivée par un administrateur.";
+
+$l['member_list'] = "Liste des membres";
+$l['avatar']  = "Avatar";
+$l['username'] = "Nom d’utilisateur";
+$l['joined'] = "Date d’inscription";
+$l['lastvisit'] = "Dernière visite";
+$l['posts'] = "Nombre de messages";
+$l['threads'] = "Nombre de sujets";
+$l['referrals'] = "Parrainages";
+$l['search_members'] = "Rechercher dans la liste des membres";
+$l['website'] = "Site web";
+$l['sort_by'] = "Trier par";
+$l['contains'] = "Contient:";
+$l['exactly'] = "Est:";
+$l['sort_by_username'] = "Trier par : nom d’utilisateur";
+$l['sort_by_regdate'] = "Trier par : date d’inscription";
+$l['sort_by_lastvisit'] = "Trier par : date de dernière visite";
+$l['sort_by_posts'] = "Trier par : nombre de messages";
+$l['sort_by_threads'] = "Trier par : nombre de sujets";
+$l['sort_by_referrals'] = "Trier par : nombre d’utilisateurs parrainés";
+$l['order_asc'] = "ordre croissant";
+$l['order_desc'] = "ordre décroissant";
+$l['asc'] = "asc";
+$l['desc'] = "desc";
+$l['forumteam'] = "Voir l’équipe du forum";
+$l['advanced_search'] = "Recherche avancée";
+$l['nav_memberlist_search'] = "Rechercher";
+$l['search_member_list'] = "Recherche avancée dans la liste des membres";
+$l['search_criteria'] = "Critères de recherche";
+$l['begins_with'] = "commence par";
+$l['username_contains'] = "contient";
+$l['username_exact'] = "est";
+$l['search_website'] = "Site web";
+$l['search_options'] = "Options de recherche";
+$l['per_page'] = "Résultats par page";
+$l['search'] = "Rechercher";
+
+$l['error_no_members'] = "<p>There were no members found with the search criteria you entered.</p><p>Please enter a different search term and try again.</p>";
+$l['error_no_members'] = "<p>Aucun utilisateur n’a été trouvé.</p><p>Veuillez ajuster vos critères de recherche et réessayer.</p>";
