c6f754b5ea09090fd294609c947b8e4406d11fdc
Axel Fontana <axel.fontana@qualifio.com>
2025-09-23 10:38:40 +0200

    feat: add translation for admin/config/attachment-types

---
+++ french/admin/config_attachment_types.lang.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['attachment_types'] = "Types de pièces jointes";
+$l['attachment_types_desc'] = "Cette section vous permet de gérer les types de pièces jointes que les utilisateurs peuvent ajouter à leurs messages. Remarque : désactiver un type de pièce jointe rendra les pièces jointes existantes de ce type inaccessibles.";
+$l['add_new_attachment_type'] = "Ajouter un nouveau type de pièce jointe";
+$l['add_attachment_type'] = "Ajouter un type de pièce jointe";
+$l['add_attachment_type_desc'] = "Ici, vous pouvez ajouter un nouveau type de pièce jointe et permettre aux utilisateurs d’attacher des fichiers de ce type à leurs messages. Vous pouvez définir l’extension, le type MIME, la taille maximale et afficher une icône pour chaque type.";
+$l['edit_attachment_type'] = "Modifier un type de pièce jointe";
+$l['edit_attachment_type_desc'] = "Ici, vous pouvez modifier un type de pièce jointe.";
+
+$l['extension'] = "Extension";
+$l['maximum_size'] = "Taille maximale";
+$l['no_attachment_types'] = "Il n’existe actuellement aucun type de pièce jointe sur votre forum.";
+
+$l['name'] = "Nom";
+$l['name_desc'] = "Saisissez le nom du type de pièce jointe.";
+$l['file_extension'] = "Extension de fichier";
+$l['file_extension_desc'] = "Saisissez l’extension de fichier que vous souhaitez autoriser (sans le point). Par exemple : txt";
+$l['mime_type'] = "Type MIME";
+$l['mime_type_desc'] = "Saisissez le type MIME renvoyé par le serveur lors du téléchargement de fichiers de ce type (<a href=\"http://www.freeformatter.com/mime-types-list.html\">voir la liste ici</a>).";
+$l['maximum_file_size'] = "Taille maximale du fichier (en kibioctets)";
+$l['maximum_file_size_desc'] = "Taille maximale autorisée pour les fichiers de ce type en KiB (1 MiB = 1024 KiB).";
+$l['limit_intro'] = "Assurez-vous que la taille maximale du fichier est inférieure au plus petit des paramètres PHP suivants :";
+$l['limit_post_max_size'] = "Taille maximale d’un envoi (post_max_size) : {1}";
+$l['limit_upload_max_filesize'] = "Taille maximale d’un fichier téléversé (upload_max_filesize) : {1}";
+$l['attachment_icon'] = "Icône de pièce jointe";
+$l['attachment_icon_desc'] = "Si vous souhaitez afficher une icône pour les pièces jointes de ce type, indiquez ici son chemin. {theme} sera remplacé par le dossier d’images du thème de l’utilisateur, ce qui permet de définir des icônes différentes selon le thème.";
+$l['save_attachment_type'] = "Enregistrer le type de pièce jointe";
+
+$l['error_invalid_attachment_type'] = "Vous avez sélectionné un type de pièce jointe invalide.";
+$l['error_missing_mime_type'] = "Veuillez saisir un type MIME.";
+$l['error_missing_extension'] = "Veuillez saisir une extension de fichier.";
+
+$l['success_attachment_type_created'] = "Le type de pièce jointe a bien été créé.";
+$l['success_attachment_type_updated'] = "Le type de pièce jointe a bien été mis à jour.";
+$l['success_attachment_type_deleted'] = "Le type de pièce jointe a bien été supprimé.";
+
+$l['confirm_attachment_type_deletion'] = "Êtes-vous sûr de vouloir supprimer ce type de pièce jointe ?";
+
+$l['success_activated_attachment_type'] = "Le type de pièce jointe sélectionné a été activé avec succès.";
+$l['success_deactivated_attachment_type'] = "Le type de pièce jointe sélectionné a été désactivé avec succès.";
+
+$l['enabled'] = "Activé ?";
+$l['forcedownload'] = "Forcer le téléchargement";
+$l['forcedownload_desc'] = "Si cette option est activée, les pièces jointes de ce type seront toujours téléchargées plutôt qu’affichées.";
+$l['avatar_file'] = "Avatar";
+$l['avatar_file_desc'] = "Voulez-vous autoriser l’utilisation de ce type de pièce jointe pour les avatars ?";
+
+$l['available_to_groups'] = "Disponibilité pour les groupes";
+$l['available_in_forums'] = "Disponibilité dans les forums";
