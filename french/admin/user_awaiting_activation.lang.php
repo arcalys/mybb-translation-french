00c85434f8235476fd987a5a17328fcc93b73bb5
Axel Fontana <axel.fontana@qualifio.com>
2025-09-23 12:51:55 +0200

    feat: add translation for admin/user/awaiting-activation

---
+++ french/admin/user_awaiting_activation.lang.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['awaiting_activation'] = "En attente d’activation";
+$l['awaiting_activation_desc'] = "Cette section vous permet de gérer les comptes en attente d’activation. Notez que tout utilisateur en attente d’activation par courriel n’aura pas besoin de confirmer son adresse si son compte est activé ici.";
+
+$l['manage_awaiting_activation'] = "Gérer les comptes en attente d’activation";
+
+$l['no_users_awaiting_activation'] = "Il n’y a actuellement aucun compte en attente d’activation.";
+
+$l['username'] = "Nom d’utilisateur";
+$l['registered'] = "Date d’inscription";
+$l['last_active'] = "Dernière activité";
+$l['email'] = "Adresse électronique";
+$l['ipaddress'] = "Adresse IP";
+$l['type'] = "Type";
+
+$l['email_activation'] = "En attente d’activation par courriel";
+$l['administrator_activation'] = "En attente d’activation par un administrateur";
+$l['admin_activation_coppa'] = "En attente d’activation par un administrateur (utilisateur mineur)";
+
+$l['activate_users'] = "Activer les utilisateurs";
+$l['delete_users'] = "Supprimer les utilisateurs";
+
+$l['confirm_activate_users'] = "Êtes-vous sûr de vouloir activer les utilisateurs sélectionnés ?";
+$l['confirm_delete_users'] = "Êtes-vous sûr de vouloir supprimer les utilisateurs sélectionnés ?";
+
+$l['no_users_selected'] = "Vous n'avez sélectionné aucun utilisateur.";
+
+$l['success_users_deleted'] = "Les utilisateurs sélectionnés ont bien été supprimés.";
+$l['success_users_activated'] = "Les utilisateurs sélectionnés ont bien été activés.";
+
+$l['emailsubject_activateaccount'] = "Activation de votre compte sur {1}";
+$l['email_adminactivateaccount'] = "{1},
+
+Un administrateur a activé votre compte sur le forum.
+
+Pour continuer, veuillez vous rendre à l’adresse suivante :
+
+{3}
+
+Vous pourrez alors vous connecter avec les identifiants que vous avez enregistrés.
+
+Merci,
+L’équipe du forum {2}";
