241ccbd7ec7021bcf5c4e2c6cecb4f590673af66
Axel Fontana <axel.fontana@qualifio.com>
2025-09-23 10:38:53 +0200

    feat: add translation for admin/config/badwords

---
+++ french/admin/config_badwords.lang.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['bad_words'] = "Censure de mots";
+$l['edit_bad_word'] = "Modifier le filtre";
+$l['edit_bad_word_desc'] = "Ici, vous pouvez modifier un filtre de mots.";
+$l['bad_word_filters'] = "Censure de mots";
+$l['bad_word_filters_desc'] = "Cette section vous permet de gérer une liste de mots ou d’expressions qui seront automatiquement remplacés dans les messages. Elle peut s’avérer utile pour remplacer des termes inappropriés.";
+
+$l['bad_word'] = "Mot à filtrer";
+$l['bad_word_desc'] = "Saisissez le mot que vous souhaitez filtrer. Le symbole '*' représente n’importe quel nombre de caractères et le symbole '+' représente un seul caractère (autre que l’espace et le saut de ligne).";
+$l['bad_word_max'] = "Un mot filtré ne peut pas dépasser 100 caractères.";
+$l['replacement'] = "Mot de remplacement";
+$l['replacement_desc'] = "Saisissez le texte qui remplacera le mot filtré. Si ce champ est laissé vide, le mot sera censuré et remplacé par des astérisques.";
+$l['regex'] = "Expression régulière";
+$l['regex_desc'] = "Traiter le champ « Mot à filtrer » comme une expression régulière.";
+$l['replacement_word_max'] = "Le mot de remplacement ne peut pas dépasser 100 caractères.";
+$l['error_replacement_word_invalid'] = "Le mot de remplacement ne peut pas être identique au mot filtré.";
+
+$l['save_bad_word'] = "Enregistrer le filtre";
+$l['no_bad_words'] = "Il n’existe actuellement aucun filtre sur votre forum.";
+$l['add_bad_word'] = "Ajouter un filtre";
+$l['add_bad_word_desc'] = "Ici, vous pouvez créer un nouveau filtre de mots.";
+
+$l['error_missing_bad_word'] = "Veuillez saisir un mot à filtrer.";
+$l['error_invalid_regex'] = "L’expression régulière spécifiée est invalide.";
+$l['error_invalid_bid'] = "Le filtre spécifié n’existe pas.";
+$l['error_bad_word_filtered'] = "Le filtre que vous avez saisi existe déjà.";
+
+$l['success_added_bad_word'] = "Le filtre a bien été ajouté.";
+$l['success_deleted_bad_word'] = "Le filtre a bien été supprimé.";
+$l['success_updated_bad_word'] = "Le filtre a bien été mis à jour.";
+
+$l['confirm_bad_word_deletion'] = "Êtes-vous sûr de vouloir supprimer ce filtre ?";
