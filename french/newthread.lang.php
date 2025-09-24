d6e522c48ef3cc28f222a66fe1801a45f69eaea7
Axel Fontana <axel.fontana@qualifio.com>
2025-09-24 18:22:27 +0200

    feat: add translation for newthread

---
+++ french/newthread.lang.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_newthread'] = "Nouveau sujet";
+$l['newthread_in'] = "Nouveau sujet dans {1}";
+$l['post_new_thread'] = "Publier un nouveau sujet";
+$l['thread_subject'] = "Titre du sujet";
+$l['thread_subject_placeholder'] = "Titre du sujet…";
+$l['your_message'] = "Votre message";
+$l['post_options'] = "Options des messages";
+$l['options_sig'] = "<strong>Signature :</strong> inclure votre signature. (utilisateurs enregistrés uniquement)";
+$l['options_emailnotify'] = "<strong>Notification par courriel :</strong> recevez un courriel à chaque nouvelle réponse. (utilisateurs enregistrés uniquement)";
+$l['options_disablesmilies'] = "<strong>Désactiver les émoticônes :</strong> empêcher l’affichage des émoticônes dans ce message.";
+$l['post_thread'] = "Publier le sujet";
+$l['preview_post'] = "Prévisualiser";
+$l['add_poll'] = "Créer un sondage";
+$l['poll'] = "Sondage";
+$l['poll_desc'] = "Vous pouvez, si vous le souhaitez, ajouter un sondage à ce sujet.";
+$l['poll_check'] = "Je souhaite créer un sondage";
+$l['num_options'] = "Nombre de réponses :";
+$l['max_options'] = "(Maximum: {1})";
+$l['mod_options'] = "Options de modération";
+$l['close_thread'] = "<strong>Verrouiller le sujet :</strong> empêcher toute publication supplémentaire dans ce sujet.";
+$l['stick_thread'] = "<strong>Épingler le sujet :</strong> maintenir ce sujet en haut du forum.";
+$l['draft_saved'] = "Le nouveau sujet a bien été enregistré comme brouillon.<br />Vous allez maintenant être redirigé vers la liste de vos brouillons.";
+$l['error_post_already_submitted'] = "Vous avez déjà publié un sujet identique dans ce forum. Veuillez visiter le forum pour consulter votre sujet.";
+$l['no_prefix'] = "Aucun préfixe";
+$l['forum_rules'] = "{1} - Règles";
+
+$l['multiquote_external_one'] = "Vous avez sélectionné un message appartenant à un autre sujet.";
+$l['multiquote_external'] = "Vous avez sélectionné {1} messages appartenant à d’autres sujets.";
+$l['multiquote_external_one_deselect'] = "Désélectionner ce message";
+$l['multiquote_external_deselect'] = "Désélectionner ces messages";
+$l['multiquote_external_one_quote'] = "Citer également ce message";
+$l['multiquote_external_quote'] = "Citer également ces messages";
+
+$l['redirect_newthread'] = "Votre sujet a bien été publié.";
+$l['redirect_newthread_poll'] = "<br />Vous allez maintenant être redirigé vers la page de configuration du sondage.";
+$l['redirect_newthread_moderation'] = "<br />L’administrateur a indiqué que tous les nouveaux sujets nécessitent une approbation. Vous allez maintenant être redirigé vers la liste des sujets.";
+$l['redirect_newthread_unviewable'] = "<br />Vous n’avez pas la permission de voir les sujets dans ce forum. Vous allez maintenant être redirigé vers le forum.";
+$l['redirect_newthread_thread'] = "<br />Vous allez maintenant être redirigé vers le nouveau sujet.";
+$l['invalidthread'] = "Le brouillon spécifié n’existe pas ou vous n’avez pas la permission de le consulter.";
+
+$l['error_stop_forum_spam_spammer'] = "Votre {1} correspond à celui d’un spammeur connu. Si vous pensez qu’il s’agit d’une erreur, veuillez contacter un administrateur.";
+$l['error_stop_forum_spam_fetching'] = "Une erreur est survenue lors de la vérification de votre sujet dans la base de données des spammeurs. Veuillez réessayer plus tard.";
+
+$l['error_suspendedposting'] = "Vos privilèges de publication sont actuellement suspendus {1}.<br /><br />
+Date de suspension : {2}";
+
+$l['error_suspendedposting_temporal'] = "jusqu’au {1}";
+$l['error_suspendedposting_permanent'] = "de façon permanente";
