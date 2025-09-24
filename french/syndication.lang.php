97552b6f25e5afa4b94bda41d43ee24d48f6cad5
Axel Fontana <axel.fontana@qualifio.com>
2025-09-24 15:52:12 +0200

    feat: add translation for syndication

---
+++ french/syndication.lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['all_forums'] = "Tous les forums";
+$l['forum'] = "Forum:";
+$l['posted_by'] = "Publié par";
+$l['on'] = "activé";
+$l['portal'] = "Portail";
