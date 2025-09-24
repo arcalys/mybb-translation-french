e4c4d152f95904b52a6e7583475ed7eede64d329
Axel Fontana <axel.fontana@qualifio.com>
2025-09-24 13:36:12 +0200

    feat: add translation for helpsections

---
+++ french/helpsections.lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+// Help Section 1
+$l['s1_name'] = "Gérer son compte utilisateur";
+$l['s1_desc'] = "Instructions de base pour s’inscrire, mettre à jour son profil et utiliser son compte sur le forum.";
+
+
+// Help Section 2
+$l['s2_name'] = "Publication";
+$l['s2_desc'] = "Publier, répondre et utiliser le forum de manière basique.";
