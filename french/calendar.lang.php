fd2f51217b61e40726a099b831b5d104562daaf8
Axel Fontana <axel.fontana@qualifio.com>
2025-09-24 12:13:09 +0200

    feat: add translation for calendar

---
+++ french/calendar.lang.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_calendar'] = "Calendrier";
+$l['nav_viewevent'] = "Voir l’événement";
+$l['nav_dayview'] = "Vue par jour";
+$l['nav_addevent'] = "Ajouter un événement";
+$l['nav_editevent'] = "Modifier l’événement";
+$l['nav_move_event'] = "Déplacer l’événement";
+
+$l['calendar_disabled'] = "La fonction de calendrier a été désactivée par un administrateur.";
+
+// In some languages these months need to be slightly different than those defined in global.lang.php
+$l['alt_month_1'] = "Janvier";
+$l['alt_month_2'] = "Février";
+$l['alt_month_3'] = "Mars";
+$l['alt_month_4'] = "Avril";
+$l['alt_month_5'] = "Mai";
+$l['alt_month_6'] = "Juin";
+$l['alt_month_7'] = "Juillet";
+$l['alt_month_8'] = "Août";
+$l['alt_month_9'] = "Septembre";
+$l['alt_month_10'] = "Octobre";
+$l['alt_month_11'] = "Novembre";
+$l['alt_month_12'] = "Décembre";
+
+$l['add_private_event'] = "Ajouter un événement privé";
+$l['add_public_event'] = "Ajouter un événement public";
+
+$l['calendar'] = "Calendrier";
+
+$l['jump_month'] = "Aller au mois";
+
+$l['birthdays_on_day'] = "Anniversaires le {1}";
+$l['birthdayhidden'] = "Masqué";
+
+$l['event'] = "événement";
+$l['events'] = "événement(s)";
+$l['add_event'] = "Ajouter un événement";
+$l['event_date'] = "Date de l’événement";
+$l['event_recurring_start_date'] = "Date de début:";
+$l['event_recurring_end_date'] = "Date de fin:";
+$l['event_recurring_repeat_days'] = "L’événement se répète :";
+$l['event_name'] = "Nom de l’événement";
+$l['event_details'] = "Détails de l’événement";
+$l['event_options'] = "Options";
+$l['private_option'] = "<strong>Privé :</strong> Vous seul pourrez voir cet événement. (utilisateurs enregistrés uniquement).";
+$l['delete_option'] = "<strong>Supprimer : </strong> supprimer cet événement.";
+$l['post_event'] = "Publier l’événement";
+$l['day_view'] = "Aperçu du jour";
+$l['birthday'] = "Anniversaire";
+$l['birthdays'] = "Anniversaires";
+$l['event_author'] = "Créateur de l’événement:";
+$l['edit_event'] = "Modifier un événement";
+$l['view_event'] = "Voir l’événement";
+$l['no_events'] = "Il n’y a aucun événement planifié pour cette date.";
+$l['years_old'] = "{1} ans";
+$l['alt_edit'] = "Modifier cet événement";
+$l['alt_delete'] = "Supprimer cet événement";
+$l['moderator_options'] = "Options de modération";
+$l['approve_event'] = "Approuver l’événement";
+$l['unapprove_event'] = "Désapprouver l’événement";
+$l['move_event'] = "Déplacer l’événement";
+$l['repeats_every_day'] = "Se répète tous les jours";
+$l['repeats_every_x_days'] = "Se répète tous les {1} jours";
+$l['repeats_on_weekdays'] = "Se répète du lundi au vendredi";
+$l['every_week_on_days'] = "Se répète chaque semaine le {1}";
+$l['every_week'] = "Se répète chaque semaine";
+$l['every_x_weeks_on_days'] = "Se répète toutes {1} semaines le<br />{2}";
+$l['every_x_weeks'] = "Se répète toutes les {1} semaines";
+$l['every_month_on_day'] = "Se répète le jour {1}<br />de chaque mois";
+$l['every_x_months_on_day'] = "Se répète le jour {1}<br />tous les {2} mois";
+$l['every_month_on_weekday'] = "Se répète le {1} {2}<br />de chaque mois";
+$l['every_x_months_on_weekday'] = "Se répète le {1} {2}<br />tous les {3} mois";
+$l['weekday_occurance_1'] = "premier";
+$l['weekday_occurance_2'] = "deuxième";
+$l['weekday_occurance_3'] = "troisième";
+$l['weekday_occurance_4'] = "quatrième";
+$l['weekday_occurance_last'] = "dernier";
+$l['every_year_on_day'] = "Se répète chaque année le {1} {2}";
+$l['every_x_years_on_day'] = "Se répète tous les {3} ans le {1} {2}";
+$l['every_year_on_weekday'] = "Se répète le {1} {2} en {3} chaque année";
+$l['every_x_year_on_weekday'] = "Se répète le {1} {2} en {3} tous les {4} ans";
+$l['delete_event'] = "Supprimer l’événement";
+$l['delete_q'] = "Supprimer ?";
+$l['delete_1'] = "Pour supprimer cet événement, cochez la case à gauche puis cliquez sur le bouton à droite.";
+$l['delete_2'] = "<strong>Attention :</strong> Cette action est irréversible.";
+$l['delete_now'] = "Supprimer";
+$l['delete_no_checkbox'] = "L’événement n’a pas été supprimé car vous n’avez pas coché la case associée.";
+$l['jump_to_calendar'] = "Aller au calendrier";
+$l['select_calendar'] = "Calendrier";
+$l['type_single'] = "Événement unique";
+$l['type_ranged'] = "Événement prolongé ou récurrent";
+$l['enter_time'] = "Heure";
+$l['start_time'] = "Début";
+$l['end_time'] = "Fin";
+$l['timezone'] = "Fuseau horaire";
+$l['ignore_timezone'] = "<strong>Ignorer le fuseau horaire :</strong> Cet événement utilisera le fuseau horaire du visiteur.";
+$l['repeats'] = "Répétition";
+$l['does_not_repeat'] = "Aucune";
+$l['repeats_daily'] = "Quotidienne";
+$l['repeats_weekdays'] = "Du lundi au vendredi";
+$l['repeats_weekly'] = "Hebdomadaire";
+$l['repeats_every'] = "Se répète tous/toutes les";
+$l['day_or_days'] = "jour(s)";
+$l['week_or_weeks_on'] = "semaine(s) le";
+$l['repeats_monthly'] = "Mensuelle";
+$l['repeats_yearly'] = "Annuelle";
+$l['repeats_on_day'] = "Se répète le jour";
+$l['of_every'] = "de chaque";
+$l['month_or_months'] = "mois";
+$l['repeats_on_the'] = "Se répète le";
+$l['day_of_every'] = "jour chaque";
+$l['repeats_on'] = "Se répète le";
+$l['every'] = "chaque";
+$l['year_or_years'] = "année(s)";
+$l['of'] = "de";
+$l['move_to_calendar'] = "Déplacer vers le calendrier";
+$l['weekly_overview'] = "Aperçu hebdomadaire";
+$l['through'] = "au";
+$l['previous_week'] = "Semaine précédente";
+$l['next_week'] = "Semaine suivante";
+$l['first'] = "premier";
+$l['second'] = "deuxième";
+$l['third'] = "troisième";
+$l['fourth'] = "quatrième";
+$l['last'] = "dernier";
+$l['all_day'] = "Toute la journée";
+$l['starts'] = "Commence ";
+$l['finishes'] = "Se termine ";
+
+$l['error_incorrectday'] = "Le jour que vous avez saisi semble incorrect. Veuillez réessayer.";
+$l['error_invalidevent'] = "L’événement spécifié est invalide ou n’existe pas.";
+$l['invalid_calendar'] = "Le calendrier spécifié n’existe pas. Êtes-vous sûr de visiter la bonne page ?";
+$l['redirect_eventdeleted'] = "L’événement a bien été supprimé.<br />Vous allez maintenant être redirigé vers le calendrier.";
+$l['redirect_eventupdated'] = "L’événement a bien été mis à jour.<br />Vous allez maintenant être redirigé vers celui-ci.";
+$l['redirect_eventadded'] = "L’événement a bien été créé.<br />Vous allez maintenant être redirigé vers celui-ci.";
+$l['redirect_eventadded_moderation'] = "L’événement a bien été créé mais il doit être approuvé par un modérateur avant d’être publié.<br />Vous allez maintenant être redirigé vers le calendrier.";
+$l['redirect_eventunapproved'] = "L’événement a bien été désapprouvé.<br />Vous allez maintenant être redirigé vers celui-ci.";
+$l['redirect_eventapproved'] = "L’événement a bien été approuvé.<br />Vous allez maintenant être redirigé vers celui-ci.";
+$l['redirect_eventmoved'] = "L’événement a bien été déplacé.<br />Vous allez maintenant être redirigé vers celui-ci.";
