3b1243c5ff15395f20063b48033b653179b6d720
Axel Fontana <axel.fontana@qualifio.com>
2025-09-24 12:19:03 +0200

    feat: add translation for datahandler-event

---
+++ french/datahandler_event.lang.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['eventdata_missing_name'] = 'Veuillez saisir un nom pour l’événement.';
+$l['eventdata_missing_description'] = 'Veuillez saisir une description pour l’événement.';
+
+$l['eventdata_invalid_start_date'] = "La date de début de l’événement que vous avez saisie est invalide. Vous devez indiquer le jour, le mois et l’année, et vérifier que le jour saisi est valide pour le mois concerné.";
+$l['eventdata_invalid_start_year'] = "Les événements ne peuvent être créés que dans les 5 prochaines années. Veuillez sélectionner une année de début raisonnable dans la liste.";
+$l['eventdata_invalid_start_month'] = "Le mois de début que vous avez saisi n’est pas valide. Veuillez entrer un mois de début valide.";
+
+$l['eventdata_invalid_end_date'] = "La date de fin de l’événement que vous avez saisie est invalide. Vous devez indiquer le jour, le mois et l’année, et vérifier que le jour saisi est valide pour le mois concerné.";
+$l['eventdata_invalid_end_year'] = "Les événements ne peuvent être créés que dans les 5 prochaines années. Veuillez sélectionner une année de fin raisonnable dans la liste.";
+$l['eventdata_invalid_end_month'] = "Le mois de fin que vous avez saisi n’est pas valide. Veuillez entrer un mois de fin valide.";
+$l['eventdata_invalid_end_day'] = "Le jour de fin que vous avez saisi n’est pas valide. Le jour sélectionné dépasse probablement le nombre de jours de ce mois.";
+
+$l['eventdata_cant_specify_one_time'] = "Si vous indiquez une heure de début pour l’événement, vous devez également indiquer une heure de fin.";
+$l['eventdata_start_time_invalid'] = "L’heure de début que vous avez saisie est invalide. Exemples valides : 12:00, 00:01.";
+$l['eventdata_end_time_invalid'] = "L’heure de fin que vous avez saisie est invalide. Exemples valides : 12:00, 00:01.";
+$l['eventdata_invalid_timezone'] = "Le fuseau horaire que vous avez sélectionné pour cet événement est invalide.";
+$l['eventdata_end_in_past'] = "La date ou l’heure de fin de votre événement est antérieure à la date ou l’heure de début.";
+
+$l['eventdata_only_ranged_events_repeat'] = "Seuls les événements prolongés (ayant une date de début et de fin) peuvent se répéter.";
+
+$l['eventdata_invalid_repeat_day_interval'] = "Vous avez saisi un intervalle de répétition en jours invalide.";
+$l['eventdata_invalid_repeat_week_interval'] = "Vous avez saisi un intervalle de répétition en semaines invalide.";
+$l['eventdata_invalid_repeat_weekly_days'] = "Vous n’avez sélectionné aucun jour de la semaine pour que cet événement se produise.";
+$l['eventdata_invalid_repeat_month_interval'] = "Vous avez saisi un intervalle de répétition en mois invalide.";
+$l['eventdata_invalid_repeat_year_interval'] = "Vous avez saisi un intervalle de répétition en années invalide.";
+$l['eventdata_event_wont_occur'] = "Veuillez ajuster les dates de début et de fin afin d’assurer une planification correcte de l’événement.";
+
+$l['eventdata_no_permission_private_event'] = "Vous ne disposez pas des permissions nécessaires pour publier des événements privés.";
