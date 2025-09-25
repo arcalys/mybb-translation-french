f681d16a3ce5f111bc50514926fd7ca126544f2b
Axel Fontana <axel.fontana@qualifio.com>
2025-09-25 15:51:47 +0200

    feat: add translation for xmlhttp

---
+++ french/xmlhttp.lang.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['no_new_subject'] = "Vous n’avez pas saisi de nouveau sujet.";
+$l['post_moderation'] = "Votre message est maintenant en attente de modération.";
+$l['thread_moderation'] = "Votre sujet est maintenant en attente de modération.";
+$l['post_doesnt_exist'] = "Le message spécifié n’existe pas.";
+$l['thread_doesnt_exist'] = "Le sujet spécifié n’existe pas.";
+$l['thread_closed_edit_subjects'] = "Ce sujet est verrouillé, vous ne pouvez pas modifier le titre.";
+$l['no_permission_edit_subject'] = "Vous n’avez pas la permission de modifier le titre de ce sujet.";
+$l['thread_closed_edit_message'] = "Ce sujet est verrouillé, vous ne pouvez pas modifier les messages.";
+$l['no_permission_edit_post'] = "Vous n’avez pas la permission de modifier ce message.";
+$l['edit_time_limit'] = "Vous ne pouvez modifier les messages que pendant {1} minutes après leur publication.";
+$l['postbit_edited'] = "Dernière modification de ce message : {1} par";
+$l['postbit_editreason'] = "Raison de la modification";
+$l['save_changes'] = "Enregistrer les modifications";
+$l['cancel_edit'] = "Annuler les modifications";
+$l['answer_valid_not_exists'] = "La question à laquelle vous essayez de répondre n’existe pas.";
+$l['captcha_not_exists'] = "L’image de vérification que vous essayez d’actualiser n’existe pas.";
+$l['captcha_valid_not_exists'] = "Le code de vérification que vous essayez de vérifier semble ne pas exister.";
+$l['captcha_does_not_match'] = "Le code de vérification que vous avez saisi est incorrect. Veuillez saisir le code tel qu’il apparaît dans l’image.";
+$l['captcha_matches'] = "Le code de vérification que vous avez saisi est correct.";
+$l['answer_does_not_match'] = "La réponse que vous avez saisie est incorrecte.";
+$l['banned_username'] = "Le nom d’utilisateur que vous avez saisi a été interdit par un administrateur.";
+$l['banned_characters_username'] = "Votre nom d’utilisateur contient un ou plusieurs caractères non autorisés.";
+$l['complex_password_fails'] = "Le mot de passe doit contenir une lettre majuscule, une lettre minuscule et un chiffre.";
+$l['username_taken'] = "{1} est déjà enregistré par un autre utilisateur.";
+$l['username_available'] = "{1} est disponible.";
+$l['invalid_username'] = "{1} n’est pas le nom d’utilisateur d’un membre enregistré.";
+$l['valid_username'] = "{1} est un référent valide.";
+$l['buddylist_error'] = "Il semble que vous n’ayez aucun ami dans votre liste. Veuillez en ajouter avant d’utiliser cette fonction.";
+$l['close'] = "Fermer";
+$l['select_buddies'] = "Sélectionner des amis";
+$l['select_buddies_desc'] = "Pour ajouter un ou plusieurs amis comme destinataires, sélectionnez-les ci-dessous et cliquez sur OK.";
+$l['selected_recipients'] = "Destinataires sélectionnés";
+$l['ok'] = "OK";
+$l['cancel'] = "Annuler";
+$l['online'] = "En ligne";
+$l['offline'] = "Hors ligne";
+$l['edited_post'] = "Message modifié";
+$l['usergroup'] = "Groupe";
