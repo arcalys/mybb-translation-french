15b8a9a7be30841c84c637c8f2f484916e5d67ca
Axel Fontana <axel.fontana@qualifio.com>
2025-09-25 10:33:59 +0200

    feat: add translation for datahandler-post

---
+++ french/datahandler_post.lang.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['postdata_invalid_user_id'] = "L’identifiant n’existe pas. Veuillez fournir un identifiant valide.";
+$l['postdata_firstpost_no_subject'] = "Veuillez saisir un titre pour ce sujet.";
+$l['postdata_missing_subject'] = "Veuillez saisir un sujet.";
+$l['postdata_missing_message'] = "Veuillez saisir un message.";
+$l['postdata_message_too_long'] = "Le message est trop long. Veuillez saisir un message de moins de {1} caractères (actuellement {2}).";
+$l['postdata_message_too_short'] = "Le message est trop court. Veuillez saisir un message de plus de {1} caractères.";
+$l['postdata_subject_too_long'] = "Le sujet est trop long. Veuillez saisir un sujet de moins de 85 caractères (actuellement {1}).";
+$l['postdata_banned_username'] = "Le nom d’utilisateur que vous avez saisi ne peut pas être utilisé. Veuillez en choisir un autre.";
+$l['postdata_bad_characters_username'] = "Le nom d’utilisateur que vous avez saisi contient des caractères non autorisés. Veuillez en choisir un autre.";
+$l['postdata_invalid_username_length'] = "La longueur du nom d’utilisateur est invalide. Veuillez saisir un nom d’utilisateur entre {1} et {2} caractères.";
+$l['postdata_username_exists'] = "Le nom d’utilisateur que vous avez saisi existe déjà. Veuillez en choisir un autre.";
+$l['postdata_post_flooding'] = "Vous essayez de publier un message trop rapidement après le précédent. Veuillez patienter encore {1} secondes.";
+$l['postdata_post_flooding_one_second'] = "Vous essayez de publier un message trop rapidement après le précédent. Veuillez patienter encore 1 seconde.";
+$l['postdata_too_many_images'] = "Votre message contient {1} images, alors que la limite d’images par message est de {2}. Veuillez réduire le nombre d’images.";
+$l['postdata_too_many_videos'] = "Votre message contient {1} vidéos, alors que la limite de vidéos par message est de {2}. Veuillez réduire le nombre de vidéos.";
+$l['postdata_invalid_prefix'] = "Le préfixe sélectionné est invalide. Veuillez sélectionner un préfixe valide.";
+$l['postdata_require_prefix'] = "Ce forum exige qu’un préfixe soit défini pour le sujet. Veuillez sélectionner un préfixe valide.";
+
+$l['thread_closed'] = "Sujet verrouillé";
+$l['thread_opened'] = "Sujet déverrouillé";
+$l['thread_stuck'] = "Sujet épinglé";
+$l['thread_unstuck'] = "Sujet désépinglé";
