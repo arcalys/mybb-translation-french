8b123fc9a60086df47139b633380f282cf2e9828
Axel Fontana <axel.fontana@qualifio.com>
2025-09-25 18:14:19 +0200

    feat: add translation for misc

---
+++ french/misc.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_helpdocs'] = "Documents d’aide";
+$l['nav_smilies'] = "Liste des émoticônes";
+$l['nav_syndication'] = "Syndication des derniers sujets (RSS)";
+
+$l['buddy_list'] = "Liste d’amis";
+$l['online'] = "En ligne";
+$l['online_none'] = "Vous n’avez aucun ami en ligne";
+$l['offline'] = "Hors ligne";
+$l['offline_none'] = "Vous n’avez aucun ami hors ligne";
+$l['delete_buddy'] = "X";
+$l['pm_buddy'] = "Envoyer un message privé";
+$l['last_active'] = "Dernière activité";
+$l['close'] = "Fermer";
+$l['no_buddies'] = "Votre liste d’amis est actuellement vide. Pour ajouter des utilisateurs à votre liste, utilisez le panneau de contrôle de l’utilisateur.";
+$l['help_docs'] = "Documents d’aide";
+
+$l['search_help_documents'] = "Rechercher dans les documents d’aide";
+$l['search_by_name'] = "Rechercher par nom";
+$l['search_by_document'] = "Rechercher par document";
+$l['enter_keywords'] = "Saisissez des mots-clés…";
+$l['search'] = "Recherche";
+$l['redirect_searchresults'] = "Votre recherche a été soumise. Vous allez maintenant être redirigé vers la liste des résultats.";
+$l['search_results'] = "Résultats de la recherche";
+$l['help_doc_results'] = "Résultats des documents d’aide";
+$l['document'] = "Document";
+$l['error_nosearchresults'] = "Votre recherche n’a retourné aucun résultat. Veuillez modifier vos critères et réessayer.";
+$l['no_help_results'] = "Aucun résultat n’a été trouvé avec les informations fournies.";
+$l['error_helpsearchdisabled'] = "La recherche dans les documents d’aide a été désactivée par un administrateur.";
+
+$l['smilies_listing'] = "Liste des émoticônes";
+$l['name'] = "Nom";
+$l['abbreviation'] = "Abréviation";
+$l['click_to_add'] = "Cliquez sur un émoticône pour l’insérer dans votre message";
+$l['close_window'] = "Fermer la fenêtre";
+$l['no_smilies'] = "Il n’y a actuellement aucun émoticône disponible.";
+
+$l['who_posted'] = "Auteur du message";
+$l['total_posts'] = "Nombre total de messages :";
+$l['user'] = "Utilisateur";
+$l['num_posts'] = "Nombre de messages";
+
+$l['forum_rules'] = "Règles du forum « {1} »";
+
+$l['error_invalid_limit'] = "La limite d’éléments du flux que vous avez entrée est invalide. Veuillez spécifier une limite valide.";
+
+$l['syndication'] = "Syndication des derniers sujets";
+$l['syndication_generated_url'] = "URL de syndication générée";
+$l['syndication_note'] = "Vous pouvez générer ci-dessous des liens vers des flux RSS spécifiques. Les liens peuvent être générés pour tous les forums, par forum ou pour un nombre précis de forums. Vous obtiendrez ensuite un lien à copier dans un lecteur RSS. <em><a href=\"https://fr.wikipedia.org/wiki/RSS\" target=\"_blank\" rel=\"noopener\">Qu’est-ce que RSS ?</a></em>";
+$l['syndication_forum'] = "Forum à syndiquer";
+$l['syndication_forum_desc'] = "Veuillez sélectionner un forum à droite. Utilisez la touche CTRL pour sélectionner plusieurs forums.";
+$l['syndication_version'] = "Version du flux";
+$l['syndication_version_desc'] = "Veuillez sélectionner la version du flux que vous souhaitez générer.";
+$l['syndication_version_json1'] = "JSON Feed 1";
+$l['syndication_version_atom1'] = "Atom 1.0";
+$l['syndication_version_rss2'] = "RSS 2.00 (par défaut)";
+$l['syndication_generate'] = "Générer l’URL de syndication";
+$l['syndication_limit'] = "Limite";
+$l['syndication_limit_desc'] = "Nombre de sujets à télécharger à la fois. La limite maximale est de 50.";
+$l['syndication_threads_time'] = "sujets à la fois";
+$l['syndicate_all_forums'] = "Syndiquer tous les forums";
+
+$l['redirect_markforumread'] = "Le forum sélectionné a été marqué comme lu.";
+$l['redirect_markforumsread'] = "Tous les forums ont été marqués comme lus.";
+$l['redirect_forumpasscleared'] = "Le mot de passe enregistré pour ce forum a été effacé.";
+$l['redirect_cookiescleared'] = "Tous les cookies ont été effacés.";
+
+$l['error_invalidforum'] = "Forum invalide";
+$l['error_invalidhelpdoc'] = "Le document d’aide spécifié n’existe pas.";
+$l['error_invalidsearch'] = "Une recherche invalide a été spécifiée. Veuillez revenir en arrière et réessayer.";
+$l['error_no_search_support'] = "Ce moteur de base de données ne supporte pas la recherche.";
+$l['error_searchflooding'] = "Vous ne pouvez effectuer qu’une recherche toutes les {1} secondes. Veuillez attendre encore {2} secondes avant de recommencer.";
+$l['error_searchflooding_1'] = "Vous ne pouvez effectuer qu’une recherche toutes les {1} secondes. Veuillez attendre encore 1 seconde avant de recommencer.";
+
+$l['dst_settings_updated'] = "Vos paramètres pour l’heure d’été ont été ajustés automatiquement.<br />Vous allez maintenant être redirigé vers l’index du forum.";
