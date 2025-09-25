27e68e551b97bdea9eb73706b8b54562a0f17d6e
Axel Fontana <axel.fontana@qualifio.com>
2025-09-25 07:55:29 +0200

    feat: add translation for polls

---
+++ french/polls.lang.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_postpoll'] = "Publier un sondage";
+$l['nav_editpoll'] = "Modifier le sondage";
+$l['nav_pollresults'] = "Résultats du sondage";
+
+$l['edit_poll'] = "Modifier le sondage";
+$l['delete_poll'] = "Supprimer le sondage";
+$l['delete_q'] = "Supprimer ?";
+$l['delete_note'] = "Pour supprimer ce sondage, cochez la case à gauche et cliquez sur le bouton à droite.";
+$l['delete_note2'] = "<strong>Attention :</strong> Une fois ce sondage supprimé, il ne pourra plus être restauré.";
+$l['poll_details'] = "Détails du sondage";
+$l['question'] = "Question:";
+$l['num_options'] = "Nombre de réponses :";
+$l['max_options'] = "Maximum :";
+$l['poll_options'] = "Réponses";
+$l['update_options'] = "Mettre à jour les réponses";
+$l['poll_options_note'] = "Les réponses du sondage devraient être courtes et concises.";
+$l['poll_settings'] = "Paramètres du sondage";
+$l['option_multiple'] = "<strong>Autoriser le choix multiple :</strong> Les utilisateurs peuvent voter pour plusieurs options.";
+$l['option_multiple_maxoptions'] = "Nombre maximum d’options par utilisateur (0 pour illimité) :";
+$l['option_public'] = "<strong>Sondage public :</strong> Permet aux utilisateurs de voir qui a voté pour quelle(s) option(s).";
+$l['option_closed'] = "<strong>Sondage fermé :</strong> Si coché, le sondage sera fermé aux votes.";
+$l['poll_timeout'] = "Durée du sondage:";
+$l['timeout_note'] = "Nombre de jour(s) pendant lesquels les utilisateurs peuvent participer à ce sondage. (Laissez cette valeur à 0 si vous ne souhaitez pas limiter la durée du sondage.)";
+$l['days_after'] = "jours après :";
+$l['update_poll'] = "Mettre à jour le sondage";
+$l['option'] = "Option";
+$l['votes'] = "votes";
+$l['post_new_poll'] = "Publier un nouveau sondage";
+$l['days'] = "jour(s)";
+$l['poll_results'] = "Résultats du sondage";
+$l['poll_total'] = "Nombre de votes :";
+$l['you_voted'] = "Vous avez voté pour cette réponse.";
+
+$l['redirect_pollposted'] = "Votre sondage a bien été publié.<br />Vous allez maintenant être redirigé vers le sujet.";
+$l['redirect_pollpostedmoderated'] = "Votre sondage a bien été publié, mais le sujet est toujours en attente de modération.<br />Vous allez maintenant être redirigé vers le forum.";
+$l['redirect_pollupdated'] = "Le sondage a bien été mis à jour.<br />Vous allez maintenant être redirigé vers le sujet.";
+$l['redirect_votethanks'] = "Votre vote a bien été enregistré. <br />Vous allez maintenant être redirigé vers le sujet.";
+$l['redirect_unvoted'] = "Votre vote a bien été annulé.<br />Vous allez maintenant être redirigé vers le sujet.";
+$l['redirect_polldeleted'] = "Le sondage a bien été supprimé.<br />Vous allez maintenant être redirigé vers le sujet.";
+
+$l['error_polloptiontoolong'] = "Certaines de vos réponses sont trop longues. Merci de les raccourcir.";
+$l['error_polloptionsequence'] = "Certaines de vos réponses contiennent une séquence interdite (<strong>||~|~||</strong>). Merci de les corriger.";
+$l['error_noquestionoptions'] = "Vous n’avez pas saisi de question pour votre sondage ou le nombre de réponses est insuffisant. Un sondage doit comporter au moins 2 réponses.<br />Veuillez corriger cela et réessayer.";
+$l['error_pollalready'] = "Ce sujet contient déjà un sondage !";
+$l['error_nopolloptions'] = "L’option de sondage spécifiée est invalide ou n’existe pas.";
+$l['error_maxpolloptions'] = "Vous avez voté pour trop d’options. Vous ne pouvez voter que pour {1} options.<br />Veuillez revenir en arrière et réessayer.";
+$l['error_alreadyvoted'] = "Vous avez déjà participé à ce sondage.";
+$l['error_notvoted'] = "Vous n’avez pas encore participé à ce sondage.";
+$l['error_invalidpoll'] = "Le sondage spécifié est invalide ou n’existe pas.";
+$l['error_pollclosed'] = "Vous ne pouvez pas voter dans un sondage qui a été fermé.";
+$l['poll_time_limit'] = "Vus ne pouvez pas ajouter de sondage à votre sujet. Les sondages ne peuvent être ajoutés que dans les {1} heures suivant la publication.";
+
+$l['poll_deleted'] = "Sondage supprimé";
+$l['poll_edited'] = "Sondage modifié";
