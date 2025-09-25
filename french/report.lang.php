9f1f6011c3baa2b8ea3b518f62ed75bcc3f439da
Axel Fontana <axel.fontana@qualifio.com>
2025-09-25 17:44:15 +0200

    feat: add translation for report

---
+++ french/report.lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['report_reason'] = "Raison du signalement :";
+$l['report_to_mod'] = "Signaler ce contenu à un modérateur";
+$l['close_window'] = "Fermer la fenêtre";
+
+// Types de contenu
+$l['report_content'] = "Signaler un contenu";
+$l['report_reason_post'] = "Signaler un message";
+$l['report_reason_profile'] = "Signaler un profil";
+$l['report_reason_reputation'] = "Signaler une réputation";
+
+// Raisons du signalement
+$l['report_reason_rules'] = "Enfreint les règles du forum";
+$l['report_reason_bad'] = "Contenu inapproprié";
+$l['report_reason_spam'] = "Contenu indésirable (spam)";
+$l['report_reason_wrong'] = "Mauvaise section";
+$l['report_reason_other'] = "Autre";
+
+$l['success_report_voted'] = "Le contenu a bien été signalé. Un membre de l’équipe du forum examinera votre demande sous peu.";
+
+$l['error_report_length'] = "Veuillez fournir une raison détaillée pour votre signalement.";
+$l['error_invalid_report'] = "Ce contenu ({1}) n’existe pas ou ne peut pas être signalé.";
+$l['error_invalid_report_reason'] = "La raison sélectionnée est invalide.";
+$l['error_comment_required'] = "Veuillez saisir un commentaire pour ce signalement.";
+$l['error_report_duplicate'] = "Ce contenu a déjà été signalé par un autre membre.<br />Vous pouvez toutefois le signaler de nouveau.";
+$l['report_reason_other_description'] = "Veuillez fournir plus d’informations sur la raison pour laquelle vous signalez ce contenu.";
