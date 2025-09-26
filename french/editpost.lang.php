1dceddb1a92ee8a3ca73374bcad22a477afe6f6d
Axel Fontana <axel.fontana@qualifio.com>
2025-09-26 10:27:09 +0200

    feat: add translation for editpost

---
+++ french/editpost.lang.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_editpost'] = "Modifier un message";
+$l['edit_post'] = "Modifier un message";
+$l['delete_post'] = "Supprimer le message";
+$l['delete_q'] = "Supprimer ?";
+$l['delete_1'] = "Pour supprimer ce message, cochez la case à gauche puis cliquez sur le bouton à droite.";
+$l['delete_2'] = "<strong>Attention :</strong> Si ce message est le premier d’un sujet, cela entraînera la suppression de l’intégralité du sujet.";
+$l['subject'] = "Sujet";
+$l['your_message'] = "Votre message";
+$l['post_options'] = "Options";
+$l['editreason'] = "Raison de la modification";
+$l['options_sig'] = "<strong>Signature :</strong> inclure votre signature. (utilisateurs enregistrés uniquement)";
+$l['options_emailnotify'] = "<strong>Notification par courriel :</strong> recevez un courriel à chaque nouvelle réponse. (utilisateurs enregistrés uniquement)";
+$l['options_disablesmilies'] = "<strong>Désactiver les émoticônes :</strong> empêcher l’affichage des émoticônes dans ce message.";
+$l['preview_post'] = "Prévisualiser";
+$l['update_post'] = "Enregistrer";
+$l['add_poll'] = "Ajouter un sondage";
+$l['poll'] = "Sondage";
+$l['poll_desc'] = "Vous pouvez, si vous le souhaitez, ajouter un sondage à ce sujet.";
+$l['poll_check'] = "Je souhaite créer un sondage";
+$l['num_options'] = "Nombre de réponses :";
+$l['max_options'] = "(Maximum : {1})";
+$l['delete_now'] = "Supprimer";
+$l['edit_time_limit'] = "Vous ne pouvez pas modifier votre message. L’administrateur a défini que les messages ne peuvent être modifiés que dans les {1} minutes suivant leur publication.";
+$l['no_prefix'] = "Pas de préfixe";
+
+$l['redirect_nodelete'] = "Le message n’a pas été supprimé car vous n’avez pas coché la case « Supprimer ».";
+$l['redirect_norestore'] = "Le message n’a pas été restauré car vous n’avez pas coché la case « Restaurer ».";
+$l['redirect_postedited'] = "Le message a bien été modifié.<br />";
+$l['redirect_postedited_redirect'] = "Vous allez maintenant être redirigé vers le sujet.";
+$l['redirect_postedited_poll'] = "Le message a bien été modifié.<br />Comme vous avez choisi d’ajouter un sondage, vous allez maintenant être redirigé vers la page de configuration du sondage.";
+$l['error_invalidpost'] = "L’adresse que vous avez suivie est invalide. Veuillez vérifier que le message spécifié existe et réessayer.";
+$l['redirect_threaddeleted'] = "Le sujet a bien été supprimé.<br />Vous allez maintenant être redirigé vers le forum.";
+$l['redirect_postdeleted'] = "Le message a bien été supprimé.<br />Vous allez maintenant être redirigé vers le sujet.";
+$l['redirect_threadrestored'] = "Le sujet a bien été restauré.<br />Vous allez maintenant être redirigé vers le forum.";
+$l['redirect_postrestored'] = "Le message a bien été restauré.<br />Vous allez maintenant être redirigé vers le sujet.";
+$l['redirect_threadclosed'] = "Vous ne pouvez pas modifier les messages existants dans ce sujet car il a été verrouillé par un modérateur.";
+$l['redirect_post_moderation'] = "L’administrateur a spécifié que toutes les modifications de messages nécessitent une approbation. Vous allez maintenant être redirigé vers le sujet.";
+$l['redirect_thread_moderation'] = "L’administrateur a spécifié que toutes les modifications de sujets nécessitent une approbation. Vous allez maintenant être redirigé vers l’index du forum.";
+$l['error_already_delete'] = "Ce message a déjà été supprimé.";
+
+$l['thread_deleted'] = "Sujet supprimé définitivement";
+$l['post_deleted'] = "Message supprimé définitivement";
+$l['thread_soft_deleted'] = "Sujet supprimé (soft delete)";
+$l['post_soft_deleted'] = "Message supprimé (soft delete)";
+$l['thread_restored'] = "Sujet restauré";
+$l['post_restored'] = "Message restauré";
+
+$l['error_already_deleted'] = "Le message sélectionné a déjà été supprimé.";
