811debc64c35ae63fcd2e467a7bb30792c102742
Axel Fontana <axel.fontana@qualifio.com>
2025-09-26 10:23:28 +0200

    feat: add translation for datahandler-warnings

---
+++ french/datahandler_warnings.lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * MyBB 1.9 Pack de langue française
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, Tous droits réservés
+ *
+ */
+
+$l['warnings_reached_max_warnings_day'] = "Vous ne pouvez pas avertir d’autres membres car vous avez atteint la limite d’avertissements autorisés par jour.<br /><br />Le nombre maximum d’avertissements que vous pouvez donner par jour est de {1}.";
+$l['warnings_error_invalid_user'] = "L’utilisateur sélectionné n’existe pas.";
+$l['warnings_error_invalid_post'] = "Le message sélectionné n’existe pas.";
+$l['warnings_error_cannot_warn_self'] = "Vous ne pouvez pas augmenter votre propre niveau d’avertissement.";
+$l['warnings_error_user_reached_max_warning'] = "Cet utilisateur ne peut pas être averti car il a déjà atteint le niveau d’avertissement maximal.";
+$l['warnings_error_no_note'] = "Vous n’avez pas saisi de note administrative pour cet avertissement.";
+$l['warnings_error_invalid_type'] = "Vous avez sélectionné un type d’avertissement invalide.";
+$l['warnings_error_cant_custom_warn'] = "Vous n’avez pas la permission de donner des avertissements personnalisés aux utilisateurs.";
+$l['warnings_error_no_custom_reason'] = "Vous n’avez pas saisi de raison pour votre avertissement personnalisé.";
+$l['warnings_error_invalid_custom_points'] = "Vous n’avez pas saisi un nombre de points valide à ajouter au niveau d’avertissement de cet utilisateur. Vous devez entrer une valeur numérique supérieure à 0 et ne dépassant pas {1}.";
+$l['warnings_error_invalid_expires_period'] = "Vous avez saisi un type d’expiration invalide.";
