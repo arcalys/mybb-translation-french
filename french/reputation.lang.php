38a669a8e590ea8e0b866d0d6ee56df3d02357c7
Axel Fontana <axel.fontana@qualifio.com>
2025-09-27 23:27:00 +0200

    feat: add translation for reputation

---
+++ french/reputation.lang.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_reputation'] = "Rapport de réputation";
+$l['nav_profile'] = "Profil de {1}";
+$l['reputation_disabled'] = "La fonction de réputation a été désactivée par un administrateur.";
+
+$l['reputation'] = "Réputation";
+$l['error'] = "Erreur";
+$l['add_no_uid'] = "Vous n’avez pas spécifié d’utilisateur à évaluer.";
+$l['add_no_permission'] = "Vous n’avez pas la permission de donner des évaluations de réputation aux utilisateurs.";
+$l['add_disabled'] = "Vous ne pouvez pas ajouter de réputation aux utilisateurs de ce groupe.";
+$l['add_yours'] = "Vous ne pouvez pas ajouter à votre propre réputation.";
+$l['add_invalidpower'] = "Vous avez sélectionné une évaluation invalide dans la liste.";
+$l['add_maxperday'] = "Vous avez déjà donné autant d’évaluations de réputation que vous le pouvez aujourd’hui.";
+$l['add_maxperuser'] = "Vous avez déjà donné à cet utilisateur autant d’évaluations que possible aujourd’hui.";
+$l['add_maxperthread'] = "Vous avez déjà donné à cet utilisateur autant d’évaluations (dans ce sujet) que possible aujourd’hui.";
+$l['add_no_comment'] = "Vous devez saisir un commentaire d’au moins {1} caractères pour évaluer cet utilisateur.";
+$l['add_toolong'] = "Le commentaire ne doit pas dépasser {1} caractères.";
+$l['add_negative_disabled'] = "L’administrateur a désactivé les évaluations négatives.";
+$l['add_neutral_disabled'] = "L’administrateur a désactivé les évaluations neutres.";
+$l['add_positive_disabled'] = "L’administrateur a désactivé les évaluations positives.";
+$l['add_all_rep_disabled'] = "L’administrateur a désactivé tous les types d’évaluations. Vous ne pouvez pas évaluer cet utilisateur.";
+$l['reputation_report'] = "Rapport de réputation pour {1}";
+$l['reputation_members'] = "Réputation provenant des utilisateurs";
+$l['reputation_posts'] = "Réputation provenant des messages";
+$l['summary'] = "Résumé";
+$l['total_reputation'] = "Réputation totale";
+$l['post_reputation'] = "Réputation du message";
+$l['positive_count'] = "Positives";
+$l['neutral_count'] = "Neutres";
+$l['negative_count'] = "Négatives";
+$l['last_week'] = "La semaine dernière";
+$l['last_month'] = "Le mois dernier";
+$l['last_6months'] = "Les 6 derniers mois";
+$l['all_time'] = "Depuis le début";
+$l['comments'] = "Commentaires";
+$l['close_window'] = "Fermer la fenêtre";
+$l['add_reputation_vote'] = "Évaluer {1}";
+$l['add_reputation_to_post'] = "Cette évaluation concerne le message de {1}.<br />";
+$l['neg_rep_disabled'] = "<span class=\"smalltext\">* - <em>Les évaluations négatives sont actuellement désactivées</em></span>";
+$l['pos_rep_disabled'] = "<span class=\"smalltext\">* - <em>Les évaluations positives sont actuellement désactivées</em></span>";
+$l['neu_rep_disabled'] = "<span class=\"smalltext\">* - <em>Les évaluations neutres sont actuellement désactivées</em></span>";
+$l['rating'] = "Évaluation";
+$l['comment'] = "Commentaire";
+$l['no_comment_needed'] = "Vous ajoutez une évaluation visible dans le profil de l’utilisateur. Aucun commentaire n’est nécessaire, mais si vous souhaitez en laisser un, veuillez le saisir ci-dessous.<br />";
+
+$l['no_comment'] = "Aucun commentaire";
+$l['vote_added'] = "Évaluation ajoutée";
+$l['vote_updated'] = "Évaluation mise à jour";
+$l['vote_deleted'] = "Évaluation supprimée";
+$l['vote_added_message'] = "Votre évaluation a bien été ajoutée pour cet utilisateur.";
+$l['vote_updated_message'] = "Votre évaluation a bien été mise à jour pour cet utilisateur.";
+$l['vote_deleted_message'] = "L’évaluation a été supprimée pour cet utilisateur.";
+$l['update_reputation_vote'] = "Modifier l’évaluation pour {1}";
+$l['positive'] = "Positive";
+$l['negative'] = "Négative";
+$l['neutral'] = "Neutre";
+$l['user_comments'] = "Votre commentaire concernant {1} :";
+$l['add_vote'] = "Publier l’évaluation";
+$l['update_vote'] = "Mettre à jour l’évaluation";
+$l['delete_vote'] = "Supprimer";
+$l['report_vote'] = "Signaler";
+$l['power_positive'] = "Positif ({1})";
+$l['power_neutral'] = "Neutre";
+$l['power_negative'] = "Négatif ({1})";
+$l['show_all'] = "Afficher : toutes les évaluations";
+$l['show_positive'] = "Afficher : évaluations positives";
+$l['show_neutral'] = "Afficher : évaluations neutres";
+$l['show_negative'] = "Afficher : évaluations négatives";
+$l['sort_updated'] = "Trier par : dernière mise à jour";
+$l['sort_username'] = "Trier par : nom d’utilisateur";
+$l['sort_ratings'] = "Trier les évaluations";
+$l['last_updated'] = "Dernière mise à jour : {1}";
+$l['postrep_given'] = "Évaluation donnée pour le <a href=\"{1}\">message de {2}</a> {3}<br />";
+$l['postrep_given_nolink'] = "Évaluation donnée pour le message de {1}<br />";
+$l['postrep_given_thread'] = "dans <a href=\"{1}\">{2}</a>";
+$l['no_reputation_votes'] = "Cet utilisateur n’a actuellement aucune évaluation correspondant aux critères ci-dessous.";
+$l['delete_reputation_confirm'] = "Êtes-vous sûr de vouloir supprimer cette évaluation ?";
+$l['delete_reputation_log'] = "Évaluation supprimée par {1} (UID : {2})";
+$l['reputations_disabled_group'] = "Le système de réputation est désactivé pour les utilisateurs de ce groupe.";
+$l['rate_user'] = "Évaluer cet utilisateur";
