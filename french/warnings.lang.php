ddea1d1429f5dc01dbdb32f55c0a5feb8d880a71
Axel Fontana <axel.fontana@qualifio.com>
2025-09-28 00:08:25 +0200

    feat: add translation for warnings

---
+++ french/warnings.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_profile'] = "Profil de {1}";
+$l['nav_warning_log'] = "Journal d’avertissements";
+$l['nav_add_warning'] = "Avertir un utilisateur";
+$l['nav_view_warning'] = "Détails de l’avertissement";
+
+$l['warning_for_post'] = "Message :";
+$l['already_expired'] = "Expiré";
+$l['details_username'] = "Nom d’utilisateur";
+$l['warning_active'] = "Actif";
+$l['warning_revoked'] = "Révoqué";
+$l['warning_log'] = "Journal d’avertissements";
+$l['warning'] = "Avertissement";
+$l['issued_by'] = "Donné par";
+$l['date_issued'] = "Date de l’avertissement";
+$l['expiry_date'] = "Expiration";
+$l['active_warnings'] = "Avertissements actifs";
+$l['expired_warnings'] = "Avertissements expirés";
+$l['warning_points'] = "({1} points)";
+$l['no_warnings'] = "Cet utilisateur n’a reçu aucun avertissement ou ils ont tous été supprimés.";
+$l['warn_user'] = "Avertir l’utilisateur";
+$l['post'] = "Message :";
+$l['warning_note'] = "Notes concernant l’avertissement";
+$l['details_warning_note'] = "Notes concernant l’avertissement";
+$l['warning_type'] = "Type d’avertissement";
+$l['custom'] = "Raison personnalisée";
+$l['reason'] = "Raison";
+$l['points'] = "Nombre de points";
+$l['details_reason'] = "Raison";
+$l['warn_user_desc'] = "Vous pouvez augmenter le niveau d’avertissement de cet utilisateur s’il a enfreint une ou plusieurs règles.";
+$l['send_pm'] = "Notifier l’utilisateur";
+$l['send_user_warning_pm'] = "Envoyer un message privé à l’utilisateur pour l’informer de cet avertissement.";
+$l['send_pm_subject'] = "Sujet";
+$l['warning_pm_subject'] = "Vous avez reçu un avertissement";
+$l['send_pm_message'] = "Message";
+$l['warning_pm_message'] = "Cher {1},
+
+Vous avez reçu un avertissement de la part d’un membre de l’équipe.
+--
+
+--";
+$l['send_pm_options'] = "Options";
+$l['send_pm_options_anonymous'] = "<strong>Message privé anonyme</strong> : envoyer ce message privé de manière anonyme.";
+$l['expiration_never'] = "définitivement";
+$l['expiration_hours'] = "heure(s)";
+$l['expiration_days'] = "jour(s)";
+$l['expiration_weeks'] = "semaine(s)";
+$l['expiration_months'] = "mois";
+$l['redirect_warned_banned'] = "<br />L’utilisateur a également été déplacé dans le groupe {1} {2}.";
+$l['redirect_warned_suspended'] = "<br />Les privilèges de publication de cet utilisateur ont été suspendus {1}.";
+$l['redirect_warned_moderate'] = "<br />Tous les messages de cet utilisateur devront désormais être modérés {1}.";
+$l['redirect_warned_pmerror'] = "<br />Veuillez noter que le message privé n’a pas pu être envoyé.";
+$l['redirect_warned'] = "Le niveau d’avertissement de {1} a été augmenté à {2}%{3}.<br />Vous allez maintenant être redirigé vers la page précédente.";
+$l['error_warning_system_disabled'] = "Vous ne pouvez pas utiliser le système d’avertissements car il a été désactivé par l’administrateur du forum.";
+$l['error_cant_warn_group'] = "Vous n’avez pas la permission d’avertir les utilisateurs de ce groupe.";
+$l['error_invalid_user'] = "L’utilisateur sélectionné n’existe pas.";
+$l['details'] = "Détails";
+$l['view'] = "Voir";
+$l['current_warning_level'] = "Niveau d’avertissement actuel : <strong>{1}%</strong> ({2}/{3})";
+$l['warning_details'] = "Détails de l’avertissement";
+$l['revoke_warning'] = "Révoquer cet avertissement";
+$l['revoke_warning_desc'] = "Pour révoquer cet avertissement, veuillez entrer une raison ci-dessous. Cela ne supprimera pas les bannissements ou suspensions déjà appliqués par cet avertissement.";
+$l['warning_is_revoked'] = "Cet avertissement a été révoqué.";
+$l['revoked_by'] = "Révoqué par";
+$l['date_revoked'] = "Date de révocation";
+$l['warning_already_revoked'] = "Cet avertissement a déjà été révoqué.";
+$l['no_revoke_reason'] = "Vous n’avez pas indiqué la raison pour laquelle vous souhaitez révoquer cet avertissement.";
+$l['redirect_warning_revoked'] = "L’avertissement a bien été révoqué et le niveau d’avertissement de l’utilisateur a été réduit.<br />Vous allez maintenant être redirigé vers le détail de l’avertissement.";
+$l['result'] = "Résultat :";
+$l['result_banned'] = "L’utilisateur sera déplacé dans le groupe banni ({1}) {2}";
+$l['result_suspended'] = "Les privilèges de publication seront suspendus {1}";
+$l['result_moderated'] = "Les messages seront modérés {1}";
+$l['result_period'] = "pendant {1} {2}";
+$l['result_period_perm'] = "de manière permanente";
+$l['hour_or_hours'] = "heure(s)";
+$l['day_or_days'] = "jour(s)";
+$l['week_or_weeks'] = "semaine(s)";
+$l['month_or_months'] = "mois";
+$l['expires'] = "Expiration";
+$l['new_warning_level'] = "Nouveau niveau d’avertissement :";
+$l['error_cant_warn_user'] = "Vous n’avez pas la permission d’avertir cet utilisateur.";
+$l['existing_post_warnings'] = "Avertissements existants pour ce message";
