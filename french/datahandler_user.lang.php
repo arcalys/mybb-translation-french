c3b0a9d91936c45c387e19d7e48778e0d2b98c3d
Axel Fontana <axel.fontana@qualifio.com>
2025-09-27 16:27:09 +0200

    feat: add translation for datahandler-user

---
+++ french/datahandler_user.lang.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['userdata_missing_username'] = "Veuillez saisir un nom d’utilisateur.";
+$l['userdata_banned_username'] = "Le nom d’utilisateur que vous avez saisi ne peut pas être utilisé. Veuillez en saisir un autre.";
+$l['userdata_bad_characters_username'] = "Le nom d’utilisateur que vous avez saisi contient des caractères interdits. Veuillez en saisir un autre.";
+$l['userdata_invalid_username_length'] = "Le nom d’utilisateur que vous avez saisi a une longueur invalide. Veuillez saisir un nom d’utilisateur compris entre {1} et {2} caractères.";
+$l['userdata_invalid_usertitle_length'] = "Le titre personnalisé que vous avez saisi a une longueur invalide. Veuillez saisir un titre d’au maximum {1} caractères.";
+$l['userdata_username_exists'] = "Le nom d’utilisateur que vous avez saisi est déjà utilisé. Veuillez en saisir un autre.";
+
+$l['userdata_invalid_password_length'] = "Le mot de passe que vous avez saisi a une longueur invalide. Veuillez saisir un mot de passe compris entre {1} et {2} caractères.";
+$l['userdata_no_complex_characters'] = "Le mot de passe que vous avez saisi n'est pas assez complexe. Veuillez inclure au moins {1} caractères, une majuscule, une minuscule et un chiffre.";
+$l['userdata_passwords_dont_match'] = "Le mot de passe de confirmation que vous avez saisi ne correspond pas à l’original. Veuillez le confirmer correctement.";
+$l['userdata_bad_password_security'] = "Le mot de passe que vous avez saisi est similaire à votre nom d’utilisateur ou votre adresse électronique. Veuillez choisir un mot de passe plus sûr.";
+
+$l['userdata_missing_email'] = "Veuillez saisir une adresse électronique.";
+$l['userdata_invalid_email_format'] = "L’adresse électronique que vous avez saisie est invalide. Veuillez saisir une adresse valide.";
+$l['userdata_emails_dont_match'] = "L’adresse électronique de confirmation que vous avez saisie ne correspond pas à l’original. Veuillez la confirmer correctement.";
+$l['userdata_banned_email'] = "L’adresse électronique que vous avez saisie ne peut pas être utilisée. Veuillez en saisir une autre.";
+$l['userdata_email_already_in_use'] = "L’adresse électronique que vous avez saisie est déjà utilisée. Veuillez en saisir une autre.";
+
+$l['userdata_dob_required'] = "Veuillez saisir votre date de naissance. Vous aurez la possibilité de masquer votre âge et votre date de naissance dans les options de votre profil.";
+$l['userdata_invalid_website'] = "L'adresse du site web saisie est invalide. Veuillez entrer une adresse valide ou laisser ce champ vide.";
+$l['userdata_invalid_birthday'] = "La date de naissance saisie est invalide. Veuillez entrer une date valide ou laisser ce champ vide.";
+$l['userdata_invalid_birthday_coppa'] = "Pour vérifier votre âge, indiquez votre année de naissance. Vous aurez la possibilité de masquer votre âge et votre date de naissance dans les options de votre profil.";
+$l['userdata_invalid_birthday_coppa2'] = "Vous devez avoir au moins 13 ans pour être membre de ce forum. Veuillez contacter un administrateur.";
+$l['userdata_invalid_birthday_privacy'] = "Veuillez choisir une option valide pour la confidentialité de votre date de naissance.";
+$l['userdata_conflicted_birthday_privacy'] = "Veuillez renseigner votre année de naissance si vous choisissez de n’afficher que votre âge.";
+$l['userdata_invalid_referrer'] = "Veuillez saisir un parrain existant ou laisser ce champ vide.";
+$l['userdata_invalid_language'] = "Veuillez choisir une langue valide.";
+$l['userdata_invalid_style'] = "Veuillez choisir un thème valide.";
+$l['userdata_away_too_long'] = "La raison d'absence est trop longue. Ce champ ne peut pas dépasser 200 caractères. Veuillez supprimer {1} caractère(s).";
+$l['userdata_missing_returndate'] = "Veuillez sélectionner le jour, le mois et l’année pour la date de retour.";
+$l['userdata_missing_required_profile_field'] = "Vous n’avez pas renseigné le champ « {1} ». Veuillez le remplir ou sélectionner une valeur.";
+$l['userdata_bad_profile_field_value'] = "La valeur saisie pour le champ « {1} » n’est pas valide. Veuillez entrer une valeur correcte.";
+$l['userdata_bad_profile_field_values'] = "Veuillez sélectionner une option valide pour le champ « {1} ».";
+$l['userdata_max_limit_reached'] = "Le champ « {1} » dépasse le nombre maximal de caractères ({2}). Veuillez le remplir correctement.";
+$l['userdata_invalid_checkfield'] = "Ce formulaire semble avoir été soumis par un spammeur. S’il s’agit d’une erreur, contactez l’administrateur.";
+$l['userdata_invalid_postnum'] = "Le nombre de messages saisi n’est pas valide. Veuillez entrer un nombre correct ou laisser le champ vide.";
+$l['userdata_invalid_threadnum'] = "Le nombre de sujets saisi n’est pas valide. Veuillez entrer un nombre correct ou laisser le champ vide.";
+
+$l['userdata_too_many_sig_images'] = "Vous ne pouvez pas mettre à jour votre signature car elle contient trop d’images. Veuillez en retirer pour continuer.";
+$l['userdata_too_many_sig_images2'] = "<strong>Attention :</strong> Le nombre maximal d’images autorisées dans une signature est {1}.";
+$l['userdata_sig_too_long'] = "Vous ne pouvez pas mettre à jour votre signature car elle est trop longue. La longueur maximale d’une signature est de {1} caractères.";
+$l['userdata_sig_remove_chars_plural'] = "Veuillez supprimer {1} caractères et réessayer.";
+$l['userdata_sig_remove_chars_singular'] = "Veuillez supprimer 1 caractère et réessayer.";
+
+$l['userdata_invalid_username'] = "Veuillez saisir un nom d’utilisateur existant.";
+$l['userdata_no_perm_to_ban'] = "Vous n’avez pas la permission de bannir cet utilisateur.";
+$l['userdata_already_banned'] = "Cet utilisateur appartient déjà à un groupe banni et ne peut pas être ajouté à un autre.";
+$l['userdata_no_ban_group'] = "Vous n’avez pas sélectionné un groupe valide vers lequel déplacer cet utilisateur.";
+$l['userdata_ban_self'] = "Vous ne pouvez pas vous bannir vous-même.";
+$l['userdata_no_reason'] = "Vous n’avez pas saisi de raison pour bannir cet utilisateur.";
+$l['userdata_reason_too_long'] = "La raison que vous avez saisie est trop longue. La longueur maximale d’une raison de bannissement est de 255 caractères.";
