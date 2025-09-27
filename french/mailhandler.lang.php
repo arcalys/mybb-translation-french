6de2479e91c8774ceade9817822f4196f8b12e26
Axel Fontana <axel.fontana@qualifio.com>
2025-09-27 16:29:53 +0200

    feat: add translation for mailhandler

---
+++ french/mailhandler.lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['error_no_connection'] = "Une erreur est survenue lors de l’établissement de la connexion avec le serveur : ";
+$l['error_no_message'] = "Veuillez saisir un message.";
+$l['error_no_subject'] = "Veuillez saisir un sujet.";
+$l['error_no_recipient'] = "Veuillez saisir un destinataire.";
+$l['error_not_sent'] = "Une erreur est survenue lors de l’envoi du courriel via la fonction mail de PHP.";
+$l['error_status_missmatch'] = "Le statut renvoyé par le serveur ne correspond pas au résultat attendu, retour : ";
+$l['error_data_not_sent'] = "Les données n’ont pas pu être envoyées au serveur : ";
+
+$l['error_occurred'] = "Une ou plusieurs erreurs se sont produites. Veuillez corriger les erreurs suivantes avant de continuer.<br />";
