0decd61dbec0eccecc1c069314849da6d6f84418
Axel Fontana <axel.fontana@qualifio.com>
2025-09-27 19:26:38 +0200

    feat: add translation for search

---
+++ french/search.lang.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_search'] = "Recherche";
+$l['nav_results'] = "Résultats";
+
+$l['pages'] = "Pages:";
+$l['pages_last'] = "dernière";
+$l['search'] = "Recherche";
+$l['search_keyword'] = "Rechercher par mot-clé";
+$l['search_username'] = "Rechercher par nom d’utilisateur";
+$l['search_entire_post'] = "rechercher dans tout le message";
+$l['search_titles_only'] = "rechercher uniquement dans les titres";
+$l['match_username'] = "correspondance exacte du nom d’utilisateur";
+$l['search_forums'] = "Rechercher dans le(s) forum(s)";
+$l['search_all_forums'] = "Rechercher dans tous les forums";
+$l['search_options'] = "Options de recherche";
+$l['threads_at_least'] = "Trouver les sujets avec au moins";
+$l['threads_at_most'] = "Trouver les sujets avec au maximum";
+$l['find_anydate'] = "Trouver les messages de n'importe quelle date";
+$l['find_yesterday'] = "Trouver les messages d’hier";
+$l['find_lastweek'] = "Trouver les messages d’il y a une semaine";
+$l['find_2weeks'] = "Trouver les messages d’il y a 2 semaines";
+$l['find_month'] = "Trouver les messages d’il y a un mois";
+$l['find_3months'] = "Trouver les messages d’il y a 3 mois";
+$l['find_6months'] = "Trouver les messages d’il y a 6 mois";
+$l['find_year'] = "Trouver les messages d’il y a un an";
+$l['and_newer'] = "et plus récents";
+$l['and_older'] = "et plus anciens";
+$l['sorting_options'] = "Options de tri";
+$l['sort_by'] = "Trier les résultats par";
+$l['sort_lastpost'] = "Date du dernier message";
+$l['sort_author'] = "Auteur";
+$l['sort_forum'] = "Nom du forum";
+$l['sort_views'] = "Nombre de vues";
+$l['sort_replies'] = "Nombre de réponses";
+$l['sort_asc'] = "Croissant";
+$l['sort_desc'] = "Décroissant";
+$l['sort_order'] = "Ordre de tri";
+$l['asc'] = "croissant";
+$l['desc'] = "décroissant";
+$l['show_results_as'] = "Afficher les résultats comme";
+$l['show_results_threads'] = "Sujets";
+$l['show_results_posts'] = "Messages";
+$l['search_results'] = "Résultats de la recherche";
+$l['post'] = "Message";
+$l['author'] = "Auteur";
+$l['forum'] = "Forum";
+$l['replies'] = "Réponses";
+$l['views'] = "Vues";
+$l['posted'] = "Publié";
+$l['thread'] = "Sujet";
+$l['lastpost'] = "Dernier message";
+$l['post_forum'] = "Forum :";
+$l['lastpost_by'] = "Dernier message par";
+$l['posted_by'] = "Publié par";
+$l['replies2'] = "réponses";
+$l['reply'] = "réponse";
+$l['selectall'] = "Sélectionner tous les résultats supplémentaires ?";
+$l['any_prefix'] = "Trouver les sujets avec n’importe quel préfixe";
+$l['no_prefix'] = "Aucun préfixe";
+$l['inline_thread_moderation'] = "Actions rapides de modération :";
+$l['close_threads'] = "Verrouiller les sujets";
+$l['open_threads'] = "Déverrouiller les sujets";
+$l['stick_threads'] = "Épingler les sujets";
+$l['unstick_threads'] = "Désépingler les sujets";
+$l['soft_delete_threads'] = "Supprimer temporairement les sujets";
+$l['restore_threads'] = "Restaurer les sujets";
+$l['delete_threads'] = "Supprimer définitivement les sujets";
+$l['move_threads'] = "Déplacer ou copier les sujets";
+$l['approve_threads'] = "Approuver les sujets";
+$l['unapprove_threads'] = "Désapprouver les sujets";
+$l['inline_soft_delete_posts'] = "Supprimer temporairement les messages";
+$l['inline_restore_posts'] = "Restaurer les messages";
+$l['inline_delete_posts'] = "Supprimer définitivement les messages";
+$l['inline_merge_posts'] = "Fusionner les messages";
+$l['inline_split_posts'] = "Séparer les messages";
+$l['inline_move_posts'] = "Déplacer les messages";
+$l['inline_approve_posts'] = "Approuver les messages";
+$l['inline_unapprove_posts'] = "Désapprouver les messages";
+$l['inline_post_moderation'] = "Modération rapide des messages :";
+$l['inline_go'] = "Aller";
+$l['clear'] = "Réinitialiser";
+$l['in'] = "dans";
+$l['icon_dot'] = "Contient vos messages"; // The spaces for the icon labels are strategically placed so that there should be no extra space at the beginning or end of the resulting label and that spaces separate each 'status' ;)
+$l['icon_no_new'] = "Aucun nouveau message";
+$l['icon_new'] = "Nouveaux messages";
+$l['icon_hot'] = " Sujet populaire";
+$l['icon_close'] = " Sujet verrouillé";
+$l['attachment_count'] = "Ce sujet contient 1 pièce jointe.";
+$l['attachment_count_multiple'] = "Ce sujet contient {1} pièces jointes.";
+$l['goto_first_unread'] = "Aller au premier message non lu";
+$l['goto_post'] = "Aller au message";
+$l['page_selected'] = "Tous les <strong>{1}</strong> résultats de cette page sont sélectionnés.";
+$l['all_selected'] = "Tous les <strong>{1}</strong> résultats de cette recherche sont sélectionnés.";
+$l['select_all'] = "Sélectionner tous les <strong>{1}</strong> résultats de cette recherche.";
+$l['clear_selection'] = "Effacer la sélection.";
+
+$l['mod_options'] = "Options de modération";
+$l['display_all'] = "Afficher tous les résultats";
+$l['display_only_approved'] = "Afficher uniquement les résultats approuvés";
+$l['display_only_unapproved'] = "Afficher uniquement les résultats non approuvés";
+$l['display_only_softdeleted'] = "Afficher uniquement les résultats supprimés temporairement";
+
+$l['redirect_searchresults'] = "Votre recherche a été effectuée et vous allez être redirigé vers la liste des résultats.";
+
+$l['error_minsearchlength'] = "Un ou plusieurs de vos termes de recherche sont plus courts que la longueur minimale requise. La longueur minimale est de {1} caractères.<br />Si vous essayez de rechercher une phrase entière, mettez-la entre guillemets. Par exemple \"Le vif corgi bondit par-dessus le chat somnolent\".";
+$l['error_nosearchresults'] = "Votre recherche n’a retourné aucun résultat. Veuillez changer vos critères et réessayer.";
+$l['error_no_search_support'] = "Ce moteur de base de données ne prend pas en charge la recherche.";
+$l['error_nosearchterms'] = "Veuillez saisir au moins un mot-clé ou un nom d’utilisateur pour effectuer une recherche.";
+$l['error_searchflooding_1'] = "Vous ne pouvez effectuer qu’une recherche toutes les {1} secondes. Veuillez attendre encore 1 seconde avant de réessayer.";
+$l['error_searchflooding'] = "Vous ne pouvez effectuer qu’une recherche toutes les {1} secondes. Veuillez attendre encore {2} secondes avant de réessayer.";
+$l['error_invalidsearch'] = "Une recherche invalide a été spécifiée. Veuillez revenir en arrière et réessayer.";
