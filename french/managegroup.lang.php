4f8adadc2edc59e3264075333352f5ca72982089
Axel Fontana <axel.fontana@qualifio.com>
2025-09-27 19:31:36 +0200

    feat: add translation for managegroup

---
+++ french/managegroup.lang.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_group_management'] = "Gestion du groupe – {1}";
+$l['nav_join_requests'] = "Demandes d’adhésion";
+$l['nav_group_memberships'] = "Vos groupes";
+
+$l['not_leader_of_this_group'] = "Vous n’êtes pas responsable de ce groupe d’utilisateurs.";
+$l['invalid_group'] = "Ce groupe n’existe pas.";
+$l['pending_requests'] = "Demandes d’adhésion en attente";
+$l['num_requests_pending'] = "Il y a actuellement <strong>{1}</strong> demande(s) d’adhésion en attente pour ce groupe.";
+$l['group_management'] = "Gestion du groupe";
+$l['members_of'] = "Membres du groupe « {1} »";
+$l['user_name'] = "Nom d’utilisateur";
+$l['contact'] = "Contact";
+$l['reg_date'] = "Date d’inscription";
+$l['post_count'] = "Messages";
+$l['remove_selected'] = "Retirer les utilisateurs sélectionnés de ce groupe";
+$l['add_member'] = "Ajouter un utilisateur";
+$l['add_member_submit'] = "Ajouter l’utilisateur à ce groupe";
+$l['invite_member'] = "Inviter un utilisateur dans « {1} »";
+$l['invite_member_submit'] = "Inviter l’utilisateur dans le groupe";
+$l['join_requests'] = "Demandes d’adhésion";
+$l['join_requests_title'] = "Demandes d’adhésion – {1}";
+$l['leader'] = "(Responsable)";
+$l['reason'] = "Raison";
+$l['accept'] = "Accepter";
+$l['ignore'] = "Ignorer";
+$l['decline'] = "Refuser";
+$l['action_requests'] = "Appliquer";
+$l['join_requests_moderated'] = "Les demandes d’adhésion ont été traitées.<br />Vous allez maintenant être redirigé vers la liste des demandes.";
+$l['no_requests'] = "Il n’y a actuellement aucune demande d’adhésion en attente pour ce groupe.";
+$l['no_users'] = "Il n’y a aucun utilisateur dans ce groupe.";
+$l['user_added'] = "L’utilisateur a bien été ajouté au groupe.";
+$l['users_removed'] = "Les utilisateurs sélectionnés ont bien été retirés du groupe.";
+$l['group_no_members'] = "Il n’y a actuellement aucun membre dans ce groupe.<br />Pour revenir à la gestion du groupe, cliquez <a href=\"usercp.php?action=usergroups\">ici</a>.";
+$l['group_public_moderated'] = "Ce groupe d’utilisateurs est public et tout le monde peut le rejoindre. Les demandes d’adhésion doivent toutefois être approuvées par un responsable du groupe.";
+$l['group_public_not_moderated'] = "Ce groupe d’utilisateurs est public et tout le monde peut le rejoindre librement.";
+$l['group_public_invite'] = "Ce groupe d’utilisateurs est public, mais nécessite une invitation d’un responsable pour y adhérer.";
+$l['group_private'] = "Ce groupe est privé. Seuls les utilisateurs ajoutés par le responsable du groupe peuvent en faire partie.";
+$l['group_default'] = "Ce groupe est un groupe de base.";
+$l['group_leaders'] = "Responsables du groupe";
+$l['no_users_selected'] = "Aucun utilisateur n’a été sélectionné pour être retiré.<br />Veuillez revenir en arrière et sélectionner les utilisateurs à retirer de ce groupe.";
+
+$l['error_alreadyingroup'] = "L’utilisateur spécifié fait déjà partie de ce groupe.";
+$l['error_alreadyinvited'] = "L’utilisateur spécifié a déjà été invité.";
+
+$l['user_invited'] = "L’utilisateur a été invité à rejoindre le groupe.";
+$l['invite_pm_subject'] = "Vous avez été invité à rejoindre « {1} »";
+$l['invite_pm_message'] = "Vous avez reçu une invitation pour rejoindre le groupe d’utilisateurs « {1} ».
+
+Pour accepter, rendez-vous sur votre page « [url={2}/usercp.php?action=usergroups]Groupes[/url] » et cliquez sur « Accepter l’invitation ».
+
+Cette invitation n’a pas de date d’expiration.";
+$l['invite_pm_message_expires'] = "Vous avez reçu une invitation pour rejoindre le groupe d’utilisateurs « {1} ».
+
+Pour accepter, rendez-vous sur votre page « [url={2}/usercp.php?action=usergroups]Groupes[/url] » et cliquez sur « Accepter l’invitation ».
+
+Cette invitation expirera dans {3} jours.";
