41b8a3bd34b6ce45a52546f2cf1529a3e56997c8
Axel Fontana <axel.fontana@qualifio.com>
2025-09-27 23:22:18 +0200

    feat: add translation for newreply

---
+++ french/newreply.lang.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['nav_newreply'] = "Publier une réponse";
+
+$l['post_reply_to'] = "Répondre à « {1} »";
+$l['post_new_reply'] = "Publier une nouvelle réponse";
+$l['reply_to'] = "Répondre au sujet : {1}";
+$l['post_subject'] = "Sujet du message";
+$l['your_message'] = "Votre message";
+$l['post_options'] = "Options des messages";
+$l['options_sig'] = "<strong>Signature :</strong> inclure votre signature. (utilisateurs enregistrés uniquement)";
+$l['options_emailnotify'] = "<strong>Notification par courriel :</strong> recevez un courriel à chaque nouvelle réponse. (utilisateurs enregistrés uniquement)";
+$l['options_disablesmilies'] = "<strong>Désactiver les émoticônes :</strong> empêcher l’affichage des émoticônes dans ce message.";
+$l['post_reply'] = "Publier la réponse";
+$l['preview_post'] = "Prévisualiser";
+$l['mod_options'] = "Options de modération";
+$l['close_thread'] = "<strong>Verrouiller le sujet :</strong> empêcher toute publication supplémentaire dans ce sujet.";
+$l['stick_thread'] = "<strong>Épingler le sujet :</strong> maintenir ce sujet en haut du forum.";
+$l['forum_rules'] = "Règles du forum « {1} »";
+$l['thread_review'] = "Aperçu du sujet (du plus récent au plus ancien)";
+$l['thread_review_more'] = "Ce sujet contient plus de {1} réponses. <a href=\"{2}\" target=\"_blank\">Lire le sujet en entier.</a>";
+$l['posted_by'] = "Publié par";
+$l['draft_saved'] = "Le message a bien été enregistré comme brouillon.<br />Vous allez maintenant être redirigé vers la liste de vos brouillons.";
+$l['error_post_already_submitted'] = "Vous avez déjà publié un message identique dans ce sujet. Veuillez consulter le sujet pour voir votre réponse.";
+$l['multiquote_external_one'] = "Vous avez sélectionné un message appartenant à un autre sujet.";
+$l['multiquote_external'] = "Vous avez sélectionné {1} messages appartenant à d’autres sujets.";
+$l['multiquote_external_one_deselect'] = "Désélectionner ce message";
+$l['multiquote_external_deselect'] = "Désélectionner ces messages";
+$l['multiquote_external_one_quote'] = "Citer également ce message";
+$l['multiquote_external_quote'] = "Citer également ces messages";
+
+$l['redirect_newreply'] = "La réponse a bien été publiée.";
+$l['redirect_newreply_moderation'] = "L’administrateur a spécifié que tous les nouveaux messages nécessitent une approbation. Vous allez maintenant être redirigé vers le sujet.";
+$l['redirect_newreply_post'] = "<br />Vous allez maintenant être redirigé vers votre message.";
+$l['redirect_newreplyerror'] = "Votre réponse a été rejetée pour manque de contenu.<br />Vous allez maintenant être redirigé vers le sujet.";
+$l['redirect_threadclosed'] = "Vous ne pouvez pas répondre à ce sujet car il a été fermé par un modérateur.";
+$l['error_post_noperms'] = "Vous n’avez pas la permission de modifier ce brouillon.";
+
+
+$l['error_stop_forum_spam_spammer'] = "Votre {1} correspond à celui d’un spammeur connu. Si vous pensez qu’il s’agit d’une erreur, veuillez contacter un administrateur.";
+$l['error_stop_forum_spam_fetching'] = "Une erreur est survenue lors de la vérification de votre message dans la base de données des spammeurs. Veuillez réessayer plus tard.";
+
+
+$l['error_suspendedposting'] = "Vos privilèges de publication sont actuellement suspendus {1}.<br /><br />
+
+Date de suspension : {2}";
+$l['error_suspendedposting_temporal'] = "jusqu’au {1}";
+$l['error_suspendedposting_permanent'] = "de façon permanente";
