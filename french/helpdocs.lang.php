c9a2219785d452fbc6867e0b3a685b4202f12394
Axel Fontana <axel.fontana@qualifio.com>
2025-09-28 16:37:20 +0200

    feat: add translation for helpdocs

---
+++ french/helpdocs.lang.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * MyBB 1.8 English Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+// Help Document 1
+$l['d1_name'] = "Inscription";
+$l['d1_desc'] = "Avantages et privilèges liés à l’inscription.";
+$l['d1_document'] = "Certaines parties de ce forum nécessitent d’être connecté et inscrit. L’inscription est gratuite et ne prend que quelques minutes.
+<br /><br />Nous vous encourageons à vous inscrire ; une fois inscrit, vous pourrez publier des messages, définir vos préférences personnelles et gérer votre profil.
+<br /><br />Parmi les fonctionnalités nécessitant généralement une inscription figurent les abonnements aux discussions, le changement de thème, l’accès au bloc-notes personnel et l’envoi de messages aux membres du forum.";
+
+// Help Document 2
+$l['d2_name'] = "Mise à jour du profil";
+$l['d2_desc'] = "Modifier les informations vous concernant enregistrées sur le forum.";
+$l['d2_document'] = "Au cours de votre utilisation du forum, vous pourriez souhaiter mettre à jour certaines informations telles que vos coordonnées de messagerie instantanée, votre mot de passe ou votre adresse électronique. Vous pouvez modifier ces informations depuis le panneau de contrôle de l’utilisateur.
+<br /><br />Pour accéder à ce panneau, cliquez simplement sur le lien situé en haut à gauche de la plupart des pages, intitulé « Mon compte ». Ensuite, choisissez le lien approprié dans la section « Profil », modifiez ou mettez à jour les éléments souhaités, puis cliquez sur le bouton « Enregistrer » en bas de la page pour que les modifications soient prises en compte.";
+
+// Help Document 3
+$l['d3_name'] = "Utilisation des cookies";
+$l['d3_desc'] = "MyBB utilise des cookies pour stocker certaines informations relatives à votre visite.";
+$l['d3_document'] = "MyBB utilise des cookies pour mémoriser vos informations de connexion si vous êtes inscrit et votre dernière visite si vous ne l’êtes pas.
+<br /><br />Les cookies sont de petits fichiers texte stockés sur votre ordinateur ; ceux définis par ce forum ne peuvent être utilisés que sur ce site et ne présentent aucun risque pour la sécurité.
+<br /><br />Les cookies de ce forum permettent également de suivre les sujets que vous avez lus et la date de votre dernière lecture.
+<br /><br />Pour supprimer tous les cookies définis par ce forum, vous pouvez cliquer <a href=\"misc.php?action=clearcookies&amp;my_post_key={1}\">ici</a>.";
+
+// Help Document 4
+$l['d4_name'] = "Connexion et déconnexion";
+$l['d4_desc'] = "Comment se connecter et se déconnecter.";
+$l['d4_document'] = "Lorsque vous vous connectez, un cookie est placé sur votre ordinateur afin de vous permettre de naviguer sur le forum sans avoir à saisir votre nom d’utilisateur et votre mot de passe à chaque fois. La déconnexion supprime ce cookie pour garantir qu’aucune autre personne ne puisse naviguer sur le forum en votre nom.
+<br /><br />Pour vous connecter, cliquez simplement sur le lien « Connexion » situé en haut à gauche du forum. Pour vous déconnecter, cliquez sur le lien « Déconnexion » au même endroit. Si vous ne parvenez pas à vous déconnecter, la suppression des cookies sur votre ordinateur aura le même effet.";
+
+// Help Document 5
+$l['d5_name'] = "Publier un nouveau sujet";
+$l['d5_desc'] = "Créer un nouveau sujet dans un forum.";
+$l['d5_document'] = "Lorsque vous accédez à un forum qui vous intéresse et que vous souhaitez publier un nouveau sujet, cliquez simplement sur le bouton « Nouveau sujet » situé en haut ou en bas du forum.
+<br /><br />Veuillez noter que vous n’avez pas forcément la permission de publier un nouveau sujet dans n’importe quel forum, car l’administrateur peut avoir restreint la publication à certains membres et groupes ou avoir archivé des sections du forum.";
+
+// Help Document 6
+$l['d6_name'] = "Publier une réponse";
+$l['d6_desc'] = "Répondre à un sujet dans un forum.";
+$l['d6_document'] = "Au cours de votre visite, vous pouvez rencontrer un sujet auquel vous souhaitez répondre. Pour ce faire, cliquez simplement sur le bouton « Répondre » situé en haut ou en bas du sujet.
+<br /><br />Veuillez noter que vous n’avez pas forcément la permission de publier une nouvelle réponse dans n’importe quel forum, car l’administrateur peut avoir restreint la publication à certains membres et groupes ou avoir archivé des sections du forum. Un modérateur peut également avoir verrouillé un sujet, ce qui empêche les utilisateurs d’y répondre.
+<br /><br />Un utilisateur ne peut réouvrir un sujet verrouillé qu’avec l’aide d’un modérateur ou d’un administrateur.";
+
+// Help Document 7
+$l['d7_name'] = "Introduction aux MyCode";
+$l['d7_desc'] = "Apprenez à utiliser MyCode pour enrichir vos messages.";
+$l['d7_document'] = "Vous pouvez utiliser les MyCodes, également appelés « BB Codes » afin de mettre en forme vos messages. Les MyCodes sont une version simplifiée du langage HTML et s’utilisent de manière similaire aux balises HTML que vous connaissez peut-être déjà.
+<br /><br />Le tableau ci-dessous présente un aperçu rapide des MyCode disponibles :
+<br /><br />
+<table class=\"tborder\" cellspacing=\"0\" cellpadding=\"5\" border=\"0\" style=\"width:90%\">
+<tbody>
+<tr>
+<td class=\"tcat\" style=\"width:55%\"><span class=\"smalltext\"><strong>Saisie</strong></span></td>
+<td class=\"tcat\" style=\"width:35%\"><span class=\"smalltext\"><strong>Rendu</strong></span></td>
+<td class=\"tcat\" style=\"width:10%\"><span class=\"smalltext\"><strong>Remarques</strong></span></td>
+</tr>
+<tr>
+<td class=\"trow1\"><span style=\"font-weight: bold; color: #ff0000;\">[b]</span>Ce texte est en gras<span style=\"font-weight: bold; color: #ff0000;\">[/b]</span></td>
+<td class=\"trow1\"><span style=\"font-weight: bold;\" class=\"mycode_b\">Ce texte est en gras</span></td>
+<td class=\"trow1\"></td>
+</tr>
+<tr>
+<td class=\"trow2\"><span style=\"font-weight: bold; color: #ff0000;\">[i]</span>Ce texte est en italique<span style=\"font-weight: bold; color: #ff0000;\">[/i]</span></td>
+<td class=\"trow2\"><span style=\"font-style: italic;\" class=\"mycode_i\">Ce texte est en italique</span></td>
+<td class=\"trow2\"></td>
+</tr>
+<tr>
+<td class=\"trow1\"><span style=\"font-weight: bold; color: #ff0000;\">[u]</span>Ce texte est souligné<span style=\"font-weight: bold; color: #ff0000;\">[/u]</span></td>
+<td class=\"trow1\"><span style=\"text-decoration: underline;\" class=\"mycode_u\">Ce texte est souligné</span></td>
+<td class=\"trow1\"></td>
+</tr>
+<tr>
+<td class=\"trow2\"><span style=\"font-weight: bold; color: #ff0000;\">[s]</span>Ce texte est barré<span style=\"font-weight: bold; color: #ff0000;\">[/s]</span></td>
+<td class=\"trow2\"><span style=\"text-decoration: line-through;\" class=\"mycode_s\">Ce texte est barré</span></td>
+<td class=\"trow2\"></td>
+</tr>
+<tr>
+<td class=\"trow1\"><span style=\"font-weight: bold; color: #ff0000;\">[url]</span>http://www.example.com/<span style=\"font-weight: bold; color: #ff0000;\">[/url]</span></td>
+<td class=\"trow1\"><a href=\"http://www.example.com/\" class=\"mycode_url\" rel=\"nofollow\">http://www.example.com/</a></td>
+<td class=\"trow1\">Les adresses seront automatiquement transformées en liens si le protocole approprié est inclus (les protocoles valides sont : http, https, ftp, news, irc, ircs et irc6).</td>
+</tr>
+<tr>
+<td class=\"trow2\"><span style=\"font-weight: bold; color: #ff0000;\">[url=http://www.example.com/]</span>Example.com<span style=\"font-weight: bold; color: #ff0000;\">[/url]</span></td>
+<td class=\"trow2\"><a href=\"http://www.example.com/\" class=\"mycode_url\" rel=\"nofollow\">Example.com</a></td>
+<td class=\"trow2\"></td>
+</tr>
+<tr>
+<td class=\"trow1\"><span style=\"font-weight: bold; color: #ff0000;\">[email]</span>example@example.com<span style=\"font-weight: bold; color: #ff0000;\">[/email]</span></td>
+<td class=\"trow1\"><a href=\"mailto:example@example.com\" class=\"mycode_email\">example@example.com</a></td>
+<td class=\"trow1\"></td>
+</tr>
+<tr>
+<td class=\"trow2\"><span style=\"font-weight: bold; color: #ff0000;\">[email=example@example.com]</span>Envoyez moi un courriel!<span style=\"font-weight: bold; color: #ff0000;\">[/email]</span></td>
+<td class=\"trow2\"><a href=\"mailto:example@example.com\" class=\"mycode_email\">Envoyez moi un courriel!</a></td>
+<td class=\"trow2\">A subject can be included by adding <strong>?subject=Subject Here</strong> after the email address.</td>
+</tr>
+<tr>
+<td class=\"trow1\"><span style=\"font-weight: bold; color: #ff0000;\">[quote]</span>Le texte cité sera ici<span style=\"font-weight: bold; color: #ff0000;\">[/quote]</span></td>
+<td class=\"trow1\"><blockquote class=\"mycode_quote\"><cite>Quote:</cite>Le texte cité sera ici</blockquote></td>
+<td class=\"trow1\"></td>
+</tr>
+<tr>
+<td class=\"trow2\"><span style=\"font-weight: bold; color: #ff0000;\">[quote='Admin' pid='1' dateline='946684800']</span>Le texte cité sera ici<span style=\"font-weight: bold; color: #ff0000;\">[/quote]</span></td>
+<td class=\"trow2\"><blockquote class=\"mycode_quote\"><cite><span> (01-01-2000, 12:00 AM)</span>Admin Wrote:  <a href=\"http://www.example.com/showthread.php?pid=1#pid1\" class=\"quick_jump\" rel=\"nofollow\"></a></cite>Le texte cité sera ici</blockquote></td>
+<td class=\"trow2\">This format is used when quoting posts. <strong>pid</strong> links to a post, <strong>dateline</strong> is a <a href=\"https://www.unixtimestamp.com/\">UNIX timestamp</a>.</td>
+</tr>
+<tr>
+<td class=\"trow1\"><span style=\"font-weight: bold; color: #ff0000;\">[code]</span>Text with preserved formatting<span style=\"font-weight: bold; color: #ff0000;\">[/code]</span></td>
+<td class=\"trow1\"><div class=\"codeblock\"><div class=\"title\">Code:</div><div class=\"body\" dir=\"ltr\"><code>Text with preserved formatting</code></div></div></td>
+<td class=\"trow1\"></td>
+</tr>
+<tr>
+<td class=\"trow2\"><span style=\"font-weight: bold; color: #ff0000;\">[php]</span>&lt;?php echo \"Hello world!\";?&gt;<span style=\"font-weight: bold; color: #ff0000;\">[/php]</span></td>
+<td class=\"trow2\"><div class=\"codeblock phpcodeblock\"><div class=\"title\">PHP Code:</div><div class=\"body\"><div dir=\"ltr\"><code><span style=\"color: #0000BB\">&lt;?php&nbsp;</span><span style=\"color: #007700\">echo&nbsp;</span><span style=\"color: #DD0000\">\"Hello&nbsp;world!\"</span><span style=\"color: #007700\">;</span><span style=\"color: #0000BB\">?&gt;</span></code></div></div></div></div></td>
+<td class=\"trow2\"></td>
+</tr>
+<tr>
+<td class=\"trow1\"><span style=\"font-weight: bold; color: #ff0000;\">[img]</span>https://secure.php.net/images/php.gif<span style=\"font-weight: bold; color: #ff0000;\">[/img]</span></td>
+<td class=\"trow1\"><img src=\"https://secure.php.net/images/php.gif\" class=\"mycode_img\"></td>
+<td class=\"trow1\"></td>
+</tr>
+<tr>
+<td class=\"trow2\"><span style=\"font-weight: bold; color: #ff0000;\">[img=50x50]</span>https://secure.php.net/images/php.gif<span style=\"font-weight: bold; color: #ff0000;\">[/img]</span></td>
+<td class=\"trow2\"><img src=\"https://secure.php.net/images/php.gif\" width=\"50\" height=\"50\" class=\"mycode_img\"></td>
+<td class=\"trow2\">Le format est largeur × hauteur.</td>
+</tr>
+<tr>
+<td class=\"trow1\"><span style=\"font-weight: bold; color: #ff0000;\">[color=red]</span>Ce texte est en rouge<span style=\"font-weight: bold; color: #ff0000;\">[/color]</span></td>
+<td class=\"trow1\"><span style=\"color: red;\" class=\"mycode_color\">Ce texte est en rouge</span></td>
+<td class=\"trow1\">Vous pouvez utiliser un <a href=\"https://www.w3schools.com/cssref/css_colors.asp\">nom de couleur</a> ou un code hexadécimal.</td>
+</tr>
+<tr>
+<td class=\"trow2\"><span style=\"font-weight: bold; color: #ff0000;\">[size=large]</span>Ce texte est grand<span style=\"font-weight: bold; color: #ff0000;\">[/size]</span></td>
+<td class=\"trow2\"><span style=\"font-size: large\" class=\"mycode_size\">Ce texte est grand</span></td>
+<td class=\"trow2\">Les valeurs autorisées sont : xx-small, x-small, small, medium, large, x-large, xx-large</td>
+</tr>
+<tr>
+<td class=\"trow1\"><span style=\"font-weight: bold; color: #ff0000;\">[size=30]</span>Ce texte fait 30px<span style=\"font-weight: bold; color: #ff0000;\">[/size]</span></td>
+<td class=\"trow1\"><span style=\"font-size: 30px\" class=\"mycode_size\">Ce texte fait 30px</span></td>
+<td class=\"trow1\">Accepte un nombre de 1 à 50</td>
+</tr>
+<tr>
+<td class=\"trow2\"><span style=\"font-weight: bold; color: #ff0000;\">[font=Impact]</span>Cette police d'écriture est « Impact »<span style=\"font-weight: bold; color: #ff0000;\">[/font]</span></td>
+<td class=\"trow2\"><span style=\"font-family: Impact;\" class=\"mycode_font\">Cette police d'écriture est « Impact »</span></td>
+<td class=\"trow2\">La police doit être disponible sur votre ordinateur</td>
+</tr>
+<tr>
+<td class=\"trow1\"><span style=\"font-weight: bold; color: #ff0000;\">[align=center]</span>Ce texte est centré<span style=\"font-weight: bold; color: #ff0000;\">[/align]</span></td>
+<td class=\"trow1\"><div style=\"text-align: center;\" class=\"mycode_align\">Ce texte est centré</div></td>
+<td class=\"trow1\">Les valeurs autorisées sont : left, center, right, justify</td>
+</tr>
+<tr>
+<td class=\"trow2\"><span style=\"font-weight: bold; color: #ff0000;\">[list]</span><br />[*]Élément #1<br />[*]Élément #2<br />[*]Élément #3<br /><span style=\"font-weight: bold; color: #ff0000;\">[/list]</span></td>
+<td class=\"trow2\"><ul class=\"mycode_list\"><li>Élément #1</li><li>Élément #2</li><li>Élément #3</li></ul></td>
+<td class=\"trow2\"></td>
+</tr>
+<tr>
+<td class=\"trow1\"><span style=\"font-weight: bold; color: #ff0000;\">[list=1]</span><br />[*]Élément #1<br />[*]Élément #2<br />[*]Élément #3<br /><span style=\"font-weight: bold; color: #ff0000;\">[/list]</span></td>
+<td class=\"trow1\"><ol class=\"mycode_list\" type=\"1\"><li>Élément #1</li><li>Élément #2</li><li>Élément #3</li></ol></td>
+<td class=\"trow1\"><strong>1</strong> peut être utilisé pour une liste numérotée, <strong>a</strong> pour une liste alphabétique et <strong>i</strong> pour une liste en chiffres romains.</td>
+</tr>
+<tr>
+<td class=\"trow2\">Une ligne qui<span style=\"font-weight: bold; color: #ff0000;\">[hr]</span>divise</td>
+<td class=\"trow2\">Une ligne qui<hr class=\"mycode_hr\">divise</td>
+<td class=\"trow2\"></td>
+</tr>
+<tr>
+<td class=\"trow1\"><span style=\"font-weight: bold; color: #ff0000;\">[video=youtube]</span>https://www.youtube.com/watch?v=dQw4w9WgXcQ<span style=\"font-weight: bold; color: #ff0000;\">[/video]</span></td>
+<td class=\"trow1\"><iframe src=\"//www.youtube.com/embed/dQw4w9WgXcQ\" allowfullscreen=\"\" width=\"460\" height=\"255\" frameborder=\"0\"></iframe></td>
+<td class=\"trow1\">Actuellement, les vidéos de Dailymotion, Facebook, LiveLeak, Metacafe, Mixer, MySpace TV, Twitch, Vimeo, Yahoo Vidéos et YouTube sont prises en charge.</td>
+</tr>
+</tbody></table>
+<br /><br />Il est possible que les administrateurs aient créé d’autres MyCodes à utiliser. Veuillez contacter un administrateur pour savoir s’il en existe et comment les utiliser.";
