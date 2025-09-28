4c7c856a7d135640c8825d8216c5e23b7a66c792
Axel Fontana <axel.fontana@qualifio.com>
2025-09-28 22:12:01 +0200

    feat: add translation for admin/config-report-reasons

---
+++ french/admin/config_report_reasons.lang.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['report_reasons'] = "Motifs de signalement";
+$l['report_reasons_desc'] = "Cette section vous permet de gérer les motifs pouvant être sélectionnés lorsque les utilisateurs signalent du contenu.";
+$l['add_new_reason'] = "Ajouter un motif";
+$l['add_new_reason_desc'] = "Ajouter un motif pouvant être sélectionné lorsque les utilisateurs signalent du contenu.";
+$l['edit_reason'] = "Modifier le motif";
+$l['edit_reason_desc'] = "Modifier un motif pouvant être sélectionné lorsque les utilisateurs signalent du contenu.";
+
+$l['applies_to'] = "Disponibilité";
+$l['order'] = "Ordre";
+$l['extra_comment'] = "Requiert un commentaire ?";
+$l['options'] = "Options";
+$l['delete_reason'] = "Supprimer le motif";
+$l['no_report_reasons'] = "Il n’y a actuellement aucun motif de signalement.";
+
+$l['reason_title'] = "Titre";
+$l['reason_title_desc'] = "Entrez le titre du motif de signalement.";
+$l['requires_extra'] = "Requiert un commentaire supplémentaire ?";
+$l['requires_extra_desc'] = "L’utilisateur doit alors saisir un commentaire supplémentaire lorsque ce motif est sélectionné.";
+$l['save_reason'] = "Enregistrer le motif";
+
+$l['content_colon'] = "Contenu :";
+
+$l['all_content'] = "Tous les contenus";
+$l['select_content'] = "Sélectionner les contenus";
+
+// Content types
+$l['report_content_'] = "Aucun contenu";
+$l['report_content_all'] = "Tous les contenus";
+$l['report_content_post'] = "Messages";
+$l['report_content_profile'] = "Profils";
+$l['report_content_reputation'] = "Évaluations";
+
+// Content reasons
+$l['report_reason_rules'] = "Enfreint les règles du forum";
+$l['report_reason_bad'] = "Contenu inapproprié";
+$l['report_reason_spam'] = "Contenu indésirable";
+$l['report_reason_wrong'] = "Mauvais forum";
+$l['report_reason_other'] = "Autre";
+
+$l['default_report_reason'] = "Par défaut";
+$l['custom_report_reason'] = "Personnalisé";
+
+$l['update_reasons_order'] = "Mettre à jour l’ordre d’affichage";
+
+$l['error_cannot_modify_reason'] = "Vous ne pouvez pas modifier ce motif.";
+$l['error_cannot_delete_reason'] = "Vous ne pouvez pas supprimer ce motif.";
+$l['error_invalid_reason'] = "Vous avez sélectionné un motif invalide.";
+$l['error_missing_title'] = "Veuillez saisir un titre pour ce motif.";
+//$l['error_missing_applies_to'] = "Vous n’avez pas sélectionné les contenus auxquels s’applique ce motif.";
+$l['error_missing_extra'] = "Vous n’avez pas indiqué si ce motif requiert un commentaire supplémentaire.";
+
+
+$l['success_reasons_disporder_updated'] = "L’ordre d’affichage des motifs de signalement a bien été mis à jour.";
+$l['success_reason_created'] = "Le motif a bien été créé.";
+$l['success_reason_updated'] = "Le motif a bien été mis à jour.";
+$l['success_reason_deleted'] = "Le motif a bien été supprimé.";
+
+$l['confirm_reason_deletion'] = "Êtes-vous sûr de vouloir supprimer ce motif ?";
