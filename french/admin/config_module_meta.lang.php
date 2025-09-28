800534985cd6a4d60a1e3565299515e87263805b
Axel Fontana <axel.fontana@qualifio.com>
2025-09-28 21:24:52 +0200

    feat: add translation for admin/config-module-meta

---
+++ french/admin/config_module_meta.lang.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * MyBB 1.8 English Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['configuration'] = "Configuration";
+
+$l['bbsettings'] = "Paramètres";
+$l['banning'] = "Bannissements";
+$l['custom_profile_fields'] = "Champs de profil";
+$l['smilies'] = "Émoticônes";
+$l['word_filters'] = "Censure";
+$l['mycode'] = "MyCode";
+$l['languages'] = "Langues";
+$l['post_icons'] = "Icônes de messages";
+$l['help_documents'] = "Documents d’aide";
+$l['plugins'] = "Extensions";
+$l['attachment_types'] = "Types de pièces jointes";
+$l['moderator_tools'] = "Outils du modérateur";
+$l['spiders_bots'] = "Robots";
+$l['calendars'] = "Calendriers";
+$l['warning_system'] = "Avertissements";
+$l['thread_prefixes'] = "Préfixes de sujets";
+$l['security_questions'] = "Questions de sécurité";
+$l['report_reasons'] = "Motifs de signalement";
+
+$l['can_manage_settings'] = "Peut gérer les paramètres ?";
+$l['can_manage_banned_accounts'] = "Peut gérer les bannissements ?";
+$l['can_manage_custom_profile_fields'] = "Peut gérer les champs de profil personnalisés ?";
+$l['can_manage_smilies'] = "Peut gérer les émoticônes ?";
+$l['can_manage_bad_words'] = "Peut gérer la censure ?";
+$l['can_manage_custom_mycode'] = "Peut gérer les MyCodes personnalisés ?";
+$l['can_manage_language_packs'] = "Peut gérer les langues ?";
+$l['can_manage_post_icons'] = "Peut gérer les icônes de messages ?";
+$l['can_manage_help_documents'] = "Peut gérer les documents d’aide ?";
+$l['can_manage_plugins'] = "Peut gérer les extensions ?";
+$l['can_manage_attachment_types'] = "Peut gérer les types de pièces jointes ?";
+$l['can_manage_spiders_bots'] = "Peut gérer les robots ?";
+$l['can_manage_calendars'] = "Peut gérer les calendriers ?";
+$l['can_manage_warning_system'] = "Peut gérer le système d’avertissement ?";
+$l['can_manage_mod_tools'] = "Peut gérer les outils de modération ?";
+$l['can_manage_thread_prefixes'] = "Peut gérer les préfixes de sujets ?";
+$l['can_manage_security_questions'] = "Peut gérer les questions de sécurité ?";
+$l['can_manage_report_reasons'] = "Peut gérer les motifs de signalement ?";
