70a586647cbcb0ffb0610c13c05224a663c1da1a
Axel Fontana <axel.fontana@qualifio.com>
2025-09-29 10:40:53 +0200

    feat: add translation for admin/tools-statistics

---
+++ french/admin/tools_statistics.lang.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['statistics'] = "Statistiques";
+$l['overall_statistics'] = "Statistiques globales";
+$l['overall_statistics_desc'] = "Cette page vous permet de consulter les statistiques globales de votre forum. Les heures utilisent le fuseau horaire par défaut (UTC).";
+
+$l['date_range'] = "Période";
+
+$l['date'] = "Date";
+$l['users'] = "Utilisateurs";
+$l['threads'] = "Sujets";
+$l['posts'] = "Messages";
+
+$l['from'] = "De";
+$l['to'] = "à";
+
+$l['increase'] = "Augmentation";
+$l['no_change'] = "Aucune variation";
+$l['decrease'] = "Diminution";
+
+$l['error_no_results_found_for_criteria'] = "Aucun résultat n’a été trouvé pour la période sélectionnée. Veuillez essayer avec une autre période.";
+$l['error_no_statistics_available_yet'] = "Aucune donnée statistique n’est disponible pour votre forum en ce moment.";
