93bcb917744fb01ff5197e9dae8fcdd1d5ccd1f5
Axel Fontana <axel.fontana@qualifio.com>
2025-09-30 10:29:39 +0200

    feat: add translation for admin/tools-spamlog

---
+++ french/admin/tools_spamlog.lang.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['spam_logs'] = 'Journal de spam';
+$l['spam_logs_desc'] = "Cette section vous permet de consulter l’historique des utilisateurs bloqués par les filtres anti-spam.";
+$l['prune_spam_logs'] = "Purger le journal de spam";
+$l['prune_spam_logs_desc'] = "Cette page vous permet de purger le journal de spam répondant à des critères spécifiques.";
+$l['spam_username'] = 'Nom d’utilisateur';
+$l['spam_email'] = 'Adresse électronique';
+$l['spam_ip'] = 'Adresse IP';
+$l['spam_date'] = 'Date';
+$l['spam_confidence'] = 'Probabilité';
+$l['no_spam_logs'] = 'Aucun utilisateur n’a encore été bloqué par les filtres anti-spam.';
+$l['success_pruned_spam_logs'] = 'Le journal de spam a bien été purgé.';
+$l['note_logs_locked'] = 'Pour des raisons de sécurité, les entrées datant de moins de 24 heures ne peuvent pas être purgées.';
+$l['all_usernames'] = "Tous les noms d’utilisateur";
+$l['all_emails'] = "Toutes les adresses électroniques";
+$l['date_range'] = "Période :";
+$l['older_than'] = "Plus anciens que ";
+$l['days'] = "jours";
+$l['filter_spam_logs'] = 'Filtrer le journal de spam';
+$l['asc'] = "croissant";
+$l['desc'] = "décroissant";
+$l['search_ip_on_sfs'] = "Rechercher cette IP sur Stop Forum Spam";
+$l['search'] = "Rechercher";
+
+$l['in'] = "dans l’ordre";
+$l['order'] = "";
+$l['sort_by'] = "Trier par";
+$l['results_per_page'] = "Résultats par page";
