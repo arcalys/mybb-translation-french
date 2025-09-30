a4e9f938ed8e76266ddb7d27dfde577f7fad3484
Axel Fontana <axel.fontana@qualifio.com>
2025-09-30 16:11:52 +0200

    feat: add translation for admin/tools-module

---
+++ french/admin/tools_module_meta.lang.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * MyBB 1.8 English Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['tools_and_maintenance'] = "Outils et maintenance";
+
+$l['maintenance'] = "Maintenance";
+$l['logs'] = "Journaux";
+
+$l['system_health'] = "État du système";
+$l['cache_manager'] = "Gestion du cache";
+$l['task_manager'] = "Tâches planifiées";
+$l['recount_and_rebuild'] = "Recalculer";
+$l['view_php_info'] = "Informations PHP";
+$l['database_backups'] = "Sauvegardes";
+$l['optimize_database'] = "Optimisation";
+$l['file_verification'] = "Vérification des fichiers";
+
+$l['administrator_log'] = "Journal d’administration";
+$l['moderator_log'] = "Journal de modération";
+$l['user_email_log'] = "Journal des courriels";
+$l['system_mail_log'] = "Journal des courriels système";
+$l['user_warning_log'] = "Journal des avertissements";
+$l['spam_log'] = "Journal de spam";
+$l['security_log'] = "Journal de sécurité";
+$l['statistics'] = "Statistiques";
+
+$l['can_access_system_health'] = "Peut consulter l’état du système ?";
+$l['can_manage_cache'] = "Peut gérer le cache ?";
+$l['can_manage_tasks'] = "Peut gérer les tâches planifiées ?";
+$l['can_manage_db_backup'] = "Peut gérer les sauvegardes de la base de données ?";
+$l['can_optimize_db'] = "Peut optimiser la base de données ?";
+$l['can_recount_and_rebuild'] = "Peut recalculter les compteurs et statistiques ?";
+$l['can_manage_admin_logs'] = "Peut gérer le journal d’administration ?";
+$l['can_manage_mod_logs'] = "Peut gérer le journal de modération ?";
+$l['can_manage_user_mail_log'] = "Peut gérer le journal des courriels ?";
+$l['can_manage_system_mail_log'] = "Peut gérer le journal des courriels système ?";
+$l['can_manage_user_warning_log'] = "Peut gérer le journal des avertissements ?";
+$l['can_manage_spam_log'] = "Peut gérer le journal de spam ?";
+$l['can_manage_security_log'] = "Peut gérer le journal de sécurité ?";
+$l['can_view_php_info'] = "Peut consulter les informations PHP  ?";
+$l['can_manage_file_verification'] = "Peut utiliser la vérification des fichiers ?";
+$l['can_view_statistics'] = "Peut consulter les statistiques ?";
