0a5fd96b2a1750ba690769e0e3486dc23bdaf464
Axel Fontana <axel.fontana@qualifio.com>
2025-09-30 16:56:16 +0200

    feat: add translation for admin/tools-mailerrors

---
+++ french/admin/tools_mailerrors.lang.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['system_email_log'] = "Journal des courriels système";
+$l['system_email_log_desc'] = "Cette section vous permet de consulter tous les courriels générés par MyBB qui ont rencontré une erreur lors de leur envoi. Ce journal est particulièrement utile pour identifier des problèmes de configuration SMTP.";
+$l['prune_system_email_log'] = "Purger le journal des courriels système";
+$l['filter_system_email_log'] = "Filtrer le journal des courriels système";
+
+$l['close_window'] = "Fermer la fenêtre";
+$l['user_email_log_viewer'] = "Consultation du journal des courriels utilisateurs";
+$l['smtp_code'] = "Code SMTP :";
+$l['smtp_server_response'] = "Réponse du serveur SMTP :";
+$l['to'] = "Destinataire";
+$l['from'] = "Destinateur";
+$l['subject'] = "Sujet";
+$l['date'] = "Date";
+$l['email'] = "Contenu du courriel";
+$l['date_sent'] = "Date d’envoi";
+$l['error_message'] = "Message d’erreur";
+$l['find'] = "Rechercher";
+$l['no_logs'] = "Aucune entrée ne correspond aux critères sélectionnés.";
+$l['subject_contains'] = "Le sujet contient";
+$l['error_message_contains'] = "Le message d’erreur contient";
+$l['to_address_contains'] = "L’adresse du destinataire contient";
+$l['from_address_contains'] = "L’adresse de l’expéditeur contient";
+$l['find_emails_to_addr'] = "Trouver tous les courriels envoyés à cette adresse";
