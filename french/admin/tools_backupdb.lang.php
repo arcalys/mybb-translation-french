8b2e92cf57bfbf81035f61f595b06f8d840719de
Axel Fontana <axel.fontana@qualifio.com>
2025-09-30 10:07:36 +0200

    feat: add translation for admin/tools-backupdb

---
+++ french/admin/tools_backupdb.lang.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['database_backups'] = "Sauvegardes de la base de données";
+$l['database_backups_desc'] = "Cette section vous permet de gérer les sauvegardes de votre base de données actuellement stockées sur votre serveur dans le répertoire dédié.";
+
+$l['new_database_backup'] = "Nouvelle sauvegarde de la base de données";
+$l['new_backup'] = "Nouvelle sauvegarde";
+$l['new_backup_desc'] = "Cette page vous permet de créer une nouvelle sauvegarde de votre base de données.";
+$l['backups'] = "Sauvegardes de la base de données";
+$l['existing_database_backups'] = "Sauvegardes existantes";
+
+$l['backup_saved_to'] = "La sauvegarde a été enregistrée dans :";
+$l['download'] = "Télécharger";
+$l['table_selection'] = "Sélection des tables";
+$l['backup_options'] = "Paramètres de la sauvegarde";
+$l['table_select_desc'] = "Vous pouvez sélectionner les tables de la base de données sur lesquelles effectuer cette opération. Maintenez « CTRL » pour en sélectionner plusieurs.";
+$l['select_all'] = "Tout sélectionner";
+$l['deselect_all'] = "Tout désélectionner";
+$l['select_forum_tables'] = "Sélectionner les tables :";
+$l['file_type'] = "Type de fichier";
+$l['file_type_desc'] = "Sélectionnez le type de fichier que vous souhaitez utiliser pour cette sauvegarde.";
+$l['gzip_compressed'] = "Compressé (GZIP)";
+$l['plain_text'] = "Texte brut";
+$l['save_method'] = "Méthode d’enregistrement";
+$l['save_method_desc'] = "Sélectionnez la méthode que vous souhaitez utiliser pour cette sauvegarde.";
+$l['backup_directory'] = "Répertoire de sauvegarde";
+$l['backup_contents'] = "Contenu";
+$l['backup_contents_desc'] = "Sélectionnez les informations que vous souhaitez inclure dans la sauvegarde.";
+$l['structure_and_data'] = "Structure et données";
+$l['structure_only'] = "Uniquement la structure";
+$l['data_only'] = "Uniquement les données";
+$l['analyze_and_optimize'] = "Analyser et optimiser les tables sélectionnées";
+$l['analyze_and_optimize_desc'] = "Souhaitez-vous que les tables sélectionnées soient analysées et optimisées lors de la sauvegarde ?";
+$l['perform_backup'] = "Créer la sauvegarde";
+$l['backup_filename'] = "Nom du fichier";
+$l['file_size'] = "Taille du fichier";
+$l['creation_date'] = "Date de création";
+$l['no_backups'] = "Il n’y a actuellement aucune sauvegarde de votre base de données.";
+
+$l['error_file_not_specified'] = "Veuillez spécifier une sauvegarde à télécharger.";
+$l['error_invalid_backup'] = "La sauvegarde spécifiée est invalide ou n’existe pas.";
+$l['error_backup_doesnt_exist'] = "La sauvegarde spécifiée n’existe pas.";
+$l['error_backup_not_deleted'] = "La sauvegarde spécifiée n’a pas été supprimée.";
+$l['error_tables_not_selected'] = "Veuillez sélectionner des tables à sauvegarder.";
+$l['error_no_zlib'] = "L’extension zlib pour PHP n’est pas activée — vous ne pouvez pas créer de sauvegardes compressées.";
+
+$l['alert_not_writable'] = "Le répertoire de sauvegarde (admin/backups) n’est pas accessible en écriture. Vous ne pouvez pas enregistrer de sauvegardes sur le serveur.";
+
+$l['confirm_backup_deletion'] = "Êtes-vous sûr de vouloir supprimer cette sauvegarde ?";
+
+$l['success_backup_deleted'] = "La sauvegarde a bien été supprimée.";
+$l['success_backup_created'] = "La sauvegarde a bien été créée.";
