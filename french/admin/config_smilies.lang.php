833b07fa2af4cae1a19f413aa81d73b3b1a5c59a
Axel Fontana <axel.fontana@qualifio.com>
2025-09-30 15:20:48 +0200

    feat: add translation for admin/config-smilies

---
+++ french/admin/config_smilies.lang.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['smilies'] = "Émoticônes";
+$l['manage_smilies'] = "Gestion des émoticônes";
+$l['manage_smilies_desc'] = "Cette section vous permet de gérer les émoticônes disponibles sur votre forum.";
+$l['add_smilie'] = "Ajouter une émoticône";
+$l['add_smilie_desc'] = "Cette page vous permet d’ajouter une nouvelle émoticône.";
+$l['add_multiple_smilies'] = "Ajouter plusieurs émoticônes";
+$l['add_multiple_smilies_desc'] = "Cette page vous permet d’ajouter plusieurs nouvelles émoticônes simultanément.";
+$l['edit_smilie'] = "Modifier une émoticône";
+$l['edit_smilie_desc'] = "Cette page vous permet de modifier une émoticône.";
+$l['mass_edit'] = "Modifications groupées";
+$l['mass_edit_desc'] = "Cette page vous permet de modifier plusieurs émoticônes simultanément.";
+
+$l['no_smilies'] = "Il n’y a actuellement aucune émoticône sur votre forum.";
+
+$l['image'] = "Image";
+$l['name'] = "Nom";
+$l['text_replace'] = "Texte à remplacer";
+$l['text_replace_desc'] = "Texte qui sera remplacé par l’émoticône (une entrée par ligne).";
+$l['image_path'] = "Chemin de l’image";
+$l['image_path_desc'] = "Chemin vers l’image de l’émoticône. Pour utiliser des images différentes en fonction du thème, utilisez <strong>{theme}</strong> pour représenter le répertoire d’images du thème courant.";
+$l['order'] = "Ordre";
+$l['display_order'] = "Ordre d’affichage";
+$l['display_order_desc'] = "Ordre dans lequel cette émoticône apparaîtra dans la liste. Ce nombre ne peut pas être identique à celui d’une autre émoticône.";
+$l['mass_edit_show_clickable'] = "Afficher dans la liste cliquable ?";
+$l['show_clickable'] = "Afficher dans la liste cliquable ?";
+$l['show_clickable_desc'] = "Souhaitez-vous que cette émoticône apparaisse dans la liste cliquable de l’éditeur de messages ?";
+$l['include'] = "Ajouter ?";
+$l['path_to_images'] = "Chemin des images";
+$l['path_to_images_desc'] = "Chemin vers le dossier contenant les images.";
+$l['smilie_delete'] = "Supprimer ?";
+$l['save_smilie'] = "Enregistrer";
+$l['save_smilies'] = "Enregistrer";
+$l['show_smilies'] = "Afficher les émoticônes";
+$l['reset'] = "Réinitialiser";
+
+$l['error_missing_name'] = "Veuillez saisir un nom pour cette émoticône.";
+$l['error_missing_text_replacement'] = "Veuillez saisir un texte de remplacement pour cette émoticône.";
+$l['error_missing_path'] = "Veuillez saisir un chemin pour l’image de cette émoticône.";
+$l['error_missing_path_multiple'] = "Veuillez saisir un chemin pour les images.";
+$l['error_missing_order'] = "Veuillez saisir un ordre d’affichage pour cette émoticône.";
+$l['error_duplicate_order'] = "Veuillez saisir un ordre d’affichage valide pour cette émoticône.";
+$l['error_missing_clickable'] = "Veuillez indiquer si l’émoticône doit apparaître dans la liste cliquable.";
+$l['error_no_smilies'] = "Aucune image n’a été trouvée dans ce répertoire ou toutes les images présentes ont déjà été ajoutées.";
+$l['error_no_images'] = "Aucune image n’a été trouvée dans ce répertoire.";
+$l['error_none_included'] = "Veuillez sélectionner des émoticônes à ajouter.";
+$l['error_invalid_path'] = "Veuillez saisir un chemin valide pour les images.";
+$l['error_invalid_smilie'] = "L’émoticône spécifiée n’existe pas.";
+
+$l['success_smilie_added'] = "L’émoticône a bien été créée.";
+$l['success_multiple_smilies_added'] = "Les émoticônes ont bien été créées.";
+$l['success_smilie_updated'] = "L’émoticône a bien été mise à jour.";
+$l['success_multiple_smilies_updated'] = "Les émoticônes ont bien été mises à jour.";
+$l['success_smilie_deleted'] = "L’émoticône a bien été supprimée.";
+$l['success_mass_edit_updated'] = "Les émoticônes ont bien été mises à jour.";
+
+$l['confirm_smilie_deletion'] = "Êtes-vous sûr de vouloir supprimer cette émoticône ?";
