c53b43caad2a73dc6910eca9269a8caa6aa4968d
Axel Fontana <axel.fontana@qualifio.com>
2025-09-30 09:30:48 +0200

    feat: add translation for admin/tools-optimizedb

---
+++ french/admin/tools_optimizedb.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['optimize_database'] = "Optimisation de la base de données";
+
+$l['table_selection'] = "Sélection des tables";
+$l['tables_select_desc'] = "Vous pouvez sélectionner les tables de la base de données sur lesquelles effectuer cette opération. Maintenez « CTRL » pour en sélectionner plusieurs.";
+$l['select_all'] = "Tout sélectionner";
+$l['deselect_all'] = "Tout désélectionner";
+$l['select_forum_tables'] = "Sélectionner les tables :";
+$l['optimize_selected_tables'] = "Optimiser les tables";
+
+$l['error_no_tables_selected'] = "Veuillez sélectionner des tables à optimiser.";
+
+$l['success_tables_optimized'] = "Les tables sélectionnées ont bien été optimisées et analysées.";
