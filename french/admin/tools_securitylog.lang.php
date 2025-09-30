efd3d6042d0c32ffa23a61ba5e2e343f05bcf6b0
Axel Fontana <axel.fontana@qualifio.com>
2025-09-30 11:07:24 +0200

    feat: add translation for admin/tools-securitylog

---
+++ french/admin/tools_securitylog.lang.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['security_log'] = "Journal de sécurité";
+$l['security_log_desc'] = "Cette section vous permet de gérer le journal de sécurité. Ce journal contient toutes les actions liées à la sécurité des utilisateurs.";
+
+$l['prune_security_log'] = "Purger le journal de sécurité";
+$l['prune_security_log_desc'] = "Cette page vous permet de purger le journal de sécurité en utilisant des critères spécifiques.";
+
+$l['username'] = "Nom d’utilisateur";
+$l['date'] = "Date";
+$l['information'] = "Action";
+$l['ipaddress'] = "Adresse IP";
+
+$l['security_log_failed_login'] = "Échec de connexion";
+$l['security_log_failed_login_admincp'] = "Échec de connexion (panneau d’administration)";
+$l['security_log_login_admincp'] = "Connexion réussie (panneau d’administration)";
+$l['security_log_incorrect_pin'] = "Code PIN du panneau d’administration incorrect";
+$l['security_log_changed_email'] = "Adresse électronique modifiée";
+$l['security_log_changed_password'] = "Mot de passe modifié";
+$l['security_log_lost_password'] = "Courriel de récupération de mot de passe envoyé";
+$l['security_log_reset_password'] = "Courriel de réinitialisation de mot de passe envoyé";
+
+$l['filter_security_logs'] = "Filtrer le journal de sécurité";
+$l['all_users'] = "Tous les utilisateurs";
+$l['all_types'] = "Tous les types d’actions";
+$l['guest'] = "Invité";
+$l['asc'] = "croissant";
+$l['desc'] = "décroissant";
+$l['username_colon'] = "Nom d’utilisateur :";
+$l['type_colon'] = "Type :";
+$l['sort_by'] = "Trier par :";
+$l['results_per_page'] = "Résultats par page :";
+$l['in'] = "dans l’ordre";
+$l['order'] = "";
+
+$l['date_range'] = "Période :";
+$l['older_than'] = "Entrées datant de plus de ";
+$l['days'] = "jours";
+
+$l['no_security_logs'] = "Aucune entrée ne correspond aux critères sélectionnés.";
+
+$l['success_pruned_security_logs'] = "Le journal de sécurité a bien été purgé.";
+$l['note_logs_locked'] = "Pour des raisons de sécurité, les entrées datant de moins de 24 heures ne peuvent pas être purgées.";
+
+$l['na_deleted'] = "N/A - Supprimé";
