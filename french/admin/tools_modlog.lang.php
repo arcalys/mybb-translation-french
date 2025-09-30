a1504e20fb477c51fd2df7fb06a8d84459cc06cf
Axel Fontana <axel.fontana@qualifio.com>
2025-09-30 11:38:36 +0200

    feat: add translation for admin/tools-modlog

---
+++ french/admin/tools_modlog.lang.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['mod_logs'] = "Journal de modération";
+$l['mod_logs_desc'] = "Cette section vous permet de gérer le journal de modération. Ce journal contient les actions que tout utilisateur peut effectuer (suppression de ses propres messages ou sujets) ainsi que les actions effectuées par les modérateurs.";
+$l['prune_mod_logs'] = "Purger le journal de modération";
+$l['prune_mod_logs_desc'] = "Cette page vous permet de purger le journal de modération en utilisant des critères spécifiques.";
+
+$l['no_modlogs'] = "Aucune entrée ne correspond aux critères sélectionnés.";
+
+$l['username'] = "Nom d’utilisateur";
+$l['na_deleted'] = "N/A - Supprimé";
+$l['date'] = "Date";
+$l['action'] = "Action";
+$l['information'] = "Détails";
+$l['ipaddress'] = "Adresse IP";
+
+$l['forum'] = "Forum :";
+$l['thread'] = "Sujet :";
+$l['post'] = "Message :";
+$l['user_info'] = "Utilisateur :";
+$l['announcement'] = "Annonce :";
+
+$l['filter_moderator_logs'] = "Filtrer le journal de modération";
+$l['forum_moderator'] = "Modérateur :";
+$l['sort_by'] = "Trier par :";
+$l['results_per_page'] = "Résultats par page :";
+$l['all_moderators'] = "Tous les modérateurs";
+$l['older_than'] = "Entrées datant de plus de ";
+
+$l['forum_name'] = "Nom du forum";
+$l['thread_subject'] = "Titre du sujet";
+
+$l['asc'] = "croissant";
+$l['desc'] = "décroissant";
+
+$l['in'] = "dans l’ordre";
+$l['order'] = "";
+$l['days'] = "jours";
+
+$l['prune_moderator_logs'] = "Purger le journal de modération";
+$l['date_range'] = "Période :";
+
+$l['success_pruned_mod_logs'] = "Le journal de modération a bien été purgé.";
+$l['note_logs_locked'] = "Pour des raisons de sécurité, les entrées datant de moins de 24 heures ne peuvent pas être purgées.";
