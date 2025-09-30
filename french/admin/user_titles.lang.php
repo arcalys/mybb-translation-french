fc892e146f6021143de6cc2fb4e37370eec529c0
Axel Fontana <axel.fontana@qualifio.com>
2025-09-30 16:00:20 +0200

    feat: add translation for admin/user-titles

---
+++ french/admin/user_titles.lang.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['user_titles'] = "Titres";
+$l['user_titles_desc'] = "Cette section permet de gérer les titres des utilisateurs. Les titres sont attribués en fonction du nombre de messages publiés et peuvent également inclure une image personnalisée.";
+$l['add_new_user_title'] = "Ajouter un titre";
+$l['add_new_user_title_desc'] = "Cette page vous permet d’ajouter un nouveau titre. <em>Attention : il ne s’agit <strong>pas</strong> du <u><a href=\"index.php?module=user-group_promotions\">système de promotions</a></u>.</em>";
+
+$l['error_missing_title'] = "Veuillez saisir un nom pour ce titre.";
+$l['error_missing_posts'] = "Veuillez saisir le nombre minimum de messages pour ce titre.";
+$l['error_cannot_have_same_posts'] = "Ce titre ne peut pas avoir le même nombre minimum de messages qu’un autre titre.";
+$l['error_invalid_user_title'] = "Le titre spécifié est invalide.";
+
+$l['success_user_title_created'] = "Le titre a bien été créé.";
+$l['success_user_title_updated'] = "Le titre a bien été mis à jour.";
+$l['success_user_title_deleted'] = "Le titre a bien été supprimé.";
+
+$l['title_to_assign'] = "Titre attribué";
+$l['title_to_assign_desc'] = "Ce titre sera affiché sous le nom des utilisateurs qui n’ont pas défini de titre personnalisé.";
+$l['minimum_posts'] = "Nombre minimum de messages";
+$l['minimum_posts_desc'] = "Le nombre minimum de messages qu’un utilisateur doit avoir publiés avant que ce titre lui soit attribué.";
+$l['number_of_stars'] = "Nombre d’étoiles";
+$l['number_of_stars_desc'] = "Le nombre d’étoiles à afficher sous ce titre. Entrez 0 pour n’afficher aucune étoile.";
+$l['star_image'] = "Image utilisée pour les étoiles";
+$l['star_image_desc'] = "Si ce titre doit afficher des étoiles, indiquez le chemin de l’image correspondante. Si ce champ est laissé vide, l’image du groupe d’utilisateurs sera utilisée. Utilisez <strong>{theme}</strong> pour représenter le répertoire d’images du thème courant.";
+$l['save_user_title'] = "Enregistrer";
+$l['edit_user_title'] = "Modifier un titre";
+$l['edit_user_title_desc'] = "Cette page vous permet de modifier un titre.";
+$l['user_title_deletion_confirmation'] = "Êtes-vous sûr de vouloir supprimer ce titre ?";
+$l['manage_user_titles'] = "Gestion des titres";
+$l['user_title'] = "Titre";
+$l['no_user_titles'] = "Il n’y a actuellement aucun titre défini sur votre forum.";
