0a7bd2189e9004be6ba0f027cf0c1a060361c2ae
Axel Fontana <axel.fontana@qualifio.com>
2025-10-01 11:41:22 +0200

    feat: add translation for admin/config-spiders

---
+++ french/admin/config_spiders.lang.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['spiders_bots'] = "Robots d’indexation";
+$l['spiders_bots_desc'] = "Cette page vous permet de gérer les robots automatiquement détectés par votre forum et de consulter leur dernière visite.";
+$l['add_new_bot'] = "Ajouter un robot";
+$l['add_new_bot_desc'] = "Cette page vous permet d’ajouter un nouveau robot que votre forum pourra détecter.";
+
+$l['edit_bot'] = "Modifier un robot";
+$l['edit_bot_desc'] = "Cette page vous permet de modifier un robot existant.";
+
+$l['bot'] = "Robot";
+$l['last_visit'] = "Dernière visite";
+$l['no_bots'] = "Il n’y a actuellement aucun robot enregistré par ce forum.";
+
+$l['name'] = "Nom";
+$l['name_desc'] = "Saisissez un nom qui servira à identifier ce robot.";
+$l['user_agent'] = "Agent utilisateur";
+$l['user_agent_desc'] = "Saisissez l’agent utilisateur du robot (les correspondances partielles sont acceptées).";
+$l['language_str'] = "Langue";
+$l['language_desc'] = "Sélectionnez la langue utilisée par le robot pour naviguer sur le forum.";
+$l['theme'] = "Thème";
+$l['theme_desc'] = "Sélectionnez le thème utilisé par le robot pour naviguer sur le forum.";
+$l['user_group'] = "Groupe";
+$l['user_group_desc'] = "Sélectionnez le groupe dont les permissions s’appliqueront au robot (il est recommandé de laisser la valeur par défaut).";
+$l['save_bot'] = "Enregistrer";
+$l['use_board_default'] = "Utiliser la valeur par défaut";
+
+$l['error_invalid_bot'] = "Le robot spécifié n’existe pas.";
+$l['error_missing_name'] = "Veuillez saisir un nom pour ce robot.";
+$l['error_missing_agent'] = "Veuillez saisir un agent utilisateur pour ce robot.";
+
+$l['success_bot_created'] = "Le robot a bien été créé.";
+$l['success_bot_updated'] = "Le robot a bien été mis à jour.";
+$l['success_bot_deleted'] = "Le robot a bien été supprimé.";
+
+$l['confirm_bot_deletion'] = "Êtes-vous sûr de vouloir supprimer ce robot ?";
