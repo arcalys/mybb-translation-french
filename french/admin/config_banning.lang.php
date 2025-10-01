644124bdf605306066c5a17ad30b30d0631bb1a3
Axel Fontana <axel.fontana@qualifio.com>
2025-10-01 17:06:26 +0200

    feat: add translation for admin/config-banning

---
+++ french/admin/config_banning.lang.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['banning'] = "Bannissements";
+$l['banned_ips'] = "Adresses IP bannies";
+$l['banned_ips_desc'] = "Cette page vous permet de gérer les adresses IP interdites d’accès à votre forum.";
+$l['banned_accounts'] = "Comptes bannis";
+$l['disallowed_usernames'] = "Noms d’utilisateur interdits";
+$l['disallowed_usernames_desc'] = "Cette page vous permet de gérer la liste des noms d’utilisateur qui ne peuvent pas être enregistrés ou utilisés. Cela peut également servir à réserver certains noms.";
+$l['disallowed_email_addresses'] = "Adresses électroniques interdites";
+$l['disallowed_email_addresses_desc'] = "Cette page vous permet de gérer la liste des adresses électroniques qui ne peuvent pas être enregistrées ou utilisées.";
+
+$l['banned_ip_addresses'] = "Adresses IP bannies";
+$l['username'] = "Nom d’utilisateur";
+$l['date_disallowed'] = "Date d’interdiction";
+$l['last_attempted_use'] = "Dernière tentative d’utilisation";
+$l['email_address'] = "Adresse électronique";
+$l['ip_address'] = "Adresse IP";
+$l['ban_date'] = "Date du bannissement";
+$l['last_access'] = "Dernier accès";
+$l['no_bans'] = "Il n’y a actuellement aucune entrée bannie.";
+
+$l['add_disallowed_username'] = "Interdire un nom d’utilisateur";
+$l['username_desc'] = "Remarque : utilisez * pour représenter n’importe quelle suite de caractères.";
+$l['disallow_username'] = "Interdire";
+
+$l['add_disallowed_email_address'] = "Interdire une adresse électronique";
+$l['email_address_desc'] = "Remarque : utilisez * pour représenter n’importe quelle suite de caractères.";
+$l['disallow_email_address'] = "Interdire";
+
+$l['ban_an_ip_address'] = "Bannir une adresse IP";
+$l['ip_address_desc'] = "Remarque : pour bannir une plage d’adresses IP, utilisez * (par ex. : 127.0.0.*) ou la notation CIDR (par ex. : 127.0.0.0/8).";
+$l['ban_ip_address'] = "Bannir";
+
+$l['error_missing_ban_input'] = "Veuillez saisir une valeur à bannir ou interdire.";
+$l['error_invalid_filter'] = "Le filtre spécifié n’est pas valide.";
+$l['error_filter_not_found'] = "Le filtre spécifié n’existe pas.";
+$l['error_filter_already_banned'] = "Le filtre que vous avez saisi est déjà interdit.";
+
+$l['success_ip_banned'] = "L’adresse IP a bien été bannie.";
+$l['success_username_disallowed'] = "Le nom d’utilisateur a bien été interdit.";
+$l['success_email_disallowed'] = "L’adresse électronique a bien été interdite.";
+$l['success_ban_deleted'] = "Le bannissement ou l’interdiction spécifié a bien été supprimé.";
+
+$l['confirm_ban_deletion'] = "Êtes-vous sûr de vouloir supprimer ce bannissement ou cette interdiction ?";
