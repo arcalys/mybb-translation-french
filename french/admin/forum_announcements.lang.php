81a1b4f8843dcfeafd7de07c50f9a664d5e3a513
Axel Fontana <axel.fontana@qualifio.com>
2025-10-01 12:12:23 +0200

    feat: add translation for admin/forum-announcements

---
+++ french/admin/forum_announcements.lang.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['forum_announcements'] = "Annonces";
+$l['forum_announcements_desc'] = "Cette page vous permet de gérer les annonces de votre forum. Les annonces globales s’affichent dans tous les forums tandis que celles définies pour un forum spécifique s’affichent dans ce forum et tous ses sous-forums.";
+$l['add_announcement'] = "Ajouter une annonce";
+$l['add_announcement_desc'] = "Cette page vous permet d’ajouter une annonce pour un forum spécifique ou pour tous les forums.";
+$l['update_announcement'] = "Modifier une annonce";
+$l['preview_announcement'] = "Prévisualiser";
+$l['update_announcement_desc'] = "Cette page vous permet de modifier les détails d’une annonce.";
+
+$l['start_date_desc'] = "À cette date et heure (UTC), l’annonce apparaîtra dans le(s) forum(s) sélectionné(s).";
+$l['end_date_desc'] = "À cette date et heure (UTC), l’annonce sera masquée sur votre forum. Vous pouvez également choisir d’en faire une annonce permanente.";
+$l['forums_to_appear_in_desc'] = "L’annonce apparaîtra dans les forums sélectionnés ci-dessous ainsi que dans tous leurs sous-forums.";
+
+$l['announcement'] = "Annonce";
+$l['global_announcements'] = "Annonces globales";
+
+$l['no_global_announcements'] = "Il n’y a actuellement aucune annonce globale.";
+$l['no_forums'] = "Il n’y a actuellement aucun forum configuré pour afficher des annonces.";
+
+$l['confirm_announcement_deletion'] = "Êtes-vous sûr de vouloir supprimer cette annonce ?";
+
+$l['success_announcement_deleted'] = "L’annonce a bien été supprimée.";
+$l['success_added_announcement'] = "L’annonce a bien été créée.";
+$l['success_updated_announcement'] = "L’annonce a bien été mise à jour.";
+
+$l['error_invalid_announcement'] = "L’annonce specifiée est invalide ou n’existe pas.";
+$l['error_missing_title'] = "Veuillez saisir un titre pour cette annonce.";
+$l['error_missing_message'] = "Veuillez saisir un message pour cette annonce.";
+$l['error_missing_forum'] = "Veuillez sélectionner au minimum un forum.";
+$l['error_invalid_start_date'] = "Veuillez saisir une date de début valide.";
+$l['error_invalid_end_date'] = "Veuillez saisir une date de fin valide.";
+$l['error_end_before_start'] = "La date de fin ne peut pas être antérieure à la date de début.";
+
+$l['add_an_announcement'] = "Détails de l’annonce";
+$l['update_an_announcement'] = "Détails de l’annonce";
+$l['save_announcement'] = "Enregistrer";
+$l['title'] = "Titre";
+$l['start_date'] = "Date de début";
+$l['end_date'] = "Date de fin";
+$l['message'] = "Message";
+$l['forums_to_appear_in'] = "Forums";
+$l['allow_html'] = "Autoriser l’HTML?";
+$l['allow_mycode'] = "Autoriser les MyCode ?";
+$l['allow_smilies'] = "Autoriser les émoticônes ?";
+$l['time'] = "Heure :";
+$l['set_time'] = "Définir la date et l’heure";
+
+$l['announcement_preview'] = "Prévisualiser";
