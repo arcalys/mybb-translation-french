ef5c27da6a0fd20b0bb3399ecc9a739d33b63b7d
Axel Fontana <axel.fontana@qualifio.com>
2025-10-04 23:07:37 +0200

    feat: add translation for admin/config-thread-prefixes

---
+++ french/admin/config_thread_prefixes.lang.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['thread_prefixes'] = "Préfixes de sujets";
+$l['thread_prefixes_in'] = "Préfixes de sujets dans « {1} »";
+$l['thread_prefixes_desc'] = "Cette page vous permet de gérer les préfixes que les utilisateurs peuvent attribuer à leurs sujets. Les sujets peuvent alors être filtrés par préfixe dans un forum.";
+
+$l['add_new_thread_prefix'] = "Ajouter un nouveau préfixe";
+$l['add_new_thread_prefix_desc'] = "Cette page vous permet d’ajouter un nouveau préfixe et de définir où et pour qui il sera disponible.";
+
+$l['edit_prefix'] = "Modifier le préfixe";
+$l['edit_prefix_desc'] = "Cette page vous permet de modifier un préfixe de sujet.";
+$l['edit_thread_prefix'] = "Modifier un préfixe";
+$l['delete_thread_prefix'] = "Supprimer le préfixe";
+
+$l['prefix_options'] = "Options du préfixe";
+$l['save_thread_prefix'] = "Enregistrer";
+
+$l['prefix'] = "Nom du préfixe";
+$l['forums'] = "Disponibilité dans les forums";
+$l['prefix_desc'] = "Version texte du préfixe qui sera affichée dans les menus de sélection.";
+$l['display_style'] = "Style d’affichage";
+$l['display_style_desc'] = "Définissez le style d’affichage du préfixe. Vous pouvez entrer du code HTML ici ou utiliser la même valeur que pour le nom.";
+$l['available_in_forums'] = "Disponibilité dans les forums";
+$l['available_to_groups'] = "Disponibilité pour les groupes";
+
+$l['no_thread_prefixes'] = "Il n’y a actuellement aucun préfixe de sujet sur votre forum.";
+
+$l['confirm_thread_prefix_deletion'] = "Êtes-vous sûr de vouloir supprimer ce préfixe ? Attention : vous devrez également mettre à jour les outils de modération personnalisés qui l’utilisaient.";
+
+$l['success_thread_prefix_created'] = "Le préfixe a bien été créé.";
+$l['success_thread_prefix_updated'] = "Le préfixe a bien été mis à jour.";
+$l['success_thread_prefix_deleted'] = "Le préfixe a bien été supprimé. Vous devrez également mettre à jour les outils de modération personnalisés qui l’utilisaient.";
+
+$l['error_missing_prefix'] = "Veuillez saisir un nom pour ce préfixe.";
+$l['error_missing_display_style'] = "Veuillez saisir un style d’affichage pour ce préfixe.";
+$l['error_no_forums_selected'] = "Veuillez sélectionner les forums dans lesquels ce préfixe sera disponible.";
+$l['error_no_groups_selected'] = "Veuillez sélectionner les groupes pour lesquels ce préfixe sera disponible.";
+$l['error_invalid_prefix'] = "Le préfixe spécifié est invalide ou n’existe pas.";
