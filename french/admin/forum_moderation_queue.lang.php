dd357adc2131d671f1be70a69067ed3aafcd9a57
Axel Fontana <axel.fontana@qualifio.com>
2025-10-04 22:50:30 +0200

    feat: add translation for admin/forum-moderation-queue

---
+++ french/admin/forum_moderation_queue.lang.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+// Tabs
+$l['moderation_queue'] = "File de modération";
+$l['threads'] = "Sujets";
+$l['threads_desc'] = "Cette page vous permet de gérer les sujets en attente de modération.";
+$l['posts'] = "Messages";
+$l['posts_desc'] = "Cette page vous permet de gérer les messages en attente de modération.";
+$l['attachments'] = "Pièces jointes";
+$l['attachments_desc'] = "Cette page vous permet de gérer les pièces jointes en attente de modération.";
+$l['threads_awaiting_moderation'] = "Sujets en attente de modération";
+$l['posts_awaiting_moderation'] = "Messages en attente de modération";
+$l['attachments_awaiting_moderation'] = "Pièces jointes en attente de modération";
+
+// Errors
+$l['error_no_posts'] = "Il n’y a actuellement aucun message en attente de modération.";
+$l['error_no_attachments'] = "Il n’y a actuellement aucune pièce jointe en attente de modération.";
+$l['error_no_threads'] = "Il n’y a actuellement aucun sujet, message ou pièce jointe en attente de modération.";
+
+// Success
+$l['success_threads'] = "Les sujets sélectionnés ont bien été modérés.";
+$l['success_posts'] = "Les messages sélectionnés ont bien été modérés.";
+$l['success_attachments'] = "Les pièces jointes sélectionnées ont bien été modérées.";
+
+// Pages
+$l['subject'] = "Sujet";
+$l['author'] = "Auteur";
+$l['posted'] = "Publié";
+$l['ignore'] = "Ignorer";
+$l['approve'] = "Approuver";
+$l['forum'] = "Forum :";
+$l['thread'] = "Sujet :";
+$l['post'] = "Pièce jointe :";
+$l['re'] = "RE :";
+$l['filename'] = "Nom du fichier";
+$l['uploadedby'] = "Ajouté par";
+$l['controls'] = "Actions";
+
+// Buttons
+$l['mark_as_ignored'] = "Tout ignorer";
+$l['mark_as_deleted'] = "Tout supprimer";
+$l['mark_as_approved'] = "Tout approuver";
+$l['perform_action'] = "Appliquer";
