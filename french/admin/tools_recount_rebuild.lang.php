fca08664649d5f27529d9c6a6b4d86ccdc43b8ef
Axel Fontana <axel.fontana@qualifio.com>
2025-10-05 20:30:14 +0200

    feat: add translation for admin/tools-recount-rebuild

---
+++ french/admin/tools_recount_rebuild.lang.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['recount_rebuild'] = "Recalculer et reconstruire";
+$l['recount_rebuild_desc'] = "Cette page vous permet de recalculer et reconstruire les données afin de corriger d’éventuelles erreurs de synchronisation sur votre forum.";
+
+$l['data_per_page'] = "Entrées par page";
+$l['recount_stats'] = "Recalculer les statistiques";
+$l['recount_stats_desc'] = "Cet outil recalculera et mettra à jour les statistiques de votre forum.";
+$l['recount_reputation'] = "Recalculer la réputation";
+$l['recount_reputation_desc'] = "Cet outil recalculera la réputation de tous les utilisateurs.";
+$l['recount_warning'] = "Recalculer les niveaux d’avertissement";
+$l['recount_warning_desc'] = "Cet outil recalculera les niveaux d’avertissements de tous les utilisateurs.";
+$l['recount_private_messages'] = "Recalculer les messages privés";
+$l['recount_private_messages_desc'] = "Cet outil recalculera le nombre de messages privés de tous les utilisateurs.";
+$l['recount_referrals'] = "Recalculer les parrainages";
+$l['recount_referrals_desc'] = "Cet outil recalculera le nombre de parrainages de tous les utilisateurs.";
+$l['rebuild_forum_counters'] = "Reconstruire les compteurs des forums";
+$l['rebuild_forum_counters_desc'] = "Cet outil mettra à jour les compteurs de messages et de sujets ainsi que le dernier message de chaque forum.";
+$l['rebuild_thread_counters'] = "Reconstruire les compteurs des sujets";
+$l['rebuild_thread_counters_desc'] = "Cet outil mettra à jour les compteurs de messages et de vues ainsi que le dernier message de chaque sujet.";
+$l['rebuild_poll_counters'] = "Reconstruire les compteurs des sondages";
+$l['rebuild_poll_counters_desc'] = "Cet outil mettra à jour les compteurs de votes et le nombre total de votes de tous les sondages.";
+$l['recount_user_posts'] = "Recalculer le nombre de messages des utilisateurs";
+$l['recount_user_posts_desc'] = "Cet outil mettra à jour le nombre de messages de tous les utilisateurs en fonction des messages présents dans la base de données et des forums où le comptage des messages est désactivé.";
+$l['recount_user_threads'] = "Recalculer le nombre de sujets des utilisateurs";
+$l['recount_user_threads_desc'] = "Cet outil mettra à jour le nombre de sujets de tous les utilisateurs en fonction des sujets présents dans la base de données et des forums où le comptage des sujets est désactivé.";
+$l['rebuild_attachment_thumbs'] = "Reconstruire les miniatures des pièces jointes";
+$l['rebuild_attachment_thumbs_desc'] = "Cet outil reconstruira les miniatures des pièces jointes afin qu’elles utilisent les dimensions actuellement définies pour le forum. Cela régénérera également les miniatures manquantes.";
+
+$l['success_rebuilt_forum_counters'] = "Les compteurs des forums ont bien été reconstruits.";
+$l['success_rebuilt_thread_counters'] = "Les compteurs des sujets ont bien été reconstruits.";
+$l['success_rebuilt_poll_counters'] = "Les compteurs des sondages ont bien été reconstruits.";
+$l['success_rebuilt_user_post_counters'] = "Le nombre de messages des utilisateurs a bien été recalculé.";
+$l['success_rebuilt_user_thread_counters'] = "Le nombre de sujets des utilisateurs a bien été recalculé.";
+$l['success_rebuilt_attachment_thumbnails'] = "Les miniatures des pièces jointes ont bien été reconstruites.";
+$l['success_rebuilt_forum_stats'] = "Les statistiques du forum ont bien été reconstruites.";
+$l['success_rebuilt_reputation'] = "La réputation des utilisateurs a bien été recalculée.";
+$l['success_rebuilt_warning'] = "Les points d’avertissement des utilisateurs ont bien été recalculés.";
+$l['success_rebuilt_private_messages'] = "Le nombre de messages privés des utilisateurs a bien été recalculé.";
+$l['success_rebuilt_referral'] = "Le nombre de parrainages des utilisateurs a bien été recalculé.";
+$l['success_rebuilt_thread_ratings'] = "Les évaluations des sujets ont bien été recalculées.";
+
+$l['confirm_proceed_rebuild'] = "Cliquez sur « Continuer » pour lancer le processus de recalcul et de reconstruction.";
+$l['automatically_redirecting'] = "Redirection automatique…";
