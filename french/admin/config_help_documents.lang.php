11db85b6bbc382c37afdd2448aff908cc6703ab0
Axel Fontana <axel.fontana@qualifio.com>
2025-10-08 22:18:32 +0200

    feat: add translation for config-help-documents

---
+++ french/admin/config_help_documents.lang.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['help_documents'] = "Documents d’aide";
+$l['add_new_section'] = "Ajouter une section";
+$l['add_new_section_desc'] = "Cette page vous permet d’ajouter une nouvelle section d’aide.";
+$l['add_new_document'] = "Ajouter un document";
+$l['add_new_document_desc'] = "Cette page vous permet d’ajouter un nouveau document d’aide.";
+$l['edit_section'] = "Modifier une section";
+$l['edit_section_desc'] = "Cette page vous permet de modifier une section d’aide.";
+$l['edit_document'] = "Modifier un document";
+$l['edit_document_desc'] = "Cette page vous permet de modifier un document d’aide.";
+$l['manage_help_documents'] = "Gestion des documents d’aide";
+$l['manage_help_documents_desc'] = "Cette page vous permet de gérer les documents d’aide de votre forum.";
+
+$l['title'] = "Nom";
+$l['short_description'] = "Brève description";
+$l['display_order'] = "Ordre d’affichage";
+$l['enabled'] = "Activé ?";
+$l['use_translation'] = "Utiliser les traductions ?";
+$l['add_section'] = "Ajouter une section";
+$l['add_document'] = "Ajouter un document";
+$l['save_section'] = "Enregistrer";
+$l['save_document'] = "Enregistrer";
+$l['section'] = "Section";
+$l['document'] = "Contenu";
+$l['id'] = "identifiant :";
+$l['custom_doc_sec'] = "Document/section personnalisé(e)";
+$l['default_doc_sec'] = "Document/section par défaut";
+$l['no_help_documents'] = "Il n’y a actuellement aucun document d’aide sur votre forum.";
+$l['section_document'] = "Section / Document";
+
+$l['error_section_missing_name'] = "Veuillez saisir un nom pour cette section.";
+$l['error_section_missing_description'] = "Veuillez saisir une brève description pour cette section.";
+$l['error_section_missing_enabled'] = "Veuillez saisir une valeur pour « Activé ? ».";
+$l['error_section_missing_translation'] = "Veuillez saisir une valeur pour « Utiliser la traduction ? ».";
+$l['error_missing_sid'] = "Veuillez saisir une section pour ce document.";
+$l['error_document_missing_name'] = "Veuillez saisir un nom pour ce document.";
+$l['error_document_missing_description'] = "Veuillez saisir une brève description pour ce document.";
+$l['error_document_missing_document'] = "Veuillez saisir un contenu pour ce document.";
+$l['error_document_missing_enabled'] = "Veuillez saisir une valeur pour « Activé ? ».";
+$l['error_document_missing_translation'] = "Veuillez saisir une valeur pour « Utiliser la traduction ? ».";
+$l['error_invalid_sid'] = "Document invalide spécifié.";
+$l['error_missing_section_id'] = "La section spécifiée est invalide ou n’existe pas.";
+$l['error_missing_hid'] = "Le document spécifié est invalide ou n’existe pas.";
+
+$l['success_help_section_added'] = "La section a bien été ajoutée.";
+$l['success_help_document_added'] = "Le document a bien été ajouté.";
+$l['success_help_section_updated'] = "La section a bien été mise à jour.";
+$l['success_help_document_updated'] = "Le document a bien été mis à jour.";
+$l['success_section_deleted'] = "La section a bien été supprimée.";
+$l['success_document_deleted'] = "Le document a bien été supprimé.";
+
+$l['confirm_section_deletion'] = "Êtes-vous sûr de vouloir supprimer cette section ?";
+$l['confirm_document_deletion'] = "Êtes-vous sûr de vouloir supprimer ce document ?";
