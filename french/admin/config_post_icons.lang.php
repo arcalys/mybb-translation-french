c2ecd97bfcaabc54b8958827bb6f0b0ccc9d2652
Axel Fontana <axel.fontana@qualifio.com>
2025-10-08 22:53:01 +0200

    feat: add translation for config-posticons

---
+++ french/admin/config_post_icons.lang.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['post_icons'] = "Icônes de message";
+$l['add_post_icon'] = "Ajouter une icône";
+$l['add_post_icon_desc'] = "Cette page vous permet d’ajouter une nouvelle icône de message.";
+$l['add_multiple_post_icons'] = "Ajouter plusieurs icônes";
+$l['add_multiple_post_icons_desc'] = "Cette page vous permet d’ajouter plusieurs nouvelles icônes de message simultanément.";
+$l['edit_post_icon'] = "Modifier une icône de message";
+$l['edit_post_icon_desc'] = "Cette page vous permet de modifier une icône de message.";
+$l['manage_post_icons'] = "Gestion des icônes de message";
+$l['manage_post_icons_desc'] = "Cette section vous permet de gérer les icônes de message disponibles sur votre forum.";
+
+$l['name_desc'] = "Nom de l’icône de message.";
+$l['image_path'] = "Chemin de l’image";
+$l['image_path_desc'] = "Chemin vers l’image de l’icône. Pour utiliser des images différentes en fonction du thème, utilisez <strong>{theme}</strong> pour représenter le répertoire d’images du thème courant.";
+$l['save_post_icon'] = "Enregistrer";
+$l['reset'] = "Réinitialiser";
+
+$l['path_to_images'] = "Chemin des images";
+$l['path_to_images_desc'] = "Chemin vers les images des icônes.";
+$l['show_post_icons'] = "Afficher les icônes";
+$l['image'] = "Image";
+$l['add'] = "Ajouter ?";
+$l['save_post_icons'] = "Enregistrer";
+
+$l['no_post_icons'] = "Il n’y a actuellement aucune icône de message sur votre forum.";
+
+$l['error_missing_name'] = "Veuillez saisir un nom pour cette icône.";
+$l['error_missing_path'] = "Veuillez saisir un chemin pour l’image de cette icône.";
+$l['error_missing_path_multiple'] = "Veuillez saisir un chemin pour les images des icônes.";
+$l['error_invalid_path'] = "Veuillez saisir un chemin valide.";
+$l['error_no_images'] = "Aucune icône de message n’a été trouvée dans le répertoire spécifié ou toutes les icônes ont déjà été ajoutées.";
+$l['error_none_included'] = "Vous n’avez sélectionné aucune icône à inclure.";
+$l['error_invalid_post_icon'] = "L’icône spécifiée est invalide ou n’existe pas.";
+
+$l['success_post_icon_added'] = "L’icône a bien été ajoutée.";
+$l['success_post_icons_added'] = "Les icônes sélectionnées ont bien été ajoutées.";
+$l['success_post_icon_updated'] = "L’icône a bien été mise à jour.";
+$l['success_post_icon_deleted'] = "L’icône a bien été supprimée.";
+
+$l['confirm_post_icon_deletion'] = "Êtes-vous sûr de vouloir supprimer cette icône ?";
