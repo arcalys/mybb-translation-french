f1300f054a2147a77f88493c1761eb3e3e3a727d
Axel Fontana <axel.fontana@qualifio.com>
2025-10-08 22:38:08 +0200

    feat: add translation for user-banning

---
+++ french/admin/user_banning.lang.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+// Tabs
+$l['banning'] = "Bannissements";
+$l['banned_accounts'] = "Comptes bannis";
+$l['banned_accounts_desc'] = "Cette page vous permet de gérer les comptes bannis de votre forum.";
+$l['ban_a_user'] = "Bannir un utilisateur";
+$l['ban_a_user_desc'] = "Cette page vous permet de bannir un utilisateur.";
+$l['edit_ban'] = "Modifier le bannissement";
+$l['edit_ban_desc'] = "Cette page vous permet de modifier le bannissement d’un compte utilisateur.";
+$l['banned_ips'] = "Adresses IP bannies";
+$l['disallowed_usernames'] = "Noms d’utilisateur interdits";
+$l['disallowed_email_addresses'] = "Adresses électroniques interdites";
+
+// Errors
+$l['error_invalid_ban'] = "Le bannissement sélectionné est invalide ou n’existe pas.";
+
+// Success
+$l['success_ban_lifted'] = "Le bannissement sélectionné a bien été levé.";
+$l['success_banned'] = "L’utilisateur sélectionné a bien été banni.";
+$l['success_ban_updated'] = "Le bannissement sélectionné a bien été mis à jour.";
+$l['success_pruned'] = "Les sujets et messages de l’utilisateur sélectionné ont bien été purgés.";
+
+// Confirm
+$l['confirm_lift_ban'] = "Êtes-vous sûr de vouloir lever ce bannissement ?";
+$l['confirm_prune'] = "Êtes-vous sûr de vouloir purger tous les sujets et messages de cet utilisateur ?";
+
+//== Pages
+//= Add / Edit
+$l['ban_username'] = "Nom d’utilisateur <em>*</em>";
+$l['autocomplete_enabled'] = "L’autocomplétion est activée pour ce champ.";
+$l['ban_reason'] = "Raison";
+$l['ban_group'] = "Groupe de bannissement <em>*</em>";
+$l['ban_group_desc'] = "Pour bannir cet utilisateur, il doit être déplacé dans un groupe dédié aux utilisateurs bannis.";
+$l['ban_time'] = "Durée <em>*</em>";
+
+//= Index
+$l['user'] = "Utilisateur";
+$l['moderation'] = "Modération";
+$l['ban_lifts_on'] = "Expiration";
+$l['time_left'] = "Temps restant";
+$l['permanently'] = "de manière permanente";
+$l['na'] = "N/A";
+$l['for'] = "pour";
+$l['bannedby_x_on_x'] = "<strong>{1}</strong><br /><small>Banni {4} par {2} ({3})</small>";
+$l['lift'] = "Lever le bannissement";
+$l['no_banned_users'] = "Il n’y a actuellement aucun utilisateur banni sur votre forum.";
+$l['prune_threads_and_posts'] = "Purger les sujets et messages";
+
+// Buttons
+$l['ban_user'] = "Bannir l’utilisateur";
+$l['update_ban'] = "Enregistrer";
