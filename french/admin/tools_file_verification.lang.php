28ecc43d7a2b7e1eacacca91f17171ef103399c1
Axel Fontana <axel.fontana@qualifio.com>
2025-10-08 08:53:07 +0200

    feat: add translation for admin/tools-file-verification

---
+++ french/admin/tools_file_verification.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['file_verification'] = "Vérification des fichiers";
+$l['checking'] = "Vérification…";
+$l['file_verification_message'] = "Cette opération vérifiera tous les fichiers par défaut de votre forum afin d’en contrôler l’intégrité. Cliquez sur « Oui » pour commencer la vérification.<br /> <small>Il est important de préciser que cette opération ne peut pas faire la distinction entre des modifications volontaires de vos fichiers et une véritable corruption. Soyez donc prudent avant de restaurer les fichiers signalés comme « corrompus ». Les fichiers corrompus ou manquants peuvent être remplacés en les copiant depuis une <a href=\"https://mybb.com/download\" target=\"_blank\" rel=\"noopener\">archive MyBB officielle</a>.</small>";
+
+$l['file_verification_checksums_missing'] = 'Impossible de vérifier les fichiers car le fichier <code>{1}</code> est manquant. Vous pouvez le télécharger depuis une <a href="https://mybb.com/download" target="_blank" rel="noopener">archive MyBB officielle</a>. Ce fichier peut ne pas être inclus dans les versions destinées au développement.';
+$l['file'] = "Fichier";
+$l['no_corrupt_files_found'] = "Aucun fichier corrompu n’a été trouvé sur votre installation.";
+$l['found_problems'] = "Problèmes détectés";
+$l['no_problems_found'] = "Aucun problème n’a été détecté";
+$l['changed'] = "Modifié";
+$l['missing'] = "Manquant";
+$l['status'] = "Statut";
