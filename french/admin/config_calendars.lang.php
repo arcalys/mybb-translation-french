6b7042493b61e60f0c1abe504fdb922cfb3fd4f8
Axel Fontana <axel.fontana@qualifio.com>
2025-10-08 18:17:44 +0200

    feat: add translation for config-calendars

---
+++ french/admin/config_calendars.lang.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['calendars'] = "Calendriers";
+$l['manage_calendars'] = "Gestion des calendriers";
+$l['manage_calendars_desc'] = "Cette section vous permet de gérer les calendriers de votre forum.";
+$l['add_calendar'] = "Ajouter un calendrier";
+$l['add_calendar_desc'] = "Cette page vous permet de créer un nouveau calendrier.";
+$l['edit_calendar'] = "Modifier un calendrier";
+$l['edit_calendar_desc'] = "Cette page vous permet de modifier un calendrier existant.";
+
+$l['calendar'] = "Calendrier";
+$l['order'] = "Ordre";
+$l['no_calendars'] = "Il n’y a actuellement aucun calendrier sur votre forum.";
+$l['save_calendar_orders'] = "Mettre à jour l’ordre d’affichage";
+
+$l['name'] = "Nom";
+$l['display_order'] = "Ordre d’affichage";
+$l['display_order_desc'] = "Ordre dans lequel ce calendrier apparaîtra dans la liste de choix des calendriers. Le calendrier correspondant à l’ordre « 1 » sera affiché par défaut.";
+$l['week_start'] = "Début de la semaine";
+$l['week_start_desc'] = "Définissez le jour de début de semaine pour ce calendrier.";
+$l['sunday'] = "Dimanche";
+$l['monday'] = "Lundi";
+$l['tuesday'] = "Mardi";
+$l['wednesday'] = "Mercredi";
+$l['thursday'] = "Jeudi";
+$l['friday'] = "Vendredi";
+$l['saturday'] = "Samedi";
+$l['event_limit'] = "Limite d’événements";
+$l['event_limit_desc'] = "Nombre d’événements à afficher avant de remplacer l’affichage par un lien vers la liste des événements.";
+$l['show_birthdays'] = "Afficher les anniversaires ?";
+$l['show_birthdays_desc'] = "Souhaitez‑vous afficher les anniversaires des membres dans ce calendrier ?";
+$l['moderate_events'] = "Modérer les nouveaux événements ?";
+$l['moderate_events_desc'] = "Si cette option est activée, tous les événements devront être modérés, sauf ceux créés par les membres ayant la permission « Ignorer la modération » pour ce calendrier.";
+$l['allow_html'] = "Autoriser l’HTML dans les événements ?";
+$l['allow_mycode'] = "Autoriser les MyCodes dans les événements ?";
+$l['allow_img'] = "Autoriser les balises [IMG] dans les événements ?";
+$l['allow_video'] = "Autoriser les balises [VIDEO] dans les événements ?";
+$l['allow_smilies'] = "Autoriser les émoticônes dans les événements ?";
+$l['save_calendar'] = "Enregistrer";
+
+$l['permissions'] = "Modifier les permissions";
+$l['edit_permissions'] = "Modifier les permissions du calendrier";
+$l['calendar_permissions_for'] = "Permissions du calendrier :";
+$l['permissions_group'] = "Groupe";
+$l['permissions_view'] = "Visualiser";
+$l['permissions_post_events'] = "Publier des événements";
+$l['permissions_bypass_moderation'] = "Ignorer la modération";
+$l['permissions_moderator'] = "Permissions de modération";
+$l['permissions_all'] = "Toutes les permissions ?";
+$l['permissions_use_group_default'] = "Utiliser les valeurs par défaut du groupe";
+$l['save_permissions'] = "Enregistrer";
+
+$l['error_invalid_calendar'] = "Le calendrier spécifié est invalide ou n’existe pas.";
+$l['error_missing_name'] = "Veuillez saisir un nom pour ce calendrier.";
+$l['error_missing_order'] = "Veuillez saisir un ordre d’affichage pour ce calendrier.";
+
+$l['success_calendar_created'] = "Le calendrier a bien été créé.";
+$l['success_calendar_updated'] = "Le calendrier a bien été mis à jour.";
+$l['success_calendar_permissions_updated'] = "Les permissions du calendrier ont bien été mises à jour.";
+$l['success_calendar_deleted'] = "Le calendrier a bien été supprimé.";
+$l['success_calendar_orders_updated'] = "L’ordre d’affichage des calendriers a bien été mis à jour.";
+
+$l['confirm_calendar_deletion'] = "Êtes-vous sûr de vouloir supprimer ce calendrier ?";
