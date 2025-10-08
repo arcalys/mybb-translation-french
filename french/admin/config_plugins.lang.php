7cdcff053e297269aba16e55d5fc9976e368db81
Axel Fontana <axel.fontana@qualifio.com>
2025-10-08 09:41:41 +0200

    feat: add translation for config-plugins

---
+++ french/admin/config_plugins.lang.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['plugins'] = "Extensions";
+$l['plugins_desc'] = "Cette page vous permet de gérer les extensions de votre forum (enregistrées dans le répertoire <strong>inc/plugins</strong>). Pour masquer une extension sans en supprimer les informations, cliquez sur « Désactiver ».";
+$l['plugin_updates'] = "Mises à jour des extensions";
+$l['plugin_updates_desc'] = "Cette page vous permet de vérifier les mises à jour disponibles pour vos extensions.";
+$l['browse_plugins'] = "Parcourir les extensions";
+$l['browse_plugins_desc'] = "Cette page vous permet de parcourir la liste des extensions disponibles sur le site officiel de MyBB et compatibles avec votre version de MyBB.";
+$l['browse_all_plugins'] = "Parcourir toutes les extensions";
+
+$l['plugin'] = "Extension";
+$l['active_plugin'] = "Extensions actives";
+$l['inactive_plugin'] = "Extensions inactives";
+$l['your_version'] = "Votre version";
+$l['latest_version'] = "Dernière version";
+$l['download'] = "Télécharger";
+$l['deactivate'] = "Désactiver";
+$l['activate'] = "Activer";
+$l['install_and_activate'] = "Installer et activer";
+$l['uninstall'] = "Désinstaller";
+$l['created_by'] = "Créée par";
+$l['no_plugins'] = "Il n’y a actuellement aucune extension sur votre forum.";
+$l['no_active_plugins'] = "Il n’y a actuellement aucune extension active sur votre forum.";
+$l['no_inactive_plugins'] = "Il n’y a actuellement aucune extension inactive sur votre forum.";
+
+$l['plugin_incompatible'] = "Cette extension est incompatible avec MyBB {1}.";
+
+$l['recommended_plugins_for_mybb'] = "Extensions recommandées pour MyBB {1}";
+$l['browse_results_for_mybb'] = "Parcourir les résultats pour MyBB {1}";
+$l['search_for_plugins'] = "Rechercher des extensions";
+$l['search'] = "Rechercher";
+
+$l['error_vcheck_no_supported_plugins'] = "Aucune des extensions installées ne prend en charge la vérification de version.";
+$l['error_vcheck_communications_problem'] = "Un problème est survenu lors de la communication avec le serveur MyBB. Veuillez réessayer dans quelques minutes.";
+$l['error_vcheck_vulnerable'] = "[Extension vulnérable] :";
+$l['error_vcheck_vulnerable_notes'] = "Cette extension a été signalée comme vulnérable par l’équipe MyBB. Nous vous recommandons de la supprimer immédiatement. Consultez les informations ci-dessous :";
+$l['error_no_input'] = "Code d’erreur 1 : aucune donnée spécifiée.";
+$l['error_no_pids'] = "Code d’erreur 2 : aucun identifiant d’extension spécifié.";
+$l['error_communication_problem'] = "Un problème est survenu lors de la communication avec le serveur MyBB. Veuillez réessayer dans quelques minutes.";
+$l['error_invalid_plugin'] = "L’extension spécifiée est invalide ou n’existe pas.";
+$l['error_no_results_found'] = "Aucun résultat trouvé avec les mots-clés saisis.";
+
+$l['success_plugins_up_to_date'] = "Toutes les extensions sont à jour.";
+$l['success_plugin_activated'] = "L’extension a bien été activée.";
+$l['success_plugin_deactivated'] = "L’extension a bien été désactivée.";
+$l['success_plugin_installed'] = "L’extension a bien été installée et activée.";
+$l['success_plugin_uninstalled'] = "L’extension a bien été désinstallée.";
