d80fd03bf8993a0285c554d3e74a9a4f3afc0af4
Axel Fontana <axel.fontana@qualifio.com>
2025-10-09 19:50:20 +0200

    feat: add translation for forum-management

---
+++ french/admin/forum_management.lang.php
@@ -0,0 +1,279 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['forum_management'] = "Forums";
+$l['forum_management_desc'] = "Cette page vous permet de gérer les catégories et forums de votre forum. Vous pouvez également y gérer les permissions et les modérateurs propres à chaque forum.";
+$l['add_forum'] = "Ajouter un forum";
+$l['add_forum_desc'] = "Cette page vous permet d’ajouter un nouveau forum ou une nouvelle catégorie. Vous pouvez également y définir leurs permissions initiales.";
+$l['copy_forum'] = "Faire une copie de ce forum";
+$l['copy_forum_desc'] = "Cette page vous permet de copier les paramètres ou les permissions d’un forum existant vers un nouveau forum ou un autre forum existant.";
+$l['forum_permissions'] = "Permissions";
+$l['forum_permissions_desc'] = "Cette page vous permet de modifier les permissions complètes d’un groupe pour un forum donné.";
+$l['view_forum'] = "Détails du forum";
+$l['view_forum_desc'] = "Cette page vous permet de gérer un forum, d’en consulter les sous-forums ou encore d'en modifier les permissions et modérateurs.";
+$l['add_child_forum'] = "Ajouter un sous-forum";
+$l['edit_forum_settings'] = "Modifier les paramètres du forum";
+$l['edit_forum_settings_desc'] = "Cette page vous permet de modifier les paramètres et les permissions d’un forum existant.";
+$l['edit_forum'] = "Modifier le forum";
+$l['edit_mod'] = "Modifier le modérateur";
+$l['edit_mod_desc'] = "Cette page vous permet de modifier les paramètres d’un modérateur en particulier.";
+$l['forum_moderators'] = "Modérateurs du forum";
+$l['forum_permissions2'] = "Permissions du forum";
+$l['more_subforums'] = "et {1} de plus.";
+
+$l['manage_forums'] = "Gestion des forums";
+$l['forum'] = "Forum";
+$l['order'] = "Ordre";
+
+$l['subforums'] = "Gérer les sous-forums";
+$l['moderators'] = "Gérer les modérateurs";
+$l['forum_thread_prefixes'] = "Gérer les préfixes de sujets";
+$l['permissions'] = "Gérer les permissions";
+$l['delete_forum'] = "Supprimer";
+
+$l['sub_forums'] = "Sous-forums";
+$l['update_forum_orders'] = "Mettre à jour l’ordre d’affichage";
+$l['update_forum_permissions'] = "Enregistrer";
+$l['reset'] = "Réinitialiser";
+$l['in_forums'] = "Forums dans le forum « {1} »";
+$l['forum_permissions_in'] = "Permissions du forum « {1} »";
+$l['moderators_assigned_to'] = "Modérateurs du forum « {1} »";
+$l['edit_permissions'] = "Modifier les permissions";
+$l['set_permissions'] = "Définir les permissions";
+$l['using_custom_perms'] = "Utilisation de permissions personnalisées";
+$l['using_default_perms'] = "Utilisation des permissions par défaut";
+$l['clear_custom_perms'] = "Supprimer les permissions personnalisées";
+$l['set_custom_perms'] = "Définir des permissions personnalisées";
+
+$l['permissions_use_group_default'] = "Utiliser les valeurs par défaut pour ce groupe";
+$l['permissions_group'] = "Groupe";
+$l['permissions_user'] = "Utilisateur";
+$l['permissions_canview'] = "Peut visualiser ?";
+$l['permissions_canpostthreads'] = "Peut publier des sujets ?";
+$l['permissions_canpostreplys'] = "Peut publier des messages ?";
+$l['permissions_canpostpolls'] = "Peut publier des sondages ?";
+$l['permissions_canuploadattachments'] = "Peut joindre des fichiers ?";
+$l['permissions_all'] = "Tous";
+
+$l['overview_allowed_actions'] = "Aperçu : actions autorisées";
+$l['overview_disallowed_actions'] = "Aperçu : actions interdites";
+$l['perm_drag_canview'] = "&#149; Visualiser";
+$l['perm_drag_canpostthreads'] = "&#149; Publier des sujets";
+$l['perm_drag_canpostreplys'] = "&#149; Publier des messages";
+$l['perm_drag_canpostpolls'] = "&#149; Publier des sondages";
+
+$l['moderator_permissions'] = "Permissions de modération";
+$l['forum_desc'] = "Forum géré par le modérateur";
+$l['edit_mod_for'] = "Modifier les options de modération pour « {1} »";
+$l['can_edit_posts'] = "Peut modifier les messages ?";
+$l['can_soft_delete_posts'] = "Peut supprimer temporairement les messages ?";
+$l['can_restore_posts'] = "Peut restaurer les messages ?";
+$l['can_delete_posts'] = "Peut supprimer définitivement les messages ?";
+$l['can_soft_delete_threads'] = "Peut supprimer temporairement les sujets ?";
+$l['can_restore_threads'] = "Peut restaurer les sujets ?";
+$l['can_delete_threads'] = "Peut supprimer définitivement les sujets ?";
+$l['can_view_ips'] = "Peut consulter les adresses IP ?";
+$l['can_view_unapprove'] = "Peut consulter les sujets et messages en attente d’approbation ?";
+$l['can_view_deleted'] = "Peut consulter les sujets et messages supprimés ?";
+$l['can_open_close_threads'] = "Peut déverrouiller/verrouiller les sujets ?";
+$l['can_stick_unstick_threads'] = "Peut épingler/désépingler les sujets ?";
+$l['can_approve_unapprove_threads'] = "Peut approuver/désapprouver les sujets ?";
+$l['can_approve_unapprove_posts'] = "Peut approuver/désapprouver les messages ?";
+$l['can_approve_unapprove_attachments'] = "Peut approuver/désapprouver les pièces jointes ?";
+$l['can_manage_threads'] = "Peut gérer les sujets (diviser, déplacer, copier, fusionner) ?";
+$l['can_manage_polls'] = "Peut gérer les sondages ?";
+$l['can_post_closed_threads'] = "Peut publier des messages dans les sujets verrouillés ?";
+$l['can_move_to_other_forums'] = "Peut déplacer les sujets vers un autre forum dont l’utilisateur n’est pas modérateur ?";
+$l['can_use_custom_tools'] = "Peut utiliser les outils de modération personnalisés ?";
+$l['can_manage_announcements'] = "Peut gérer les annonces de ce forum ?";
+$l['can_manage_reported_posts'] = "Peut gérer les messages signaliés dans ce forum ?";
+$l['can_view_mod_log'] = "Peut consulter les entrées du journal de modération pour ce forum ?";
+$l['moderator_cp_permissions'] = "Permissions du panneau de modération";
+$l['moderator_cp_permissions_desc'] = "Cet utilisateur doit pouvoir accéder au panneau de modération et disposer des permissions de son groupe pour accéder à ces outils afin que ces permissions soient effectives.";
+
+$l['save_mod'] = "Enregistrer";
+
+$l['no_forums'] = "Il n’y a actuellement aucun forum.";
+$l['no_moderators'] = "Il n’y a actuellement aucun modérateur pour ce forum.";
+
+$l['success_forum_disporder_updated'] = "L’ordre d’affichage des forums a bien été mis à jour.";
+$l['success_forum_deleted'] = "Le forum a bien été supprimé. Vous devriez maintenant exécuter l’outil permettant de <a href=\"index.php?module=tools-recount_rebuild\">recalculer</a> les statistiques du forum.";
+$l['success_moderator_deleted'] = "Le modérateur sélectionné a bien été supprimé.<br />Attention : cela ne modifie pas les permissions de son groupe ; il peut donc encore disposer de permissions de modération.";
+$l['success_forum_permissions_updated'] = "Les permissions du forum ont bien été mises à jour.";
+$l['success_forum_updated'] = "Les paramètres du forum ont bien été mis à jour.";
+$l['success_moderator_updated'] = "Le modérateur sélectionné a bien été mis à jour.";
+$l['success_custom_permission_cleared'] = "Les permissions personnalisées de ce forum ont bien été réinitialisées.";
+
+$l['error_invalid_forum'] = "Le forum spécifié est invalide ou n’existe pas.";
+$l['error_invalid_moderator'] = "Le modérateur spécifié est invalide ou n’existe pas.";
+$l['error_invalid_fid'] = "Le forum spécifié est invalide ou n’existe pas.";
+$l['error_forum_parent_child'] = "Impossible de définir le forum parent comme l’un de ses sous-forums.";
+$l['error_forum_parent_itself'] = "Le forum parent ne peut pas être le forum lui-même.";
+$l['error_incorrect_moderator'] = "Veuillez sélectionner un modérateur valide.";
+
+$l['confirm_moderator_deletion'] = "Êtes-vous sûr de vouloir supprimer ce modérateur pour ce forum ?";
+$l['confirm_forum_deletion'] = "Êtes-vous sûr de vouloir supprimer ce forum ?";
+$l['confirm_clear_custom_permission'] = "Êtes-vous sûr de vouloir supprimer les permissions personnalisées ?";
+
+$l['forum_type'] = "Type de forum";
+$l['forum_type_desc'] = "Sélectionnez le type de forum que vous souhaitez créer — un forum dans lequel il est possible de publier des messages ou une catégorie qui regroupe d’autres forums.";
+$l['category'] = "Catégorie";
+$l['title'] = "Nom";
+$l['description'] = "Description";
+$l['save_forum'] = "Enregistrer";
+$l['parent_forum'] = "Parent";
+$l['parent_forum_desc'] = "Le forum qui contiendra celui-ci. Les catégories n’ont normalement pas de forum parent, mais il est possible de leur en attribuer un si nécessaire.";
+$l['none'] = "Aucun";
+$l['display_order'] = "Ordre d’affichage";
+
+$l['show_additional_options'] = "Afficher les options avancées";
+$l['hide_additional_options'] = "Masquer les options avancées";
+$l['additional_forum_options'] = "Options avancées";
+$l['forum_link'] = "Lien du forum";
+$l['forum_link_desc'] = "Pour rediriger ce forum vers un autre emplacement, saisissez l’adresse de destination. <strong>Attention :</strong> ceci désactivera les fonctionnalités normales du forum, mais vous pourrez toujours définir des permissions pour celui-ci.";
+$l['forum_password'] = "Mot de passe d’accès au forum";
+$l['forum_password_desc'] = "Pour protéger ce forum, vous pouvez définir un mot de passe obligatoire pour y accéder. <strong>Attention :</strong> les utilisateurs doivent tout de même disposer des permissions nécessaires pour y accéder.";
+$l['access_options'] = "Options d’accès";
+$l['forum_is_active'] = "Forum actif ?";
+$l['forum_is_active_desc'] = "Si cette option est désactivée, le forum ne sera pas visible des utilisateurs et sera considéré comme « inexistant ».";
+$l['forum_is_open'] = "Forum ouvert ?";
+$l['forum_is_open_desc'] = "Si cette option est désactivée, les utilisateurs ne pourront pas publier dans ce forum, quelles que soient leurs permissions.";
+
+$l['copy_to_new_forum'] = "Copier vers un nouveau forum";
+$l['source_forum'] = "Forum source";
+$l['source_forum_desc'] = "Forum depuis lequel copier les paramètres et/ou les permissions.";
+$l['destination_forum'] = "Forum de destination";
+$l['destination_forum_desc'] = "Forum vers lequel copier les paramètres et/ou les permissions.";
+$l['new_forum_settings'] = "Nouveau forum : paramètres";
+$l['copy_settings_and_properties'] = "Copier les paramètres et propriétés du forum";
+$l['copy_settings_and_properties_desc'] = "S’applique uniquement si le forum de destination existe déjà.";
+$l['copy_user_group_permissions'] = "Copier les permissions des groupes d’utilisateurs";
+$l['copy_user_group_permissions_desc'] = "Maintenez « CTRL » pour en sélectionner plusieurs.";
+
+$l['override_user_style'] = "Remplacer le thème sélectionné par l’utilisateur pour ce forum";
+$l['style_options'] = "Options d’affichage";
+$l['forum_specific_style'] = "Thème spécifique à ce forum :";
+$l['use_default'] = "Utiliser la valeur par défaut";
+$l['dont_display_rules'] = "Ne pas afficher de règles dans ce forum";
+$l['display_rules_inline'] = "Afficher les règles de ce forum dans la liste des sujets";
+$l['display_rules_inline_new'] = "Afficher les règles de ce forum dans la liste des sujets et lors de la rédaction d’un message";
+$l['display_rules_link'] = "Afficher un lien vers les règles de ce forum";
+$l['display_method'] = "Mode d’affichage :";
+$l['rules'] = "Règles :";
+$l['forum_rules'] = "Règles du forum";
+$l['name'] = "Nom";
+$l['username'] = "Nom d’utilisateur";
+$l['moderator_username_desc'] = "Nom d’utilisateur du modérateur à ajouter.";
+$l['add_user_as_moderator'] = "Ajouter un utilisateur comme modérateur";
+$l['usergroup'] = "Groupe";
+$l['add_usergroup_as_moderator'] = "Ajouter un groupe comme modérateur";
+$l['moderator_usergroup_desc'] = "Sélectionnez un groupe à ajouter comme modérateur dans la liste ci-dessous.";
+$l['add_usergroup_moderator'] = "Enregistrer";
+$l['add_user_moderator'] = "Enregistrer";
+
+$l['default_view_options'] = "Options de visualisation par défaut";
+$l['default_date_cut'] = "Période par défaut :";
+$l['default_sort_by'] = "Tri par défaut :";
+$l['default_sort_order'] = "Ordre de tri par défaut :";
+
+$l['board_default'] = "Valeur par défaut du forum";
+
+$l['datelimit_1day'] = "Le dernier jour";
+$l['datelimit_5days'] = "Les 5 derniers jours";
+$l['datelimit_10days'] = "Les 10 derniers jours";
+$l['datelimit_20days'] = "Les 20 derniers jours";
+$l['datelimit_50days'] = "Les 50 derniers jours";
+$l['datelimit_75days'] = "Les 75 derniers jours";
+$l['datelimit_100days'] = "Les 100 derniers jours";
+$l['datelimit_lastyear'] = "La dernière année";
+$l['datelimit_beginning'] = "Depuis le début";
+
+$l['sort_by_subject'] = "Titre du sujet";
+$l['sort_by_lastpost'] = "Date du dernier message";
+$l['sort_by_starter'] = "Auteur du sujet";
+$l['sort_by_started'] = "Date de création du sujet";
+$l['sort_by_replies'] = "Nombre de réponses";
+$l['sort_by_views'] = "Nombre de vues";
+
+$l['sort_order_asc'] = "Croissant";
+$l['sort_order_desc'] = "Décroissant";
+
+$l['misc_options'] = "Autres options";
+$l['allow_html'] = "Autoriser l’HTML dans les messages";
+$l['allow_mycode'] = "Autoriser les MyCodes dans les messages";
+$l['allow_smilies'] = "Autoriser les émoticônes dans les messages";
+$l['allow_img_code'] = "Autoriser les balises [img] dans les messages (nécessite que les MyCodes soient autorisés)";
+$l['allow_video_code'] = "Autoriser les balises [video] dans les messages (nécessite que les MyCodes soient autorisés)";
+$l['allow_post_icons'] = "Autoriser l’utilisation des icônes de message";
+$l['show_forum_jump'] = "Afficher ce forum dans le menu « Aller au forum »";
+$l['use_postcounts'] = "Les messages de ce forum doivent être comptabilisés dans le total des messages des utilisateurs";
+$l['use_threadcounts'] = "Les sujets de ce forum doivent être comptabilisés dans le total des sujets des utilisateurs";
+$l['require_thread_prefix'] = "Exiger un préfixe pour tous les sujets";
+
+$l['use_permissions'] = "Utiliser les permissions";
+$l['use_permissions_desc'] = "Sélectionnez le type de permissions à utiliser pour ce groupe d’utilisateurs : permissions héritées (les permissions personnalisées seront supprimées) ou permissions personnalisées.";
+$l['inherit_permissions'] = "Utiliser les permissions du groupe d’utilisateurs ou hériter des permissions des forums parents";
+$l['custom_permissions'] = "Utiliser des permissions personnalisées (ci-dessous)";
+$l['custom_permissions_for'] = "— Permissions personnalisées pour";
+
+$l['inherited_permission'] = "permissions héritées";
+$l['custom_permission'] = "permissions personnalisées";
+
+$l['save_permissions'] = "Enregistrer";
+
+$l['error_missing_title'] = "Veuillez saisir un nom pour ce forum.";
+$l['error_no_parent'] = "Veuillez saisir un parent pour ce forum.";
+$l['error_not_empty'] = "Les forums contenant des sujets ne peuvent pas être convertis en catégories.";
+$l['error_forum_link_not_empty'] = "Les forums contenant des sujets ne peuvent pas rediriger vers une autre adresse.";
+
+$l['success_forum_added'] = "Le forum a bien été créé.";
+$l['success_moderator_added'] = "Le modérateur à bien été ajouté à ce forum.";
+$l['success_forum_permissions_saved'] = "Les permissions du forum ont bien été enregistrées.";
+$l['success_forum_copied'] = "Le forum sélectionné a bien été copié.";
+
+$l['error_moderator_already_added'] = "L’utilisateur ou le groupe sélectionné est déjà modérateur de ce forum.";
+$l['error_moderator_not_found'] = "Le nom d’utilisateur ou le groupe spécifié est introuvable.";
+$l['error_new_forum_needs_name'] = "Veuillez saisir un nom pour le nouveau forum.";
+$l['error_invalid_source_forum'] = "Le forum source est invalide.";
+$l['error_invalid_destination_forum'] = "Le forum de destination est invalide.";
+
+$l['group_viewing'] = "Visualisation";
+$l['group_posting'] = "Publication";
+$l['group_editing'] = "Modification";
+$l['group_moderate'] = "Modération";
+$l['group_polls'] = "Sondages";
+$l['group_misc'] = "Divers";
+
+$l['viewing_field_canview'] = "Peut consulter le forum ?";
+$l['viewing_field_canviewthreads'] = "Peut consulter les sujets de ce forum ?";
+$l['viewing_field_canonlyviewownthreads'] = "Ne peut consulter que ses propres sujets ?";
+$l['viewing_field_candlattachments'] = "Peut télécharger des pièces jointes ?";
+
+$l['posting_field_canpostthreads'] = "Peut publier des sujets ?";
+$l['posting_field_canpostreplys'] = "Peut publier des messages ?";
+$l['posting_field_canonlyreplyownthreads'] = "Ne peut répondre qu’à ses propres sujets ?";
+$l['posting_field_canpostattachments'] = "Peut joindre de nouveaux fichiers ?";
+
+$l['editing_field_caneditposts'] = "Peut modifier ses propres messages ?";
+$l['editing_field_candeleteposts'] = "Peut supprimer ses propres messages ?";
+$l['editing_field_candeletethreads'] = "Peut supprimer ses propres sujets ?";
+$l['editing_field_caneditattachments'] = "Peut mettre à jour ses propres pièces jointes ?";
+$l['editing_field_canviewdeletionnotice'] = "Peut voir les notifications de suppression ?";
+
+$l['moderate_field_modposts'] = "Les nouveaux messages doivent-ils être approuvés ?";
+$l['moderate_field_modthreads'] = "Les nouveaux sujets doivent-ils être approuvés ?";
+$l['moderate_field_modattachments'] = "Les nouvelles pièces jointes doivent-elles être approuvées ?";
+$l['moderate_field_mod_edit_posts'] = "Les messages modifiés doivent-ils être réapprouvés ?";
+
+$l['polls_field_canpostpolls'] = "Peut publier des sondages ?";
+$l['polls_field_canvotepolls'] = "Peut participer aux sondages ?";
+
+$l['misc_field_cansearch'] = "Peut rechercher dans ce forum ?";
+
+$l['confirm_proceed_deletion'] = "Cliquez sur « Continuer » pour confirmer la suppression du forum.";
+$l['automatically_redirecting'] = "Redirection automatique…";
