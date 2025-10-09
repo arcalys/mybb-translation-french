94ef17b629f3d1bc3f577f6bba3eb9e6abbd9789
Axel Fontana <axel.fontana@qualifio.com>
2025-10-09 09:56:11 +0200

    feat: add translation for config-languages

---
+++ french/admin/config_languages.lang.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['languages'] = "Langues";
+$l['languages_desc'] = "Cette page vous permet de gérer les différentes langues installées sur votre forum.";
+$l['find_language_packs'] = "Rechercher d’autres langues";
+$l['language_files'] = "Fichiers de langue";
+$l['language_files_desc'] = "Cette page vous permet de gérer les fichiers d’une langue.";
+$l['quick_phrases'] = "Phrases rapides";
+$l['quick_phrases_desc'] = "Accès rapide aux expressions les plus souvent modifiées.";
+
+$l['edit_with'] = "Comparer avec « {1} »";
+$l['edit_language_variables'] = "Modifier les textes";
+$l['edit_language_variables_desc'] = "Cette page vous permet de modifier les textes d’une langue installée sur votre forum.";
+$l['edit_properties_desc'] = "Cette page vous permet de modifier les propriétés d’une langue installée sur votre forum.";
+$l['installed_language_packs'] = "Langues installées";
+$l['front_end'] = "Interface utilisateur";
+$l['admin_cp'] = "Panneau d’administration";
+$l['save_language_file'] = "Enregistrer";
+$l['nav_editing_set'] = "Modifier les propriétés d’une langue";
+$l['edit_properties'] = "Modifier les propriétés";
+$l['friendly_name'] = "Nom d’affichage";
+$l['language_in_html'] = "Code ISO de la langue";
+$l['charset'] = "Encodage des caractères";
+$l['admin'] = "Contient les textes du panneau d’administration ?";
+$l['rtl'] = "S’écrit de droite à gauche ?";
+$l['support_link'] = "Lien du site de support";
+$l['support_name'] = "Nom du site de support";
+
+$l['quickphrases_agreement'] = 'Conditions d’inscription — Titre';
+$l['quickphrases_agreement_1'] = 'Conditions d’inscription — Paragraphe 1';
+$l['quickphrases_agreement_2'] = 'Conditions d’inscription — Paragraphe 2';
+$l['quickphrases_agreement_3'] = 'Conditions d’inscription — Paragraphe 3';
+$l['quickphrases_agreement_4'] = 'Conditions d’inscription — Paragraphe 4';
+$l['quickphrases_agreement_5'] = 'Conditions d’inscription — Paragraphe 5';
+
+$l['quickphrases_error_nopermission_guest_1'] = 'Message d’absence d’autorisation — Paragraphe 1';
+$l['quickphrases_error_nopermission_guest_2'] = 'Message d’absence d’autorisation — Paragraphe 2';
+$l['quickphrases_error_nopermission_guest_3'] = 'Message d’absence d’autorisation — Paragraphe 3';
+$l['quickphrases_error_nopermission_guest_4'] = 'Message d’absence d’autorisation — Paragraphe 4';
+
+$l['no_languages'] = "Il n’y a actuellement aucune langue installée sur votre forum.";
+$l['no_language_files_front_end'] = "Il n’y a aucun fichier de langue pour l’interface utilisateur.";
+$l['no_language_files_admin_cp'] = "Il n’y a aucun fichier de langue pour le panneau d’administration.";
+
+$l['error_invalid_set'] = "La langue spécifiée est invalide ou n’existe pas.";
+$l['error_invalid_file'] = "Le fichier de langue spécifié est invalide ou n’existe pas.";
+$l['error_cannot_write_to_file'] = "Impossible d’écrire dans le fichier. Vérifiez que les permissions sont correctes et réessayez.";
+$l['error_folders_fail'] = "Un problème est survenu lors de l’accès à la langue demandée. Assurez-vous que les dossiers existent et qu’ils sont accessibles en écriture.";
+
+$l['alert_note_cannot_write'] = "Avant de pouvoir modifier ce fichier, vous devez ajuster ses permissions afin que le serveur puisse y écrire.";
+
+$l['success_langfile_updated'] = "Le fichier a bien été mis à jour.";
+$l['success_langprops_updated'] = "Les propriétés ont bien été mises à jour.";
+$l['success_quickphrases_updated'] = "Les phrases rapides ont bien été mises à jour.";
+
+$l['issues'] = "Problèmes";
+$l['phrases'] = "Clés de langue";
+$l['issues_ok'] = "Ce fichier est correct.";
+$l['issues_nothingtocompare'] = "Il n’y a rien à comparer ici.";
+$l['issues_warning'] = "Veuillez vérifier ce fichier.";
