faf721fbed02f9e9f8bce864e77793ea66f8fa5e
Axel Fontana <axel.fontana@qualifio.com>
2025-10-09 22:39:07 +0200

    feat: add translation for user-admin-permissions

---
+++ french/admin/user_admin_permissions.lang.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['admin_permissions'] = "Permissions d’administration";
+$l['user_permissions'] = "Permissions des utilisateurs";
+$l['user_permissions_desc'] = "Cette page vous permet de gérer les permissions d’administration pour chaque utilisateur. Vous pouvez ainsi restreindre leur accès à certaines sections du panneau d’administration.";
+$l['group_permissions'] = "Permissions des groupes";
+$l['group_permissions_desc'] = "Cette page vous permet de gérer les permissions d’administration pour chaque groupe ayant accès au panneau d’administration. Vous pouvez également restreindre leur accès à certaines sections du panneau d’administration.";
+$l['default_permissions'] = "Permissions par défaut";
+$l['default_permissions_desc'] = "Les permissions d’administration par défaut s’appliquent aux utilisateurs qui ne disposent pas de permissions personnalisées ni de permissions héritées d’un groupe.";
+
+$l['admin_permissions_updated'] = "Les permissions ont bien été mises à jour.";
+$l['revoke_permissions'] = "Révoquer les permissions";
+$l['edit_permissions'] = "Modifier les permissions";
+$l['set_permissions'] = "Définir les permissions";
+$l['edit_permissions_desc'] = "Cette page vous permet de restreindre l’accès à des onglets ou à des pages spécifiques. Notez que l’onglet « Accueil » est accessible à tous les administrateurs.";
+$l['update_permissions'] = "Mettre à jour les permissions";
+$l['view_log'] = "Voir le journal d’administration";
+$l['permissions_type_group'] = "Type de permissions du groupe";
+$l['permissions_type_user'] = "Type de permissions de l’utilisateur";
+$l['no_group_perms'] = "Aucune permission de groupe n’a encore été définie.";
+$l['no_user_perms'] = "Aucune permission d’utilisateur n’a encore été définie.";
+$l['edit_user'] = "Modifier le profil de l’utilisateur";
+$l['using_individual_perms'] = "Utilise des permissions individuelles";
+$l['using_custom_perms'] = "Utilise des permissions personnalisées";
+$l['using_group_perms'] = "Utilise les permissions du groupe";
+$l['using_default_perms'] = "Utilise les permissions par défaut";
+$l['last_active'] = "Dernière activité";
+$l['user'] = "Utilisateur";
+$l['edit_group'] = "Modifier le groupe";
+$l['default'] = "Par défaut";
+$l['group'] = "Groupe";
+
+$l['error_super_admin'] = "Vous ne pouvez pas effectuer cette action sur cet utilisateur car il s’agit d’un super administrateur.";
+$l['error_delete_no_uid'] = "Veuillez saisir un identifiant d’utilisateur ou de groupe d’administrateur.";
+$l['error_delete_invalid_uid'] = "Veuillez saisir un identifiant d’utilisateur ou de groupe d’administrateur valide.";
+
+$l['success_perms_deleted'] = "Les permissions ont bien été révoquées.";
+
+$l['confirm_perms_deletion'] = "Êtes-vous sûr de vouloir révoquer les permissions de cet utilisateur ou de ce groupe ?";
+$l['confirm_perms_deletion2'] = "Êtes-vous sûr de vouloir révoquer les permissions de cet utilisateur ?";
+$l['confirm_perms_deletion3'] = "Êtes-vous sûr de vouloir révoquer les permissions de ce groupe ?";
