2445a07c96c828397c7e9917f36efcc17d5563c3
Axel Fontana <axel.fontana@qualifio.com>
2025-10-10 21:39:06 +0200

    feat: add translation for config-warning

---
+++ french/admin/config_warning.lang.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['warning_system'] = "Système d’avertissements";
+$l['warning_types'] = "Types d’avertissements";
+$l['warning_types_desc'] = "Cette page vous permet de gérer la liste des différents types d’avertissements que l’équipe du forum peut attribuer aux utilisateurs.";
+$l['add_warning_type'] = "Ajouter un nouveau type d’avertissement";
+$l['add_warning_type_desc'] = "Cette page vous permet de créer un nouveau type d’avertissement prédéfini. Vous pouvez définir le nombre de points à ajouter pour ce type ainsi que la durée avant l’expiration des avertissements de ce type.";
+$l['edit_warning_type'] = "Modifier un type d’avertissement";
+$l['edit_warning_type_desc'] = "Cette page vous permet de modifier ce type d’avertissement. Vous pouvez définir le nombre de points à ajouter pour ce type ainsi que la durée avant l’expiration des avertissements de ce type.";
+$l['warning_levels'] = "Niveaux d’avertissements";
+$l['warning_levels_desc'] = "Cette page vous permet de gérer les niveaux d’avertissements qui définissent les actions à appliquer lorsqu’un utilisateur atteint un certain pourcentage des points d’avertissement maximum. Vous pouvez par exemple bannir un utilisateur ou suspendre ses privilèges.";
+$l['add_warning_level'] = "Ajouter un niveau d’avertissement";
+$l['add_warning_level_desc'] = "Cette page vous permet de créer un nouveau niveau d’avertissement. Les niveaux d’avertissements définissent les actions à appliquer aux utilisateurs lorsqu’ils atteignent un pourcentage spécifique du niveau d’avertissement maximum.";
+$l['edit_warning_level'] = "Modifier un niveau d’avertissement";
+$l['edit_warning_level_desc'] = "Cette page vous permet de modifier un niveau d’avertissement.";
+
+$l['percentage'] = "Pourcentage";
+$l['action_to_take'] = "Action";
+$l['move_banned_group'] = "Déplacer dans le groupe banni ({3}) pendant {1} {2}";
+$l['move_banned_group_permanent'] = "Déplacer dans le groupe banni ({1}) de façon permanente";
+$l['suspend_posting'] = "Suspendre les privilèges de publication pendant {1} {2}";
+$l['suspend_posting_permanent'] = "Suspendre les privilèges de publication de façon permanente";
+$l['moderate_new_posts'] = "Modérer les nouveaux messages pendant {1} {2}";
+$l['moderate_new_posts_permanent'] = "Modérer les nouveaux messages de façon permanente";
+$l['no_warning_levels'] = "Il n’y a actuellement aucun niveau d’avertissement sur votre forum.";
+
+$l['warning_type'] = "Type d’avertissement";
+$l['points'] = "Points";
+$l['expires_after'] = "Expiration";
+$l['no_warning_types'] = "Il n’y a actuellement aucun type d’avertissement sur votre forum.";
+
+$l['warning_points_percentage'] = "Pourcentage des points d’avertissement maximum";
+$l['warning_points_percentage_desc'] = "Saisissez une valeur numérique entre 1 et 100.";
+$l['action_to_be_taken'] = "Action à appliquer";
+$l['action_to_be_taken_desc'] = "Sélectionnez l’action à appliquer lorsque les utilisateurs atteignent ce niveau.";
+$l['ban_user'] = "Bannir l’utilisateur";
+$l['banned_group'] = "Groupe banni :";
+$l['ban_length'] = "Durée du bannissement :";
+$l['suspend_posting_privileges'] = "Suspendre les privilèges de publication";
+$l['suspension_length'] = "Durée de la suspension :";
+$l['moderate_posts'] = "Modérer les messages";
+$l['moderation_length'] = "Durée de la modération :";
+$l['save_warning_level'] = "Enregistrer le niveau d’avertissement";
+
+$l['title'] = "Titre";
+$l['points_to_add'] = "Points à ajouter";
+$l['points_to_add_desc'] = "Le nombre de points à ajouter au niveau d’avertissement d’un utilisateur.";
+$l['warning_expiry'] = "Expiration de l’avertissement";
+$l['warning_expiry_desc'] = "Combien de temps après son attribution cet avertissement doit-il expirer ?";
+$l['save_warning_type'] = "Enregistrer le type d’avertissement";
+
+$l['expiration_hours'] = "heures(s)";
+$l['expiration_days'] = "jours(s)";
+$l['expiration_weeks'] = "semaines(s)";
+$l['expiration_months'] = "mois";
+$l['expiration_never'] = "jamais";
+$l['expiration_permanent'] = "permanent";
+
+$l['error_invalid_warning_level'] = "Le niveau d’avertissement spécifié est invalide ou n’existe pas.";
+$l['error_invalid_warning_percentage'] = "Veuillez saisir une valeur de pourcentage valide pour ce niveau d’avertissement. La valeur doit être comprise entre 1 et 100.";
+$l['error_invalid_warning_type'] = "Le type d’avertissement spécifié est invalide ou n’existe pas.";
+$l['error_missing_action_type'] = "Veuillez sélectionner une action à appliquer pour ce nouveau niveau d’avertissement.";
+$l['error_missing_type_title'] = "Veuillez saisir un titre ce type d’avertissement.";
+$l['error_missing_type_points'] = "Veuillez saisir un nombre valide de points à ajouter lors de l’attribution de ce type d’avertissement. Vous devez entrer un nombre entre 1 et {1}.";
+
+$l['success_warning_level_created'] = "Le niveau d’avertissement a bien été créé.";
+$l['success_warning_level_updated'] = "Le niveau d’avertissement a bien été mis à jour.";
+$l['success_warning_level_deleted'] = "Le niveau d’avertissement a bien été supprimé.";
+$l['success_warning_type_created'] = "Le type d’avertissement a bien été créé.";
+$l['success_warning_type_updated'] = "Le type d’avertissement a bien été mis à jour.";
+$l['success_warning_type_deleted'] = "Le type d’avertissement a bien été supprimé.";
+
+$l['confirm_warning_level_deletion'] = "Êtes-vous sûr de vouloir supprimer ce niveau d’avertissement ?";
+$l['confirm_warning_type_deletion'] = "Êtes-vous sûr de vouloir supprimer ce type d’avertissement ?";
