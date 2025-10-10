18e3d917eb03b1b3602d70070de3ee0eecf0a732
Axel Fontana <axel.fontana@qualifio.com>
2025-10-10 20:52:54 +0200

    feat: add translation for global

---
+++ french/admin/global.lang.php
@@ -0,0 +1,383 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['today_rel'] = "<span title=\"{1}\">Aujourd’hui</span>";
+$l['yesterday_rel'] = "<span title=\"{1}\">Hier</span>";
+$l['today'] = "Aujourd’hui";
+$l['yesterday'] = "Hier";
+
+$l['size_yb'] = "YiB";
+$l['size_zb'] = "ZiB";
+$l['size_eb'] = "EiB";
+$l['size_pb'] = "PiB";
+$l['size_tb'] = "TiB";
+$l['size_gb'] = "GiB";
+$l['size_mb'] = "MiB";
+$l['size_kb'] = "KiB";
+$l['size_bytes'] = "octets";
+$l['na'] = "N/A";
+
+// Header language strings
+$l['mybb_admin_panel'] = "Panneau d’administration";
+$l['mybb_admin_cp'] = "MyBB Admin CP";
+$l['logged_in_as'] = "Connecté en tant que :";
+$l['view_board'] = "Accueil du forum";
+$l['logout'] = "Déconnexion";
+
+// Footer language strings
+$l['generated_in'] = "Généré en {1} avec <a href=\"{2}\" target=\"_blank\">{3} requêtes SQL</a>. Mémoire: {4}";
+
+// Login page
+$l['enter_username_and_password'] = "Veuillez saisir votre {1} et votre mot de passe pour continuer.";
+$l['login_username'] = "nom d’utilisateur";
+$l['login_email'] = "adresse électronique";
+$l['login_username_and_password'] = "nom d’utilisateur/adresse électronique";
+$l['mybb_admin_login'] = "Panneau d’administration — Connexion";
+$l['return_to_forum'] = "Retourner sur le forum";
+$l['please_login'] = "Connexion";
+$l['username'] = "Nom d’utilisateur :";
+$l['username1'] = "Adresse électronique :";
+$l['username2'] = "Nom d’utilisateur/Adresse électronique :";
+$l['password'] = "Mot de passe:";
+$l['secret_pin'] = "Code PIN :";
+$l['login'] = "Connexion";
+$l['lost_password'] = "Mot de passe oublié ?";
+
+$l['error_invalid_admin_session'] = "Session d’administration invalide.";
+$l['error_admin_session_expired'] = "Votre session d’administration a expiré.";
+$l['error_invalid_ip'] = "Votre adresse IP n’est pas valide pour cette session.";
+$l['error_mybb_admin_lockedout'] = "Ce compte a été verrouillé.";
+$l['error_mybb_admin_lockedout_message'] = "Votre compte a été verrouillé après {1} tentatives de connexion. Un courriel contenant les instructions pour le déverrouiller vous a été envoyé.";
+$l['error_mybb_not_admin_account'] = "Vous n’avez pas la permission d’accéder au panneau d’administration.";
+
+$l['error_invalid_username'] = "Veuillez saisir un nom d’utilisateur valide.";
+$l['error_invalid_uid'] = "Veuillez saisir un identifiant utilisateur valide.";
+$l['error_invalid_token'] = "Veuillez saisir un code d’activation valide.";
+
+$l['success_logged_out'] = "Vous vous êtes déconnecté avec succès.";
+$l['error_invalid_username_password'] = "La combinaison de {1} et du mot de passe que vous avez saisi est invalide.";
+
+// Action Confirmation
+$l['confirm_action'] = "Êtes-vous sûr de vouloir effectuer cette action ?";
+
+// Common words and phrases
+$l['home'] = "Accueil";
+$l['name'] = "Nom";
+$l['size'] = "Taille";
+$l['controls'] = "Actions";
+$l['view'] = "Afficher";
+$l['yes'] = "Oui";
+$l['no'] = "Non";
+$l['cancel'] = "Annuler";
+$l['options'] = "Options";
+$l['proceed'] = "Démarrer";
+$l['ok'] = "OK";
+$l['error'] = "Erreur";
+$l['edit'] = "Modifier";
+$l['never'] = "Jamais";
+$l['legend'] = "Légende";
+$l['version'] = "Version";
+$l['languagevar'] = "Langue";
+$l['use_default'] = "Utiliser la valeur par défaut";
+$l['file'] = "Fichier";
+$l['go'] = "Appliquer";
+$l['clear'] = "Réinitialiser";
+$l['unknown'] = "Inconnu";
+$l['year'] = "année";
+$l['year_short'] = "a";
+$l['years'] = "années";
+$l['years_short'] = "a";
+$l['month'] = "mois";
+$l['month_short'] = "m";
+$l['months'] = "mois";
+$l['months_short'] = "m";
+$l['week'] = "semaine";
+$l['week_short'] = "sem";
+$l['weeks'] = "semaines";
+$l['weeks_short'] = "sem";
+$l['day'] = "jour";
+$l['day_short'] = "j";
+$l['days'] = "jours";
+$l['days_short'] = "j";
+$l['hour'] = "heure";
+$l['hour_short'] = "h";
+$l['hours'] = "heures";
+$l['hours_short'] = "h";
+$l['minute'] = "minute";
+$l['minute_short'] = "min";
+$l['minutes'] = "minutes";
+$l['minutes_short'] = "min";
+$l['second'] = "seconde";
+$l['second_short'] = "s";
+$l['seconds'] = "secondes";
+$l['seconds_short'] = "s";
+$l['permanent'] = "Permanent";
+$l['all_forums'] = "Tous les forums";
+$l['all_groups'] = "Tous les groupes";
+$l['all_prefix'] = "Tous les préfixes";
+$l['select_forums'] = "Sélectionner les forums";
+$l['select_groups'] = "Sélectionner les groupes";
+$l['select_prefix'] = "Sélectionner les préfixes";
+$l['forums_colon'] = "Forums :";
+$l['groups_colon'] = "Groupes :";
+$l['prefix_colon'] = "Préfixes :";
+$l['none'] = "Aucun";
+$l['mybb_acp'] = "Panneau d’administration de MyBB";
+$l['pages'] = "Pages";
+$l['previous'] = "Précédente";
+$l['page'] = "Page";
+$l['next'] = "Suivante";
+$l['delete'] = "Supprimer";
+$l['reset'] = "Réinitialiser";
+$l['and'] = "et";
+$l['on'] = "Activé";
+$l['off'] = "Désactivé";
+$l['alt_enabled'] = "Activé";
+$l['alt_disabled'] = "Désactivé";
+$l['enable'] = "Activer";
+$l['disable'] = "Désactiver";
+$l['saved'] = 'Enregistré';
+$l['guest'] = 'Invité';
+
+$l['rel_in'] = "Dans ";
+$l['rel_ago'] = "Il y a";
+$l['rel_less_than'] = "moins d’";
+$l['rel_time'] = "<span title=\"{5}{6}\">{4} {1}{2} {3}</span>";
+$l['rel_minutes_single'] = "minute";
+$l['rel_minutes_plural'] = "minutes";
+$l['rel_hours_single'] = "heure";
+$l['rel_hours_plural'] = "heures";
+
+// Parser bits
+$l['quote'] = "Citation :";
+$l['wrote'] = "a écrit :";
+$l['code'] = "Code:";
+$l['php_code'] = "Code PHP :";
+$l['linkback'] = "Message d’origine";
+
+// Les mois de l’année
+$l['january'] = "Janvier";
+$l['february'] = "Février";
+$l['march'] = "Mars";
+$l['april'] = "Avril";
+$l['may'] = "Mai";
+$l['june'] = "Juin";
+$l['july'] = "Juillet";
+$l['august'] = "Août";
+$l['september'] = "Septembre";
+$l['october'] = "Octobre";
+$l['november'] = "Novembre";
+$l['december'] = "Décembre";
+
+// Access Denied
+$l['access_denied'] = "Accès refusé";
+$l['access_denied_desc'] = "Vous n’avez pas la permission d’accéder à cette section du panneau d’administration.";
+
+// Super Administrator required
+$l['cannot_perform_action_super_admin_general'] = "Vous ne pouvez pas effectuer cette action car vous n’êtes pas un super administrateur.<br /><br />Pour pouvoir effectuer cette action, vous devez ajouter votre identifiant utilisateur à la liste des super administrateurs dans le fichier inc/config.php.";
+
+// AJAX
+$l['loading_text'] = "Chargement<br />Veuillez patienter…";
+
+// Time zone selection boxes
+
+$l['timezone_gmt_minus_1200'] = "(UTC -12:00) Îles Howland et Baker";
+$l['timezone_gmt_minus_1100'] = "(UTC -11:00) Nome, Îles Midway";
+$l['timezone_gmt_minus_1000'] = "(UTC -10:00) Hawaï, Papeete";
+$l['timezone_gmt_minus_950'] = "(UTC -9:30) Îles Marquises";
+$l['timezone_gmt_minus_900'] = "(UTC -9:00) Alaska";
+$l['timezone_gmt_minus_800'] = "(UTC -8:00) Heure du Pacifique";
+$l['timezone_gmt_minus_700'] = "(UTC -7:00) Heure des Rocheuses";
+$l['timezone_gmt_minus_600'] = "(UTC -6:00) Heure du Centre, Mexico";
+$l['timezone_gmt_minus_500'] = "(UTC -5:00) Heure de l’Est, Bogota, Lima, Quito";
+$l['timezone_gmt_minus_450'] = "(UTC -4:30) Caracas";
+$l['timezone_gmt_minus_400'] = "(UTC -4:00) Heure de l’Atlantique, La Paz, Halifax";
+$l['timezone_gmt_minus_350'] = "(UTC -3:30) Terre-Neuve";
+$l['timezone_gmt_minus_300'] = "(UTC -3:00) Brésil, Buenos Aires, Georgetown, Îles Falkland";
+$l['timezone_gmt_minus_200'] = "(UTC -2:00) Atlantique Sud, Géorgie du Sud et Îles Sandwich du Sud";
+$l['timezone_gmt_minus_100'] = "(UTC -1:00) Açores, Cap-Vert";
+$l['timezone_gmt'] = "(UTC) Casablanca, Dublin, Édimbourg, Londres, Lisbonne, Monrovia";
+$l['timezone_gmt_100'] = "(UTC +1:00) Berlin, Bratislava, Bruxelles, Copenhague, Madrid, Paris, Prague, Rome, Varsovie";
+$l['timezone_gmt_200'] = "(UTC +2:00) Athènes, Istanbul, Le Caire, Jérusalem, Afrique du Sud";
+$l['timezone_gmt_300'] = "(UTC +3:00) Kaliningrad, Minsk, Bagdad, Riyad, Nairobi";
+$l['timezone_gmt_350'] = "(UTC +3:30) Téhéran";
+$l['timezone_gmt_400'] = "(UTC +4:00) Moscou, Abou Dhabi, Bakou, Mascate, Tbilissi";
+$l['timezone_gmt_450'] = "(UTC +4:30) Kaboul";
+$l['timezone_gmt_500'] = "(UTC +5:00) Islamabad, Karachi, Tachkent";
+$l['timezone_gmt_550'] = "(UTC +5:30) Mumbai, Kolkata, Chennai, New Delhi";
+$l['timezone_gmt_575'] = "(UTC +5:45) Katmandou";
+$l['timezone_gmt_600'] = "(UTC +6:00) Almaty, Dhaka, Iekaterinbourg";
+$l['timezone_gmt_650'] = "(UTC +6:30) Yangon";
+$l['timezone_gmt_700'] = "(UTC +7:00) Bangkok, Hanoï, Jakarta";
+$l['timezone_gmt_800'] = "(UTC +8:00) Pékin, Hong Kong, Perth, Singapour, Taipei, Manille";
+$l['timezone_gmt_850'] = "(UTC +8:30) Pyongyang";
+$l['timezone_gmt_875'] = "(UTC +8:45) Eucla";
+$l['timezone_gmt_900'] = "(UTC +9:00) Osaka, Sapporo, Séoul, Tokyo, Irkoutsk";
+$l['timezone_gmt_950'] = "(UTC +9:30) Adélaïde, Darwin";
+$l['timezone_gmt_1000'] = "(UTC +10:00) Melbourne, Papouasie-Nouvelle-Guinée, Sydney, Iakoutsk";
+$l['timezone_gmt_1050'] = "(UTC +10:30) Île Lord Howe";
+$l['timezone_gmt_1100'] = "(UTC +11:00) Magadan, Nouvelle-Calédonie, Îles Salomon, Vladivostok";
+$l['timezone_gmt_1150'] = "(UTC +11:30) Île Norfolk";
+$l['timezone_gmt_1200'] = "(UTC +12:00) Auckland, Wellington, Fidji, Îles Marshall";
+$l['timezone_gmt_1275'] = "(UTC +12:45) Îles Chatham";
+$l['timezone_gmt_1300'] = "(UTC +13:00) Samoa, Tonga, Tokelau";
+$l['timezone_gmt_1400'] = "(UTC +14:00) Îles de la Ligne";
+$l['timezone_gmt_short'] = "UTC {1}({2})";
+
+// Global language strings used for log deletion pages
+$l['confirm_delete_logs'] = "Êtes-vous sûr de vouloir supprimer les entrées sélectionnées ?";
+$l['confirm_delete_all_logs'] = "Êtes-vous sûr de vouloir supprimer toutes les entrées filtrées ?";
+$l['selected_logs_deleted'] = "Les entrées sélectionnées ont été supprimées.";
+$l['all_logs_deleted'] = "Toutes les entrées du journal ont été supprimées.";
+
+$l['delete_selected'] = "Supprimer les entrées sélectionnées";
+$l['delete_all'] = "Supprimer toutes les entrées filtrées";
+
+// Misc
+$l['encountered_errors'] = "Les erreurs suivantes ont été rencontrées :";
+$l['invalid_post_verify_key'] = "Un code d’autorisation ne correspond pas. Veuillez confirmer que vous souhaitez effectuer l’action ci-dessous.";
+$l['invalid_post_verify_key2'] = "Un code d’autorisation ne correspond pas. Veuillez vérifier que vous accédez correctement à cette page.";
+$l['unknown_error'] = "Une erreur inconnue s’est produite.";
+
+// Code buttons editor language strings
+$l['editor_bold'] = "Mettre le texte en gras";
+$l['editor_italic'] = "Mettre le texte en italique";
+$l['editor_underline'] = "Souligner le texte";
+$l['editor_strikethrough'] = "Barrer le texte";
+$l['editor_subscript'] = "Mettre en indice";
+$l['editor_superscript'] = "Mettre en exposant";
+$l['editor_alignleft'] = "Aligner à gauche";
+$l['editor_center'] = "Centrer";
+$l['editor_alignright'] = "Aligner à droite";
+$l['editor_justify'] = "Justifier";
+$l['editor_fontname'] = "Nom de la police";
+$l['editor_fontsize'] = "Taille de la police";
+$l['editor_fontcolor'] = "Couleur de la police";
+$l['editor_removeformatting'] = "Supprimer la mise en page";
+$l['editor_cut'] = "Couper";
+$l['editor_copy'] = "Copier";
+$l['editor_paste'] = "Coller";
+$l['editor_cutnosupport'] = "Votre navigateur ne permet pas de couper du contenu. Veuillez utiliser le raccourci clavier « Ctrl/Cmd+X »";
+$l['editor_copynosupport'] = "Votre navigateur ne permet pas de copier du contenu. Veuillez utiliser le raccourci clavier « Ctrl/Cmd+C »";
+$l['editor_pastenosupport'] = "Votre navigateur ne permet pas de coller du contenu. Veuillez utiliser le raccourci clavier « Ctrl/Cmd+V »";
+$l['editor_pasteentertext'] = "Collez votre texte dans la zone ci-dessous :";
+$l['editor_pastetext'] = "Coller du texte";
+$l['editor_numlist'] = "Insérer une liste ordonnée";
+$l['editor_bullist'] = "Insérer une liste à puces";
+$l['editor_undo'] = "Annuler";
+$l['editor_redo'] = "Rétablir";
+$l['editor_rows'] = "Lignes :";
+$l['editor_cols'] = "Colonnes :";
+$l['editor_inserttable'] = "Insérer un tableau";
+$l['editor_inserthr'] = "Insérer une ligne horizontale";
+$l['editor_code'] = "Bloc de code";
+$l['editor_php'] = "Bloc de code PHP";
+$l['editor_width'] = "Largeur (optionnel) :";
+$l['editor_height'] = "Hauteur (optionnel) :";
+$l['editor_insertimg'] = "Insérer une image";
+$l['editor_email'] = "Adresse électronique :";
+$l['editor_insertemail'] = "Insérer une adresse électronique";
+$l['editor_url'] = "Adresse :";
+$l['editor_insertlink'] = "Insérer un lien";
+$l['editor_unlink'] = "Supprimer le lien";
+$l['editor_more'] = "Voir plus";
+$l['editor_insertemoticon'] = "Insérer une émoticône";
+$l['editor_videourl'] = "Adresse de la vidéo :";
+$l['editor_videotype'] = "Type de vidéo :";
+$l['editor_insert'] = "Insérer";
+$l['editor_insertyoutubevideo'] = "Insérer une vidéo YouTube";
+$l['editor_currentdate'] = "Insérer la date actuelle";
+$l['editor_currenttime'] = "Insérer l’heure actuelle";
+$l['editor_print'] = "Imprimer";
+$l['editor_viewsource'] = "Voir le code source";
+$l['editor_description'] = "Description (optionnel) :";
+$l['editor_enterimgurl'] = "Adresse de l’image :";
+$l['editor_enteremail'] = "Adresse électronique :";
+$l['editor_enterdisplayedtext'] = "Saisissez le texte à afficher :";
+$l['editor_enterurl'] = "Saisissez l’adresse :";
+$l['editor_enteryoutubeurl'] = "Saisissez l’URL ou l’ID de la vidéo YouTube :";
+$l['editor_insertquote'] = "Insérer une citation";
+$l['editor_invalidyoutube'] = "Vidéo YouTube invalide";
+$l['editor_dailymotion'] = "Dailymotion";
+$l['editor_metacafe'] = "MetaCafe";
+$l['editor_mixer'] = "Mixer";
+$l['editor_vimeo'] = "Vimeo";
+$l['editor_youtube'] = "Youtube";
+$l['editor_facebook'] = "Facebook";
+$l['editor_liveleak'] = "LiveLeak";
+$l['editor_insertvideo'] = "Insérer une vidéo";
+$l['editor_maximize'] = "Agrandir";
+
+$l['missing_task'] = "Erreur : le fichier de la tâche n’existe pas";
+$l['task_backup_cannot_write_backup'] = "Erreur : la tâche de sauvegarde de la base de données ne peut pas écrire dans le répertoire de sauvegardes.";
+$l['task_backup_ran'] = "La tâche de sauvegarde de la base de données s’est exécutée avec succès.";
+$l['task_checktables_ran'] = "La tâche de vérification des tables s’est exécutée avec succès et aucune table corrompue n’a été trouvée.";
+$l['task_checktables_ran_found'] = "Attention : la tâche de vérification des tables s’est exécutée avec succès et a réparé {1} table(s).";
+$l['task_dailycleanup_ran'] = "La tâche de nettoyage quotidien s’est exécutée avec succès.";
+$l['task_hourlycleanup_ran'] = "La tâche de nettoyage horaire s’est exécutée avec succès.";
+$l['task_logcleanup_ran'] = "La tâche de nettoyage des journaux s’est exécutée avec succès et les anciens journaux ont été supprimés.";
+$l['task_promotions_ran'] = "La tâche de promotion s’est exécutée avec succès.";
+$l['task_threadviews_ran'] = "La tâche de comptage des vues des sujets s’est exécutée avec succès.";
+$l['task_usercleanup_ran'] = "La tâche de nettoyage des utilisateurs s’est exécutée avec succès.";
+$l['task_massmail_ran'] = "La tâche d’envoi de courriels groupés s’est exécutée avec succès.";
+$l['task_userpruning_ran'] = "La tâche de suppression des utilisateurs inactifs s’est exécutée avec succès.";
+$l['task_delayedmoderation_ran'] = "La tâche de modération différée s’est exécutée avec succès.";
+$l['task_massmail_ran_errors'] = "Une ou plusieurs erreurs se sont produites lors de l’envoi à « {1} » :
+{2}";
+$l['task_versioncheck_ran'] = "La tâche de vérification de version a été exécutée avec succès.";
+$l['task_versioncheck_ran_errors'] = "Impossible de se connecter à MyBB pour vérifier la version.";
+$l['task_recachestylesheets_ran'] = "Les {1} feuilles de style ont été régénérées.";
+$l['task_sendmailqueue_ran'] = "La tâche d’envoi des courriels a pour le moment traité {1} messages.";
+
+$l['massmail_username'] = "Nom d’utilisateur";
+$l['email_addr'] = "Adresse électronique";
+$l['board_name'] = "Nom du forum";
+$l['board_url'] = "Adresse du forum";
+
+// Unlock ACP
+$l['lockout_unlock'] = "Déverrouiller l’accès au panneau d’administration";
+$l['enter_username_and_token'] = "Veuillez saisir votre nom d’utilisateur et le code d’activation pour continuer.";
+$l['unlock_token'] = "Code d’activation :";
+$l['unlock_account'] = "Déverrouiller le compte";
+
+// Email message for if an admin account has been locked out
+$l['locked_out_subject'] = "Compte administrateur verrouillé sur {1}";
+$l['locked_out_message'] = "{1},
+
+Votre compte administrateur sur {2} a été verrouillé après {3} tentatives de connexion.
+
+Pour déverrouiller votre compte, veuillez vous rendre à l’adresse suivante :
+
+{4}/{5}/index.php?action=unlock&uid={7}&token={6}
+
+Si le lien ci-dessus ne fonctionne pas correctement, rendez-vous sur :
+
+{4}/{5}/index.php?action=unlock
+
+Vous devrez saisir les informations suivantes :
+Nom d’utilisateur : {1}
+Code d’activation : {6}
+
+Merci,
+L’équipe du forum";
+
+$l['comma'] = ", ";
+
+$l['search_for_a_user'] = "Rechercher un utilisateur";
+
+$l['mybb_engine'] = "MyBB";
+
+// If the language string for "Username" is too cramped in the ACP Login box
+// then use this to define how much larger you want the gap to be (in px)
+// $l['login_field_width'] = "0";
+
+$l['my2fa'] = "Authentification à deux facteurs";
+$l['my2fa_failed'] = "Le code est incorrect, vous avez été déconnecté";
+$l['my2fa_code'] = "Veuillez saisir le code d’authentification";
+$l['my2fa_label'] = "Code d’authentification :";
+$l['my2fa_no_codes'] = "Vous avez utilisé tous vos codes de récupération. Veuillez vous rendre sur la <a href=\"index.php?module=home-preferences&amp;action=recovery_codes\">page des codes de récupération</a> pour générer une nouvelle liste.";
