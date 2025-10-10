de05878d34d3219a48a7518ec07426748c8e94d4
Axel Fontana <axel.fontana@qualifio.com>
2025-10-10 21:12:20 +0200

    feat: add translation for config-profile-fields

---
+++ french/admin/config_profile_fields.lang.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['custom_profile_fields'] = "Champs de profil personnalisés";
+$l['custom_profile_fields_desc'] = "Cette page vous permet de gérer les champs de profil personnalisés.";
+$l['add_profile_field'] = "Ajouter un champ de profil";
+$l['add_new_profile_field'] = "Ajouter un champ de profil";
+$l['add_new_profile_field_desc'] = "Cette page vous permet d’ajouter un nouveau champ de profil personnalisé.";
+$l['edit_profile_field'] = "Modifier un champ de profil";
+$l['edit_profile_field_desc'] = "Cette page vous permet de modifier un champ de profil personnalisé existant.";
+
+$l['title'] = "Nom";
+$l['short_description'] = "Brève description";
+$l['maximum_length'] = "Longueur maximale";
+$l['maximum_length_desc'] = "Nombre maximum de caractères pouvant être saisis. Cela s’applique uniquement aux champs texte et aux zones de texte.";
+$l['field_length'] = "Longueur du champ";
+$l['field_length_desc'] = "La longueur de ce champ. Cela ne s’applique qu’aux listes déroulantes simples ou multiples.";
+$l['display_order'] = "Ordre d’affichage";
+$l['display_order_desc'] = "Ceci définit l’ordre d’affichage des champs de profil. Cette valeur ne doit pas être identique à celle d’un autre champ.";
+$l['text'] = "Texte";
+$l['textarea'] = "Zone de texte";
+$l['select'] = "Liste déroulante";
+$l['multiselect'] = "Liste déroulante à choix multiple";
+$l['radio'] = "Boutons radio";
+$l['checkbox'] = "Cases à cocher";
+$l['field_type'] = "Type de champ";
+$l['field_type_desc'] = "Type de champ qui sera affiché aux utilisateurs.";
+$l['contact_field'] = "Champ de contact";
+$l['contact_field_desc'] = "Ce champ doit-il être considéré comme un champ de contact ?";
+$l['field_regex'] = "Expression régulière";
+$l['field_regex_desc'] = "Saisissez une expression régulière devant être comparée avec la saisie de l’utilisateur. Assurez-vous qu’elle soit valide et sécurisée — aucune vérification n’est effectuée. Laissez-vide pour ne pas filtrer la saisie.<br />
+<strong>Exemple :</strong> ([a-z0-9_\- ,.+]+)";
+$l['selectable_options'] = "Options sélectionnables ?";
+$l['selectable_options_desc'] = "Veuillez saisir une option par ligne. Cela ne s’applique qu’aux listes déroulantes, cases à cocher et boutons radio.";
+$l['required'] = "Obligatoire ?";
+$l['required_desc'] = "Ce champ doit-il être complété lors de l’inscription ou de la modification du profil ? Notez que cela ne s’applique pas si le champ est masqué ou non modifiable.";
+$l['show_on_registration'] = "Afficher à l’inscription ?";
+$l['show_on_registration_desc'] = "Ce champ doit-il apparaître dans le formulaire d’inscription ? Cela ne s’applique pas si le champ n’est pas modifiable. Les champs obligatoires apparaîtront toujours lors de l’inscription.";
+$l['display_on_profile'] = "Afficher sur le profil ?";
+$l['display_on_profile_desc'] = "Ce champ doit-il être affiché sur le profil des utilisateurs ?";
+$l['display_on_postbit'] = "Afficher sur les messages ?";
+$l['display_on_postbit_desc'] = "Ce champ doit-il être affiché dans les messages des utilisateurs ?";
+$l['viewableby'] = "Consultable par";
+$l['viewableby_desc'] = "Sélectionnez les groupes autorisés à consulter ce champ de profil.";
+$l['editableby'] = "Modifiable par";
+$l['editableby_desc'] = "Sélectionnez les groupes autorisés à modifier ce champ de profil.";
+$l['min_posts_enabled'] = "Nombre minimal de messages ?";
+$l['min_posts_enabled_desc'] = "Ce champ doit-il être disponible uniquement pour les utilisateurs ayant un certain nombre de messages ? Si oui, veuillez préciser ce nombre.";
+$l['parser_options'] = "Options de formatage";
+$l['parse_allowhtml'] = "Autoriser l’HTML dans ce champ de profil.";
+$l['parse_allowmycode'] = "Autoriser les MyCodes dans ce champ de profil.";
+$l['parse_allowsmilies'] = "Autoriser les émoticônes dans ce champ de profil.";
+$l['parse_allowimgcode'] = "Autoriser les balises [img] dans ce champ de profil.";
+$l['parse_allowvideocode'] = "Autoriser les balises [video] dans ce champ de profil.";
+$l['save_profile_field'] = "Enregistrer";
+$l['name'] = "Nom";
+$l['contact'] = "Champ de contact ?";
+$l['registration'] = "Inscription ?";
+$l['editable'] = "Modifiable ?";
+$l['profile'] = "Affiché sur le profil ?";
+$l['postbit'] = "Affiché dans les sujets ?";
+$l['edit_field'] = "Modifier";
+$l['delete_field'] = "Supprimer";
+$l['no_profile_fields'] = "Il n’y a actuellement aucun champ de profil personnalisé sur votre forum.";
+
+$l['error_missing_name'] = "Veuillez saisir un nom pour ce champ de profil.";
+$l['error_missing_description'] = "Veuillez saisir une description pour ce champ de profil.";
+$l['error_invalid_fid'] = "Le champ de profil spécifié est invalide ou n’existe pas.";
+
+$l['success_profile_field_added'] = "Le champ de profil a bien été créé.";
+$l['success_profile_field_saved'] = "Le champ de profil a bien été mis à jour.";
+$l['success_profile_field_deleted'] = "Le champ de profil a bien été supprimé.";
+
+$l['confirm_profile_field_deletion'] = "Êtes-vous sûr de vouloir supprimer ce champ de profil ?";
