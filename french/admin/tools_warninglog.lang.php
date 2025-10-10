ae761722771e68d0bfc0b59218fa2b3b3788d5da
Axel Fontana <axel.fontana@qualifio.com>
2025-10-10 20:31:06 +0200

    feat: add translation for tools-warninglog

---
+++ french/admin/tools_warninglog.lang.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['warning_logs'] = "Journal des avertissements";
+$l['warning_logs_desc'] = "Cette page vous permet de consulter le journal des avertissements donnés aux utilisateurs du forum.";
+$l['warned_user'] = "Utilisateur averti";
+$l['warning'] = "Avertissement";
+$l['date_issued'] = "Date de l’avertissement";
+$l['expires'] = "Expiration";
+$l['expiry_date'] = "Date d’expiration";
+$l['issued_date'] = "Date de l’avertissement";
+$l['issued_by'] = "Utilisateur ayant donné l’avertissement";
+$l['details'] = "Détails";
+$l['filter_warning_logs'] = "Filtrer le journal des avertissements";
+$l['filter_warned_user'] = "Utilisateur averti :";
+$l['filter_issued_by'] = "Avertissement donné par :";
+$l['filter_reason'] = "Le motif contient :";
+$l['sort_by'] = "Trier par :";
+$l['results_per_page'] = "Résultats par page :";
+$l['view'] = "Visualiser";
+$l['no_warning_logs'] = "Aucune entrée ne correspond aux critères sélectionnés.";
+$l['revoked'] = "Révoqué ";
+$l['post'] = "Message";
+
+$l['asc'] = "croissant";
+$l['desc'] = "décroissant";
+
+$l['in'] = "dans l’ordre";
+$l['order'] = " ";
+
+$l['warning_details'] = "Détails de l’avertissement";
+$l['warning_note'] = "Notes concernant l’avertissement";
+$l['already_expired'] = "Expiré";
+$l['warning_revoked'] = "Révoqué";
+$l['warning_active'] = "Actif";
+$l['error_invalid_warning'] = "L’avertissement spécifié est invalide ou n’existe pas.";
+
+$l['revoke_warning'] = "Révoquer cet avertissement";
+$l['revoke_warning_desc'] = "Veuillez saisir la raison pour laquelle vous souhaitez révoquer cet avertissement. Cela ne supprimera pas les bannissements ni les suspensions liés à cet avertissement.";
+
+$l['reason'] = "Raison :";
+$l['warning_is_revoked'] = "Cet avertissement a bien été révoqué.";
+$l['revoked_by'] = "Révoqué par :";
+$l['date_revoked'] = "Date de révocation :";
+$l['error_already_revoked'] = "Cet avertissement a déjà été révoqué.";
+$l['error_no_revoke_reason'] = "Veuillez saisir la raison pour laquelle vous souhaitez révoquer cet avertissement.";
+$l['redirect_warning_revoked'] = "Cet avertissement a bien été révoqué et le niveau d’avertissement de l’utilisateur a été diminué.";
+
+$l['warning_points'] = "({1} points)";
