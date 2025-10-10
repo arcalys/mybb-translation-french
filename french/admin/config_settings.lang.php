1dbd8c631308e17ad27e33fd962c58ce3c65b830
Axel Fontana <axel.fontana@qualifio.com>
2025-10-10 20:06:22 +0200

    feat: add translation for config-settings

---
+++ french/admin/config_settings.lang.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['board_settings'] = "Paramètres du forum";
+$l['change_settings'] = "Configurer le forum";
+$l['change_settings_desc'] = "Cette page vous permet de gérer l’ensemble des paramètres liés à votre forum. Sélectionnez un groupe de paramètres ci-dessous pour en modifier les options.";
+$l['add_new_setting'] = "Ajouter un paramètre";
+$l['add_new_setting_desc'] = "Cette page vous permet d’ajouter un nouveau paramètre à votre forum.";
+$l['modify_existing_settings'] = "Modifier les paramètres";
+$l['modify_existing_settings_desc'] = "Cette page vous permet de modifier les paramètres existants et leur ordre d’affichage.";
+$l['add_new_setting_group'] = "Ajouter un groupe de paramètres";
+$l['add_new_setting_group_desc'] = "Cette page vous permet d’ajouter un nouveau groupe de paramètres à votre forum.";
+$l['edit_setting_group'] = "Modifier le groupe de paramètres";
+$l['edit_setting_group_desc'] = "Cette page vous permet de modifier un groupe de paramètres existant.";
+
+$l['title'] = "Titre";
+$l['description'] = "Description";
+$l['group'] = "Groupe";
+$l['display_order'] = "Ordre d’affichage";
+$l['name'] = "Identifiant";
+$l['name_desc'] = "Cet identifiant unique est utilisé pour référencer ce paramètre dans les scripts, traductions et templates.";
+$l['group_name_desc'] = "Cet identifiant unique est utilisé par le système de traduction.";
+$l['text'] = "Texte";
+$l['numeric_text'] = "Texte numérique";
+$l['textarea'] = "Zone de texte";
+$l['yesno'] = "Choix Oui / Non";
+$l['onoff'] = "Choix Activé / Désactivé";
+$l['select'] = "Liste déroulante";
+$l['radio'] = "Boutons radio";
+$l['checkbox'] = "Cases à cocher";
+$l['language_selection_box'] = "Liste de sélection de la langue";
+$l['forum_selection_box'] = "Liste de sélection de forums (multiple)";
+$l['forum_selection_single'] = "Liste de sélection d’un forum";
+$l['group_selection_box'] = "Liste de sélection de groupes (multiple)";
+$l['group_selection_single'] = "Liste de sélection d’un groupe";
+$l['adminlanguage'] = "Liste de sélection de la langue du panneau d’administration";
+$l['cpstyle'] = "Liste de sélection du thème du panneau de contrôle";
+$l['prefix_selection_box'] = "Liste de sélection de préfixes";
+$l['php'] = "Code PHP évalué";
+$l['type'] = "Type";
+$l['extra'] = "Informations supplémentaires";
+$l['extra_desc'] = "Si ce paramètre est une liste de sélection, un bouton radio ou une case à cocher, entrez une liste d’éléments sous forme de paires clé=élément. Séparez chaque élément par une nouvelle ligne. S’il s’agit de PHP, entrez le code PHP à évaluer.";
+$l['value'] = "Valeur";
+$l['insert_new_setting'] = "Enregistrer";
+$l['edit_setting'] = "Modifier ce paramètre";
+$l['delete_setting'] = "Supprimer ce paramètre";
+$l['setting_configuration'] = "Configuration du paramètre";
+$l['update_setting'] = "Enregistrer";
+$l['save_settings'] = "Enregistrer les paramètres";
+$l['setting_groups'] = "Groupes de paramètres";
+$l['bbsettings'] = "paramètres";
+$l['insert_new_setting_group'] = "Enregistrer";
+$l['setting_group_setting'] = "Groupe de paramètres/Paramètre";
+$l['order'] = "Ordre";
+$l['delete_setting_group'] = "Supprimer le groupe de paramètres";
+$l['save_display_orders'] = "Mettre à jour l’ordre d’affichage";
+$l['update_setting_group'] = "Enregistrer";
+$l['modify_setting'] = "Modifier un paramètre";
+$l['search'] = "Rechercher";
+$l['plugin_settings'] = "Paramètres additionnels";
+
+$l['show_all_settings'] = "Afficher tous les paramètres";
+$l['settings_search'] = "Rechercher dans les paramètres";
+
+$l['confirm_setting_group_deletion'] = "Êtes-vous sûr de vouloir supprimer ce groupe de paramètres ?";
+$l['confirm_setting_deletion'] = "Êtes-vous sûr de vouloir supprimer ce paramètre ?";
+
+$l['error_format_dimension'] = "Le format défini pour {1} est invalide.";
+$l['error_field_minnamelength'] = "La longueur minimale du nom d’utilisateur ne peut pas être supérieure à la longueur maximale.";
+$l['error_field_minpasswordlength'] = "La longueur minimale du mot de passe ne peut pas être supérieure à la longueur maximale.";
+$l['error_field_minpasswordlength_complex'] = "La longueur minimale du mot de passe doit être d’au moins 3 caractères lorsque les mots de passe complexes sont activés.";
+$l['error_field_postmaxavatarsize'] = "Dimensions maximales de l’avatar dans les messages";
+$l['error_field_useravatardims'] = "Dimensions par défaut de l’avatar";
+$l['error_field_maxavatardims'] = "Dimensions maximales de l’avatar";
+$l['error_field_memberlistmaxavatarsize'] = "Dimensions maximales de l’avatar affiché dans la liste des membres";
+$l['error_missing_title'] = "Veuillez saisir un titre pour ce paramètre.";
+$l['error_missing_group_title'] = "Veuillez saisir un titre pour ce groupe de paramètres.";
+$l['error_invalid_gid'] = "Veuillez sélectionner un groupe valide pour ce paramètre.";
+$l['error_invalid_gid2'] = "Le groupe de paramètres spécifié est invalide ou n’existe pas.";
+$l['error_missing_name'] = "Veuillez saisir un identifiant pour ce paramètre.";
+$l['error_missing_group_name'] = "Veuillez saisir un identifiant pour ce groupe de paramètres.";
+$l['error_invalid_type'] = "Veuillez saisir un type valide pour ce paramètre";
+$l['error_invalid_sid'] = "Le paramètre spécifié est invalide ou n’existe pas.";
+$l['error_duplicate_name'] = "L’identifiant spécifié est déjà utilisé pour le paramètre « {1} » — il doit être unique.";
+$l['error_duplicate_group_name'] = "L’identifiant spécifié est déjà utilisé pour le groupe de paramètres « {1} » — il doit être unique.";
+$l['error_no_settings_found'] = "Aucun paramètre n’a été trouvé.";
+$l['error_cannot_edit_default'] = "Les paramètres et groupes par défaut ne peuvent pas être modifiés ou supprimés.";
+$l['error_cannot_edit_php'] = "Il s’agit d’un type de paramètre spécial qui ne peut pas être modifié.";
+$l['error_ajax_search'] = "Un problème est survenu lors de la recherche des paramètres :";
+$l['error_ajax_unknown'] = "Une erreur inconnue s’est produite lors de la recherche des paramètres.";
+$l['error_chmod_settings_file'] = "Le fichier de configuration « ./inc/settings.php » n’est pas accessible en écriture. Veuillez ajuster ses permissions (CHMOD à 666).<br />Pour plus d’informations sur le CHMOD, consultez la <a href=\"https://docs.mybb.com/1.8/administration/security/file-permissions\" target=\"_blank\" rel=\"noopener\">documentation officielle</a>.";
+$l['error_admin_email_settings_empty'] = '<div class="smalltext" style="font-weight: normal;">Veuillez noter que l’<strong>adresse électronique de l’administrateur</strong> n’a pas été enregistrée car ce champ doit contenir une adresse électronique valide.</div>';
+
+$l['success_setting_added'] = "Le paramètre a bien été créé.";
+$l['success_setting_updated'] = "Le paramètre a bien été mis à jour.";
+$l['success_setting_deleted'] = "Le paramètre a bien été supprimé.";
+$l['success_settings_updated'] = "Les paramètres ont bien été mis à jour.";
+$l['success_settings_updated_hiddencaptchaimage'] = '<div class="smalltext" style="font-weight: normal;">Veuillez noter que le paramètre <strong>Champ CAPTCHA caché</strong> a été rétabli à <strong>{1}</strong> en raison d’un conflit avec le champ <strong>{2}</strong> dans le formulaire d’inscription.</div>';
+$l['success_settings_updated_username_method_conflict'] = '<div class="smalltext" style="font-weight: normal;">Veuillez noter que le paramètre <strong>Méthodes de connexion autorisées</strong> n’a pas été mis à jour car l’enregistrement multiple d’une même adresse électronique est actuellement autorisé.</div>';
+$l['success_settings_updated_username_method'] = '<div class="smalltext" style="font-weight: normal;">Veuillez noter que le paramètre <strong>Méthodes de connexion autorisées</strong> n’a pas été mis à jour, car plusieurs utilisateurs utilisent actuellement la même adresse électronique.</div>';
+$l['success_settings_updated_allowmultipleemails'] = '<div class="smalltext" style="font-weight: normal;">Veuillez noter que le paramètre <strong>Autoriser l’enregistrement multiple d’une même adresse électronique ?</strong> ne peut pas être activé car le paramètre <strong>Méthodes de connexion autorisées</strong> permet la connexion via l’adresse électronique.</div>';
+$l['success_settings_updated_captchaimage'] = '<div class="smalltext" style="font-weight: normal;">Veuillez noter que le paramètre <strong>Images CAPTCHA pour l’inscription et la publication</strong> a été rétabli à <strong>CAPTCHA par défaut de MyBB</strong> en raison de l’absence de clé(s) publique(s)/privée(s).</div>';
+$l['success_settings_updated_minsearchword'] = '<div class="smalltext" style="font-weight: normal;">Veuillez noter que le paramètre <strong>Longueur minimale des termes de recherche</strong> a été ajusté pour correspondre à la configuration du système de base de données.</div>';
+$l['success_display_orders_updated'] = "L’ordre d’affichage des paramètres a bien été mis à jour.";
+$l['success_setting_group_added'] = "Le groupe de paramètres a bien été créé.";
+$l['success_setting_group_updated'] = "Le groupe de paramètres a bien été mis à jour.";
+$l['success_setting_group_deleted'] = "Le groupe de paramètres a bien été supprimé.";
+$l['success_duplicate_settings_deleted'] = "Tous les groupes de paramètres en double ont bien été supprimés.";
+
+$l['searching'] = 'Recherche en cours…';
+$l['search_error'] = 'Une erreur s’est produite lors de la récupération des résultats de recherche :';
+$l['search_done'] = 'Terminé !';
