64f0901012fa3d69945d592a792b846b223630ce
Axel Fontana <axel.fontana@qualifio.com>
2025-10-11 16:43:15 +0200

    feat: add translation for user-mass-mail

---
+++ french/admin/user_mass_mail.lang.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['mass_mail'] = "Courriels groupés";
+
+$l['mass_mail_queue'] = "File d'attente des courriels groupés";
+$l['mass_mail_queue_desc'] = "Cette page vous permet de gérer les courriels groupés planifiés ou en cours d’envoi.";
+$l['create_mass_mail'] = "Créer un nouveau courriel groupé";
+$l['create_mass_mail_desc'] = "Cette page vous permet de créer un nouveau courriel groupé et de définir ses options.";
+$l['mass_mail_archive'] = "Archive des courriels groupés";
+$l['mass_mail_archive_desc'] = "Cette page vous permet de consulter l'historique des courriels groupés terminés.";
+$l['edit_mass_mail'] = "Modifier l'envoi groupé";
+$l['edit_mass_mail_desc'] = "Ici, vous pouvez modifier un envoi groupé et ses options.";
+$l['send_mass_mail'] = "Envoyer le courriel groupé";
+
+$l['email_addr'] = "Adresse électronique";
+$l['board_name'] = "Nom du forum";
+$l['board_url'] = "Adresse du forum";
+$l['personalize_message'] = "Personnaliser ce message :";
+
+$l['message_settings'] = "Paramètres du message";
+$l['subject'] = "Sujet";
+$l['subject_desc'] = "Indiquez l’objet de votre message.";
+$l['send_via_email'] = "Envoyer un courriel";
+$l['send_via_pm'] = "Envoyer un message privé";
+$l['message_type'] = "Type d’envoi";
+$l['deliver_immediately'] = "Envoi immédiat";
+$l['deliver_specific'] = "Envoi différé";
+$l['delivery_date'] = "Date d’envoi";
+$l['delivery_date_desc'] = "Veuillez saisir la date et l’heure auxquelles vous souhaitez envoyer ce message.";
+$l['per_page'] = "Envois simultanés";
+$l['per_page_desc'] = "Nombre de courriels à envoyer simultanément.";
+$l['plain_text_only'] = "Texte uniquement";
+$l['html_only'] = "HTML uniquement";
+$l['html_and_plain_text'] = "HTML et texte";
+$l['message_format'] = "Format du message";
+$l['define_html_message'] = "Contenu du message (HTML)";
+$l['define_html_message_desc'] = "Saisissez la version HTML du message";
+$l['auto_gen_plain_text'] = "Tenter de générer automatiquement la version texte";
+$l['define_text_version'] = "Contenu du message (texte)";
+$l['define_text_version_desc'] = "Saisissez la version texte du message";
+$l['define_the_recipients'] = "Définir les destinataires";
+$l['username_contains'] = "Le nom d’utilisateur contient…";
+$l['email_addr_contains'] = "L’adresse électronique contient…";
+$l['members_of'] = "Les membres des groupes suivants…";
+$l['greater_than'] = "plus de";
+$l['is_exactly'] = "exactement";
+$l['less_than'] = "moins de";
+$l['more_than'] = "plus de";
+$l['post_count_is'] = "Le nombre de messages est";
+$l['hours'] = "heures";
+$l['days'] = "jours";
+$l['weeks'] = "semaines";
+$l['months'] = "mois";
+$l['years'] = "années";
+$l['ago'] = " ";
+$l['user_last_active'] = "La date de dernière activité était il y a";
+$l['user_registered'] = "L’utilisateur s’est inscrit il y a";
+$l['save_mass_mail'] = "Enregistrer";
+$l['additional_user_groups_desc'] = "Maintenez « CTRL » pour en sélectionner plusieurs.";
+
+$l['step_four'] = "Étape 4";
+$l['delivery_method'] = "Type d’envoi";
+$l['private_message'] = "Message privé";
+$l['email'] = "Courriel";
+$l['subject'] = "Sujet";
+$l['message'] = "Message";
+$l['text_based'] = "Texte";
+$l['preview'] = "Prévisualiser";
+$l['mass_mail_preview'] = "Prévisualiser un courriel groupé";
+$l['html_based'] = "HTML";
+$l['total_recipients'] = "Nombre de destinataires";
+$l['change_recipient_conds'] = "Modifier les critères des destinataires";
+$l['review_message'] = "Vérifier votre message";
+$l['define_delivery_date'] = "Fixer la date d’envoi";
+$l['schedule_for_delivery'] = "Planifier l’envoi";
+$l['username'] = "Nom d’utilisateur";
+
+$l['step_three'] = "Étape 3";
+$l['next_step'] = "Étape suivante";
+
+$l['step_two'] = "Étape 2";
+$l['review_text_version'] = "Vérifier la version texte";
+$l['review_text_version_desc'] = "Veuillez vérifier la version texte générée automatiquement.";
+
+$l['step_one'] = "Étape 1";
+
+$l['status'] = "Statut";
+$l['recipients'] = "Destinataires";
+$l['delivered'] = "Envoyé";
+$l['canceled'] = "Annulé";
+$l['resend'] = "Renvoyer";
+$l['no_archived_messages'] = "Il n’y a actuellement aucun courriel groupé envoyé ou annulé sur votre forum.";
+
+$l['draft'] = "Brouillon";
+$l['queued'] = "Dans la file d’attente";
+$l['delivering'] = "En cours";
+$l['na'] = "N/A";
+$l['mass_mail_cancel_confirmation'] = "Êtes-vous sûr de vouloir annuler l’envoi de ce courriel groupé ?";
+$l['no_unsent_messages'] = "Il n’y a actuellement aucun courriel groupé planifié ou en cours d’envoi sur votre forum.";
+
+$l['error_invalid_mid'] = "Le courriel groupé spécifié est invalide ou n’existe pas.";
+$l['error_only_in_future'] = "Vous ne pouvez pas planifier un envoi dans le passé.";
+$l['error_no_users'] = "Vos critères de recherche n'ont renvoyé aucun utilisateur. Veuillez ajuster vos critères et réessayer.";
+$l['error_missing_plain_text'] = "Veuillez saisir un message texte pour ce courriel groupé.";
+$l['error_missing_subject'] = "Veuillez saisir un titre pour ce courriel groupé.";
+$l['error_missing_message'] = "Veuillez saisir un message pour ce courriel groupé.";
+$l['error_missing_html'] = "Veuillez saisir un message HTML pour ce courriel groupé.";
+$l['error_delete_invalid_mid'] = "Le courriel groupé que vous souhaitez supprimer est invalide ou n’existe pas.";
+
+$l['success_mass_mail_saved'] = "Le courriel groupé a bien été enregistré.";
+$l['success_mass_mail_deleted'] = "Le courriel groupé a bien été supprimé.";
+$l['success_mass_mail_resent'] = "Le courriel groupé a bien été dupliqué. Veuillez le vérifier.";
+$l['success_mass_mail_canceled'] = "Le courriel groupé a bien été annulé.";
+
+$l['mass_mail_deletion_confirmation'] = "Êtes-vous sûr de vouloir supprimer ce courriel groupé ?";
