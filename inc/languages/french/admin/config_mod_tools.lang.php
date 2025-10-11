1f1e88a11ac8b9ab40dabd053957a23515775954
Axel Fontana <axel.fontana@qualifio.com>
2025-10-11 18:58:23 +0200

    feat: add translation for config-mod-tools

---
+++ inc/languages/french/admin/config_mod_tools.lang.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['mod_tools'] = "Outils de modération";
+
+$l['thread_tools'] = "Outils de sujet";
+$l['thread_tools_desc'] = "Cette page vous permet de gérer des outils de sujet personnalisés. Ces outils permettent de créer des combinaisons d’actions de modération pouvant être appliquées aux sujets et aux messages. Ils fonctionnent comme les outils de modération standard lors de la gestion de votre forum.";
+
+$l['add_thread_tool'] = "Ajouter un outil de sujet";
+$l['add_new_thread_tool'] = "Ajouter un outil de sujet";
+$l['add_thread_tool_desc'] = "Cette page vous permet d’ajouter un nouvel outil de modération de sujet. Cet outil sera accessible dans les forums et dans les sujets aux côtés des outils de modération standard.";
+
+$l['post_tools'] = "Outils de message";
+$l['post_tools_desc'] = "Cette page vous permet de gérer des outils de message personnalisés. Ces outils permettent de créer des combinaisons d’actions de modération pouvant être appliquées aux sujets et aux messages. Ils fonctionnent comme les outils de modération standard lors de la gestion de votre forum.";
+
+$l['add_post_tool'] = "Ajouter un outil de message";
+$l['add_new_post_tool'] = "Ajouter un outil de message";
+$l['add_post_tool_desc'] = "Cette page vous permet d’ajouter un nouvel outil de modération de message. Cet outil sera accessible dans les sujets aux côtés des outils de modération standard.";
+
+$l['edit_post_tool'] = "Modifier un outil de message";
+$l['edit_post_tool_desc'] = "Cette page vous permet de modifier un outil de message.";
+$l['edit_thread_tool'] = "Modifier un outil de sujet";
+$l['edit_thread_tool_desc'] = "Cette page vous permet de modifier un outil de sujet.";
+
+$l['no_thread_tools'] = "Il n’y a actuellement aucun outil de sujet sur votre forum.";
+$l['no_post_tools'] = "Il n’y a actuellement aucun outil de message sur votre forum.";
+
+$l['confirm_thread_tool_deletion'] = "Êtes-vous sûr de vouloir supprimer cet outil de sujet ?";
+$l['confirm_post_tool_deletion'] = "Êtes-vous sûr de vouloir supprimer cet outil de message ?";
+
+$l['success_post_tool_deleted'] = "L’outil de message sélectionné a bien été supprimé.";
+$l['success_thread_tool_deleted'] = "L’outil de sujet sélectionné a bien été supprimé.";
+
+$l['error_invalid_post_tool'] = "L’outil de message spécifié est invalide ou n’existe pas.";
+$l['error_invalid_thread_tool'] = "L’outil de sujet spécifié est invalide ou n’existe pas.";
+
+$l['general_options'] = "Options générales";
+$l['short_description'] = "Brève description";
+$l['available_in_forums'] = "Disponibilité dans les forums";
+$l['available_to_groups'] = "Disponibilité pour les groupes";
+$l['show_confirmation'] = "Afficher une page de confirmation";
+$l['save_thread_tool'] = "Enregistrer";
+
+$l['title'] = "Nom";
+
+$l['thread_moderation'] = "Modération du sujet";
+$l['approve_unapprove'] = "Approuver/désapprouver le sujet ?";
+
+$l['no_change'] = "Ne rien changer";
+$l['approve'] = "Approuver";
+$l['unapprove'] = "Désapprouver";
+$l['open'] = "Déverrouiller";
+$l['close'] = "Verrouiller";
+$l['stick'] = "Épingler";
+$l['unstick'] = "Désépingler";
+$l['toggle'] = "Inverser";
+$l['days'] = "Jours";
+$l['no_prefix'] = "No Prefix";
+$l['restore'] = "Restaurer";
+$l['softdelete'] = "Supprimer temporairement";
+
+$l['forum_to_move_to'] = "Forum vers lequel déplacer :";
+$l['leave_redirect'] = "Laisser une redirection ?";
+$l['delete_redirect_after'] = "Supprimer la redirection après";
+$l['do_not_move_thread'] = "Ne pas déplacer le sujet";
+$l['do_not_copy_thread'] = "Ne pas copier le sujet";
+$l['move_thread'] = "Déplacer le sujet ?";
+$l['move_thread_desc'] = "Si vous déplacez la ou les sujets, le champ « supprimer la redirection après… jours » ne doit être renseigné que si une redirection est laissée.";
+$l['forum_to_copy_to'] = "Forum vers lequel copier :";
+$l['copy_thread'] = "Copier le sujet ?";
+$l['open_close_thread'] = "Déverrouiller/verrouiller le sujet ?";
+$l['stick_unstick_thread'] = "Épingler/désépingler le sujet ?";
+$l['softdelete_restore_thread'] = "Supprimer temporairement ou restaurer le sujet ?";
+$l['delete_thread'] = "Supprimer définitivement le sujet ?";
+$l['merge_thread'] = "Fusionner le sujet ?";
+$l['merge_thread_desc'] = "Utilisable uniquement dans la modération rapide.";
+$l['delete_poll'] = "Supprimer le sondage ?";
+$l['delete_redirects'] = "Supprimer les redirections ?";
+$l['remove_subscriptions'] = "Supprimer les abonnements au sujet ?";
+$l['recount_rebuild'] = "Recalculer et reconstruire ?";
+$l['apply_thread_prefix'] = "Appliquer un préfixe au sujet ?";
+$l['new_subject'] = "Nouveau titre ?";
+$l['new_subject_desc'] = "Saisissez le nouveau titre du sujet.";
+
+$l['subject_message_replacements'] = "Remplacements disponibles";
+$l['subject_message_replacements_desc'] = "{subject} représente le sujet d’origine. {username} représente le nom d’utilisateur du modérateur. {author} représente le nom d’utilisateur de l’auteur du sujet.";
+
+$l['add_new_reply'] = "Publier une réponse";
+$l['add_new_reply_desc'] = "Laissez ce champ vide pour désactiver cette fonctionnalité.";
+$l['reply_subject'] = "Sujet de la réponse";
+$l['reply_subject_desc'] = "Utilisé uniquement si une réponse est ajoutée.";
+
+$l['success_mod_tool_created'] = "L’outil de modération a bien été créé.";
+$l['success_mod_tool_updated'] = "L’outil de modération a bien été mis à jour.";
+
+$l['inline_post_moderation'] = "Modération rapide";
+$l['delete_posts'] = "Supprimer définitivement les messages ?";
+$l['merge_posts'] = "Fusionner les messages ?";
+$l['merge_posts_desc'] = "Uniquement si utilisé depuis la modération rapide.";
+$l['approve_unapprove_posts'] = "Approuver/désapprouver les messages ?";
+$l['softdelete_restore_posts'] = "Supprimer temporairement/restaurer les messages ?";
+
+$l['split_posts'] = "Diviser les messages";
+$l['split_posts2'] = "Diviser les messages ?";
+$l['do_not_split'] = "Ne pas diviser les messages";
+$l['split_to_same_forum'] = "Diviser dans le même forum";
+$l['close_split_thread'] = "Fermer le sujet divisé ?";
+$l['stick_split_thread'] = "Épingler le sujet divisé ?";
+$l['unapprove_split_thread'] = "Désapprouver le sujet divisé ?";
+$l['split_thread_prefix'] = "Préfixe du sujet divisé";
+$l['split_thread_subject'] = "Titre du sujet divisé";
+$l['split_thread_subject_desc'] = "Requis uniquement si des messages sont divisés.";
+$l['add_new_split_reply'] = "Ajouter une réponse au sujet divisé";
+$l['add_new_split_reply_desc'] = "Laissez ce champ vide pour ne pas ajouter de réponse.";
+$l['split_reply_subject'] = "Sujet de la réponse";
+$l['split_reply_subject_desc'] = "Utilisé uniquement si une réponse est ajoutée.";
+$l['save_post_tool'] = "Enregistrer l’outil de message";
+
+$l['send_private_message'] = "Envoyer un message privé";
+$l['private_message_message'] = "Message";
+$l['private_message_message_desc'] = "Message à envoyer à l’auteur de ce sujet. Laissez ce champ vide pour désactiver cette fonctionnalité.";
+$l['private_message_subject'] = "Sujet";
+$l['private_message_subject_desc'] = "Saisissez le sujet du message privé.";
+
+$l['error_missing_title'] = "Veuillez saisir un nom pour cet outil.";
+$l['error_missing_description'] = "Veuillez saisir une brève description pour cet outil.";
+$l['error_no_forums_selected'] = "Veuillez sélectionner les forums dans lesquels cet outil sera disponible.";
+$l['error_no_groups_selected'] = "Veuillez sélectionner les groupes pour lesquels cet outil sera disponible.";
+$l['error_forum_is_category'] = "Vous ne pouvez pas choisir une catégorie comme forum de destination.";
