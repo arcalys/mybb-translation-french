602f1457e59b38b7e1a2cae5d8c50db19d1ebf60
Axel Fontana <axel.fontana@qualifio.com>
2025-10-14 11:08:33 +0200

    feat: add translation for style-templates

---
+++ inc/languages/french/admin/style_templates.lang.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * MyBB 1.9 French Language Pack
+ * Traduction française pour MyBB 1.9
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['template_sets'] = "Ensembles de templates";
+$l['template_set'] = "Ensemble de templates";
+$l['templates'] = "Templates {1}";
+
+$l['manage_template_sets'] = "Gestion des ensembles de templates";
+$l['manage_template_sets_desc'] = "Cette page vous permet de gérer les ensembles de templates de votre forum et d’en personnaliser l’apparence.";
+$l['add_set'] = "Créer un ensemble de templates";
+$l['add_set_desc'] = "Cette page vous permet de créer un nouvel ensemble de templates. Cette action crée une copie des templates par défaut de MyBB qui vous permet de modifier les templates sans affecter les autres thèmes du forum.";
+$l['add_template'] = "Créer un template";
+$l['add_template_desc'] = "Cette page vous permet de créer un nouveau template.";
+$l['add_template_group'] = "Créer un groupe de templates";
+$l['add_template_group_desc'] = "Cette page vous permet de créer un groupe de templates et de rassembler des templates.";
+$l['search_replace'] = "Rechercher/Remplacer";
+$l['search_replace_desc'] = "Cette page vous permet de faire une recherche et un remplacement de texte dans les templates.";
+$l['find_updated'] = "Rechercher les templates mis à jour";
+$l['find_updated_desc'] = "Cette page vous permet de trouver les templates modifiés dans les nouvelles versions de MyBB afin de pouvoir appliquer correctement les changements.";
+$l['edit_template'] = "Modifier un template";
+$l['editing_template'] = "Modification du template : {1}";
+$l['edit_template_desc'] = "Cette page vous permet de modifier le titre et le contenu d’un template.";
+$l['edit_set'] = "Modifier un ensemble de templates";
+$l['edit_set_desc'] = "Cette page vous permet de modifier les propriétés d’un ensemble de templates.";
+$l['manage_templates'] = "Gestion des templates";
+$l['manage_templates_desc'] = "Cette page vous permet de gérer les templates de cet ensemble. Cliquez sur un template pour le modifier ou sélectionnez « Édition complète » pour accéder à toutes les propriétés.";
+$l['diff_report'] = "Rapport de différences";
+$l['diff_report_desc'] = "Effectue une analyse des différences entre les templates et vous montre exactement les modifications apportées entre votre copie et la dernière version officielle.";
+
+$l['title'] = "Nom";
+$l['save'] = "Enregistrer";
+$l['search_for'] = "Rechercher";
+$l['replace_with'] = "Remplacer par (optionnel)";
+$l['reset'] = "Réinitialiser";
+$l['find_templates'] = "Rechercher des templates";
+$l['find_and_replace'] = "Rechercher et remplacer";
+$l['search_template_names'] = "Rechercher dans les noms des templates";
+$l['ungrouped_templates'] = "Templates non groupés";
+
+$l['search_noneset'] = "Vous n’avez pas saisi de termes de recherche.";
+$l['search_results'] = "Résultats de la recherche de templates";
+$l['search_header'] = "Recherche de « {1} » dans {2}";
+$l['search_updated'] = "Mis à jour {1}";
+$l['search_found'] = "Trouvé dans {1}";
+$l['search_created_custom'] = "Template personnalisé créé pour {1}";
+$l['search_edit'] = "modifier";
+$l['search_change_original'] = "modifier l’original";
+$l['search_noresults'] = "Aucun template ne contient la chaîne '<strong>{1}</strong>'";
+$l['search_noresults_title'] = "Aucun template ne correspond au titre '<strong>{1}</strong>'";
+$l['default_templates'] = "Templates par défaut";
+
+$l['edit_template_breadcrumb'] = "Modifier le template : ";
+
+$l['global_templates'] = "Templates globaux";
+$l['master_templates'] = "Templates principaux";
+
+$l['not_used_by_any_themes'] = "N’est utilisé par aucun thème";
+$l['used_by'] = "Utilisé par : ";
+$l['used_by_all_themes'] = "Utilisé par tous les thèmes";
+
+$l['expand_templates'] = "Afficher les templates";
+$l['edit_template_set'] = "Modifier l’ensemble de templates";
+$l['delete_template_set'] = "Supprimer l’ensemble de templates";
+$l['empty_template_set'] = "<em>Il n’y a aucun template dans cet ensemble.</em>";
+
+$l['inline_edit'] = "Modification rapide";
+$l['full_edit'] = "Modification complète";
+$l['revert_to_orig'] = "Revenir à l’original";
+$l['delete_template'] = "Supprimer le template";
+$l['edit_in'] = "Modifier dans";
+
+$l['group_calendar'] = "des calendriers";
+$l['group_forumdisplay'] = "de l’affichage des forums";
+$l['group_index'] = "de la page d’accueil";
+$l['group_error'] = "des messages d’erreur";
+$l['group_memberlist'] = "de la liste des membres";
+$l['group_multipage'] = "de la pagination";
+$l['group_private'] = "de la messagerie privée";
+$l['group_portal'] = "du portail";
+$l['group_postbit'] = "de la liste des messages";
+$l['group_posticons'] = "des icônes de message";
+$l['group_showthread'] = "de l’affichage des sujets";
+$l['group_usercp'] = "du panneau de l’utilisateur";
+$l['group_online'] = "de la page des utilisateurs en ligne";
+$l['group_forumbit'] = "de la liste des forums";
+$l['group_editpost'] = "de l’édition des messages";
+$l['group_forumjump'] = "du sélecteur de forum";
+$l['group_moderation'] = "de la modération";
+$l['group_nav'] = "de la navigation";
+$l['group_search'] = "de la recherche";
+$l['group_showteam'] = "de la page de l’équipe du forum";
+$l['group_reputation'] = "du système de réputation";
+$l['group_newthread'] = "de la création de sujet";
+$l['group_newreply'] = "de la réponse aux sujets";
+$l['group_member'] = "des profils membres";
+$l['group_warning'] = "du système d’avertissements";
+$l['group_global'] = "des éléments globaux";
+$l['group_header'] = "de l’en-tête";
+$l['group_managegroup'] = "de la gestion des groupes";
+$l['group_misc'] = "des éléments divers";
+$l['group_modcp'] = "du panneau de modération";
+$l['group_announcement'] = "des annonces";
+$l['group_polls'] = "des sondages";
+$l['group_post'] = "des messages";
+$l['group_printthread'] = "de la version imprimable des sujets";
+$l['group_report'] = "des signalements";
+$l['group_smilieinsert'] = "de l’insertion d’émoticônes";
+$l['group_stats'] = "des statistiques";
+$l['group_xmlhttp'] = "des requêtes XMLHTTP";
+$l['group_footer'] = "du pied de page";
+$l['group_video'] = "du MyCode vidéo";
+$l['group_sendthread'] = "de l’envoi de sujet";
+$l['group_mycode'] = "des MyCodes";
+
+$l['expand'] = "Afficher les templates";
+$l['collapse'] = "Masquer les templates";
+
+$l['save_continue'] = "Enregistrer et continuer l’édition";
+$l['save_close'] = "Enregistrer et revenir à la liste";
+
+$l['template_name'] = "Nom du template";
+$l['template_name_desc'] = "Nom du template. Si vous modifiez le nom d’un template par défaut, MyBB créera un template personnalisé avec ce nouveau nom.";
+$l['template_set_desc'] = "Dans quel ensemble de templates ce template doit-il être inclus ?";
+
+$l['template_group_prefix'] = "Préfixe du groupe de templates";
+$l['template_group_prefix_desc'] = "Saisissez un préfixe unique pour ce groupe de templates. Par exemple, pour regrouper les templates <em>hello_world</em>, <em>hello_foobar</em> et <em>hello_foo</em>, saisissez <strong>hello</strong>.";
+$l['template_group_title'] = "Titre du groupe de templates";
+$l['template_group_title_desc'] = "Saisissez un titre pour ce groupe de templates. Il s’agit du titre qui sera affiché dans la liste des templates. Par exemple, pour les templates <em>hello</em>, saisissez <strong>Hello</strong>.";
+
+$l['edit_template_group'] = "Modifier le groupe de templates";
+$l['editing_template_group'] = "Modification du groupe de templates : {1}";
+$l['delete_template_group'] = "Supprimer le groupe de templates";
+$l['save_template_group'] = "Enregistrer";
+
+$l['templates_the_same'] = "Les deux templates que vous avez sélectionnés sont identiques et ne peuvent pas être comparés.";
+$l['master_updated_ins'] = "Les modifications effectuées entre votre version précédente et celle-ci sont mises en évidence de cette manière.";
+$l['master_updated_del'] = "Toutes les modifications que vous avez apportées à vos templates (les anciens) sont mises en évidence de cette manière.";
+$l['template_diff_analysis'] = "Analyse des différences de template";
+$l['search_names_header'] = "Recherche des templates dont le nom contient « {1} »";
+
+$l['updated_template_welcome1'] = "Modifier - Intègre les mises à jour de la dernière version dans votre template actuel.";
+$l['updated_template_welcome2'] = "Restaurer - Rétablit la version d’origine, toutes vos modifications seront perdues.";
+$l['updated_template_welcome3'] = "Diff - Affiche les différences entre votre template et la version officielle.";
+
+$l['no_global_templates'] = "Il n’y a actuellement aucun template global.";
+$l['no_updated_templates'] = "Il n’y a actuellement aucun template mis à jour depuis votre dernière mise à jour.";
+
+$l['confirm_template_set_deletion'] = "Êtes-vous sûr de vouloir supprimer cet ensemble de templates ?";
+$l['confirm_template_group_delete'] = "Êtes-vous sûr de vouloir supprimer ce groupe de templates ? Cette action ne supprime pas les templates contenus dans le groupe.";
+$l['confirm_template_deletion'] = "Êtes-vous sûr de vouloir supprimer ce template ?";
+$l['confirm_template_revertion'] = "Êtes-vous sûr de vouloir rétablir ce template ?";
+
+$l['error_security_problem'] = "Un problème de sécurité potentiel a été détecté dans le template. Veuillez vérifier vos modifications ou contacter MyBB pour obtenir de l’aide.";
+$l['error_missing_input'] = "Veuillez vous assurer d’avoir fourni toutes les informations nécessaires pour modifier ce template (tid et sid).";
+$l['error_already_exists'] = "Le nom du template est déjà utilisé. Veuillez choisir un autre nom.";
+$l['error_invalid_template'] = "Veuillez sélectionner un template valide.";
+$l['error_missing_set_title'] = "Veuillez saisir un nom pour l’ensemble de templates.";
+$l['error_invalid_input'] = "Veuillez vous assurer que l’ID de l’ensemble de templates est correct.";
+$l['error_invalid_set'] = "Veuillez sélectionner un ensemble de templates valide.";
+$l['error_invalid_template_set'] = "Ensemble de templates sélectionné invalide.";
+$l['error_themes_attached_template_set'] = "Cet ensemble de templates ne peut pas être supprimé car des thèmes y sont attachés.";
+$l['error_missing_group_prefix'] = "Veuillez saisir un préfixe pour le groupe de templates.";
+$l['error_invalid_group_title'] = "Les underscores (_) étant utilisés comme délimiteurs, ils sont interdits dans les préfixes de groupes de templates. Veuillez choisir un autre préfixe.";
+$l['error_missing_group_title'] = "Veuillez saisir un titre pour le groupe de templates.";
+$l['error_duplicate_group_prefix'] = "Un groupe de templates utilise déjà avec ce préfixe. Veuillez choisir un autre préfixe.";
+$l['error_missing_template_group'] = "Le groupe de templates spécifié est invalide ou n’existe pas.";
+$l['error_default_template_group'] = "Vous ne pouvez pas modifier ou supprimer un groupe de templates par défaut.";
+
+$l['success_template_saved'] = "Le template a bien été enregistré.";
+$l['success_template_deleted'] = "Le template a bien été supprimé.";
+$l['success_template_reverted'] = "Le template a bien été restauré.";
+$l['success_template_set_saved'] = "L’ensemble de templates a bien été enregistré.";
+$l['success_template_set_deleted'] = "L’ensemble de templates a bien été supprimé.";
+$l['success_template_group_saved'] = "Le groupe de templates a bien été enregistré.";
+$l['success_template_group_deleted'] = "Le groupe de templates a bien été supprimé.";
