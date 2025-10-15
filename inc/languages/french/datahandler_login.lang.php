ab457f3681b6e187d0ce8c904bb35b78405a0d38
Axel Fontana <axel.fontana@qualifio.com>
2025-10-15 08:58:51 +0200

    fix(datahandler/login): remove unnecessary newline

--- inc/languages/french/datahandler_login.lang.php
+++ inc/languages/french/datahandler_login.lang.php
@@ -6,9 +6,9 @@
  *
  */
 
-$l['logindata_invalidpwordusername'] = "Vous avez saisi une combinaison nom d’utilisateur/mot de passe invalide.<br /><br />Si vous avez oublié votre mot de passe, veuillez <a href=\"member.php?action=lostpw\">le réinitialiser</a>.";
-$l['logindata_invalidpwordusernameemail'] = "Vous avez saisi une combinaison adresse électronique/mot de passe invalide.<br /><br />Si vous avez oublié votre mot de passe, veuillez <a href=\"member.php?action=lostpw\">le réinitialiser</a>.";
-$l['logindata_invalidpwordusernamecombo'] = "Vous avez saisi une combinaison nom d’utilisateur/mot de passe ou adresse électronique/mot de passe invalide.<br /><br />Si vous avez oublié votre mot de passe, veuillez <a href=\"member.php?action=lostpw\">le réinitialiser</a>.";
+$l['logindata_invalidpwordusername'] = "Vous avez saisi une combinaison nom d’utilisateur/mot de passe invalide.<br />Si vous avez oublié votre mot de passe, veuillez <a href=\"member.php?action=lostpw\">le réinitialiser</a>.";
+$l['logindata_invalidpwordusernameemail'] = "Vous avez saisi une combinaison adresse électronique/mot de passe invalide.<br />Si vous avez oublié votre mot de passe, veuillez <a href=\"member.php?action=lostpw\">le réinitialiser</a>.";
+$l['logindata_invalidpwordusernamecombo'] = "Vous avez saisi une combinaison nom d’utilisateur/mot de passe ou adresse électronique/mot de passe invalide.<br />Si vous avez oublié votre mot de passe, veuillez <a href=\"member.php?action=lostpw\">le réinitialiser</a>.";
 
 $l['logindata_regimageinvalid'] = "Le code de vérification d’image que vous avez saisi est incorrect. Veuillez entrer le code tel qu’il apparaît dans l’image.";
 $l['logindata_regimagerequired'] = "Veuillez saisir le code de vérification d’image pour continuer le processus de connexion. Entrez le code tel qu’il apparaît dans l’image.";
